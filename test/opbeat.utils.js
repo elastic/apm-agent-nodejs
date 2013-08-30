6a0905ce1dbfbf7815e7949761efbba9ff68a150
Thomas Watson Steen <w@tson.dk>
2013-08-30 16:02:41 +0200

    Add test of utils.parseStack()

--- test/opbeat.utils.js
+++ test/opbeat.utils.js
@@ -13,4 +13,20 @@ describe('utils', function () {
       utils.validStack([]).should.equal(false);
     });
   });
+
+  describe('.parseStack()', function () {
+    it('should call the callback with an array of stack frames', function (done) {
+      var err = new Error();
+      err.stack; // Error.prepareStackTrace is only called when stack is accessed, so access it
+      utils.parseStack(err.structuredStackTrace, function (frames) {
+        frames.should.be.an.instanceOf(Array);
+        frames.length.should.not.equal(0);
+        frames[0].should.have.ownProperty('filename');
+        frames[0].should.have.ownProperty('function');
+        frames[0].should.have.ownProperty('lineno');
+        frames[0].should.have.ownProperty('in_app');
+        done();
+      });
+    });
+  });
 });
