97be35b72139c75d8701e5de4edac560e93e2b75
Thomas Watson Steen <w@tson.dk>
2015-10-01 11:49:27 +0200

    Fix tests

--- test/instrumentation/index.js
+++ test/instrumentation/index.js
@@ -5,6 +5,15 @@ var test = require('tape')
 var Instrumentation = require('../../lib/instrumentation')
 
 test('basic', function (t) {
+  var expexted = [
+    { transaction: 'foo0', signature: 't00', kind: 'type' },
+    { transaction: 'foo0', signature: 't01', kind: 'type' },
+    { transaction: 'foo0', signature: 'transaction', kind: 'transaction' },
+    { transaction: 'foo1', signature: 't10', kind: 'type' },
+    { transaction: 'foo1', signature: 't11', kind: 'type' },
+    { transaction: 'foo1', signature: 'transaction', kind: 'transaction' }
+  ]
+
   var ins = new Instrumentation(mockClient(function (endpoint, data, cb) {
     var now = new Date()
     var ts = new Date(now.getFullYear(), now.getMonth(), now.getDate(), now.getHours(), now.getMinutes())
@@ -22,28 +31,31 @@ test('basic', function (t) {
       t.ok(trans.durations.every(Number.isFinite.bind(Number)))
     })
 
-    t.equal(data.traces.length, 4)
+    t.equal(data.traces.length, 6)
 
     data.traces.forEach(function (trace, index) {
       var trans = 0
-      if (index > 1) {
-        trans++
-        index -= 2
-      }
-      t.equal(trace.transaction, 'foo' + trans)
-      t.equal(trace.signature, 't' + trans + index)
+      var rootTrans = expexted[index].signature === 'transaction'
+      var parents = rootTrans ? [] : ['transaction']
+      if (index > 2) trans++
+      t.equal(trace.transaction, expexted[index].transaction)
+      t.equal(trace.signature, expexted[index].signature)
       t.equal(trace.durations.length, 1)
-      t.ok(trace.durations.every(Array.isArray.bind(Array)))
+      t.ok(trace.durations.every(Array.isArray.bind(Array)), 'duration should be an array of arrays')
       trace.durations.every(function (arr) {
         t.ok(Number.isFinite(arr[0]))
         t.equal(arr[1], data.transactions[trans].durations[0])
       })
-      t.ok(trace.start_time > 0)
-      t.ok(trace.start_time < 1)
-      t.equal(trace.kind, 'type')
+      if (rootTrans) {
+        t.equal(trace.start_time, 0)
+      } else {
+        t.ok(trace.start_time > 0, 'start_time should be > 0')
+        t.ok(trace.start_time < 1, 'start_time should be < 0')
+      }
+      t.equal(trace.kind, expexted[index].kind)
       t.equal(trace.timestamp, ts.toISOString())
       t.deepEqual(trace.frames, []) // TODO
-      t.deepEqual(trace.parents, [])
+      t.deepEqual(trace.parents, parents)
       t.deepEqual(trace.extra, {})
     })
 
@@ -74,10 +86,13 @@ test('basic', function (t) {
 
 test('same tick', function (t) {
   var ins = new Instrumentation(mockClient(function (endpoint, data, cb) {
-    t.equal(data.traces[0].signature, 't0')
-    t.equal(data.traces[1].signature, 't1')
-    t.deepEqual(data.traces[0].parents, [])
-    t.deepEqual(data.traces[1].parents, ['t0'])
+    t.equal(data.traces.length, 3)
+    t.equal(data.traces[0].signature, 't1')
+    t.equal(data.traces[1].signature, 't0')
+    t.equal(data.traces[2].signature, 'transaction')
+    t.deepEqual(data.traces[0].parents, ['transaction', 't0'])
+    t.deepEqual(data.traces[1].parents, ['transaction'])
+    t.deepEqual(data.traces[2].parents, [])
     t.end()
   }))
 
@@ -92,10 +107,13 @@ test('same tick', function (t) {
 
 test('serial - no parents', function (t) {
   var ins = new Instrumentation(mockClient(function (endpoint, data, cb) {
+    t.equal(data.traces.length, 3)
     t.equal(data.traces[0].signature, 't0')
     t.equal(data.traces[1].signature, 't1')
-    t.deepEqual(data.traces[0].parents, [])
-    t.deepEqual(data.traces[1].parents, [])
+    t.equal(data.traces[2].signature, 'transaction')
+    t.deepEqual(data.traces[0].parents, ['transaction'])
+    t.deepEqual(data.traces[1].parents, ['transaction'])
+    t.deepEqual(data.traces[2].parents, [])
     t.end()
   }))
 
@@ -116,11 +134,13 @@ test('serial - no parents', function (t) {
 
 test('serial - with parents', function (t) {
   var ins = new Instrumentation(mockClient(function (endpoint, data, cb) {
-    t.equal(data.traces.length, 2)
-    t.equal(data.traces[0].signature, 't0')
-    t.equal(data.traces[1].signature, 't1')
-    t.deepEqual(data.traces[0].parents, [])
-    t.deepEqual(data.traces[1].parents, ['t0'])
+    t.equal(data.traces.length, 3)
+    t.equal(data.traces[0].signature, 't1')
+    t.equal(data.traces[1].signature, 't0')
+    t.equal(data.traces[2].signature, 'transaction')
+    t.deepEqual(data.traces[0].parents, ['transaction', 't0'])
+    t.deepEqual(data.traces[1].parents, ['transaction'])
+    t.deepEqual(data.traces[2].parents, [])
     t.end()
   }))
 
@@ -139,16 +159,16 @@ test('serial - with parents', function (t) {
 
 test('cross stack parenting', function (t) {
   var ins = new Instrumentation(mockClient(function (endpoint, data, cb) {
-    t.equal(pointerChain('_stackPrevStarted', t0), 't0')
-    t.equal(pointerChain('_stackPrevStarted', t1), 't1')
-    t.equal(pointerChain('_stackPrevEnded', t0), 't0')
-    t.equal(pointerChain('_stackPrevEnded', t1), 't1')
+    t.equal(pointerChain('_stackPrevStarted', t0), 't0 -> transaction')
+    t.equal(pointerChain('_stackPrevStarted', t1), 't1 -> transaction')
 
-    t.equal(data.traces.length, 2)
-    t.equal(data.traces[0].signature, 't0')
-    t.equal(data.traces[1].signature, 't1')
-    t.deepEqual(data.traces[0].parents, [])
-    t.deepEqual(data.traces[1].parents, [])
+    t.equal(data.traces.length, 3)
+    t.equal(data.traces[0].signature, 't1')
+    t.equal(data.traces[1].signature, 't0')
+    t.equal(data.traces[2].signature, 'transaction')
+    t.deepEqual(data.traces[0].parents, ['transaction'])
+    t.deepEqual(data.traces[1].parents, ['transaction'])
+    t.deepEqual(data.traces[2].parents, [])
     t.end()
   }))
 
@@ -173,16 +193,16 @@ test('cross stack parenting', function (t) {
 
 test('stack merging', function (t) {
   var ins = new Instrumentation(mockClient(function (endpoint, data, cb) {
-    t.equal(pointerChain('_stackPrevStarted', t0), 't0')
-    t.equal(pointerChain('_stackPrevStarted', t1), 't1')
-    t.equal(pointerChain('_stackPrevEnded', t0), 't0 -> t1')
-    t.equal(pointerChain('_stackPrevEnded', t1), 't1')
+    t.equal(pointerChain('_stackPrevStarted', t0), 't0 -> transaction')
+    t.equal(pointerChain('_stackPrevStarted', t1), 't1 -> transaction')
 
-    t.equal(data.traces.length, 2)
-    t.equal(data.traces[0].signature, 't0')
-    t.equal(data.traces[1].signature, 't1')
-    t.deepEqual(data.traces[0].parents, [])
-    t.deepEqual(data.traces[1].parents, [])
+    t.equal(data.traces.length, 3)
+    t.equal(data.traces[0].signature, 't1')
+    t.equal(data.traces[1].signature, 't0')
+    t.equal(data.traces[2].signature, 'transaction')
+    t.deepEqual(data.traces[0].parents, ['transaction'])
+    t.deepEqual(data.traces[1].parents, ['transaction'])
+    t.deepEqual(data.traces[2].parents, [])
     t.end()
   }))
 
@@ -210,27 +230,27 @@ test('stack merging', function (t) {
 
 test('stack branching - no parents', function (t) {
   var ins = new Instrumentation(mockClient(function (endpoint, data, cb) {
-    t.equal(pointerChain('_stackPrevStarted', t0), 't0')
-    t.equal(pointerChain('_stackPrevStarted', t1), 't1 -> t0')
-    t.equal(pointerChain('_stackPrevEnded', t0), 't0')
-    t.equal(pointerChain('_stackPrevEnded', t1), 't1')
+    t.equal(pointerChain('_stackPrevStarted', t0), 't0 -> transaction')
+    t.equal(pointerChain('_stackPrevStarted', t1), 't1 -> t0 -> transaction')
 
-    t.equal(data.traces.length, 2)
+    t.equal(data.traces.length, 3)
     t.equal(data.traces[0].signature, 't0')
     t.equal(data.traces[1].signature, 't1')
-    t.deepEqual(data.traces[0].parents, [])
-    t.deepEqual(data.traces[1].parents, [])
+    t.equal(data.traces[2].signature, 'transaction')
+    t.deepEqual(data.traces[0].parents, ['transaction'])
+    t.deepEqual(data.traces[1].parents, ['transaction'])
+    t.deepEqual(data.traces[2].parents, [])
     t.end()
   }))
 
   var trans = ins.startTransaction()
-  var t0 = trans.startTrace('t0')
-  var t1 = trans.startTrace('t1')
+  var t0 = trans.startTrace('t0') // 1
+  var t1 = trans.startTrace('t1') // 2
   setTimeout(function () {
-    t1.end()
+    t0.end() // 3
   }, 25)
   setTimeout(function () {
-    t0.end()
+    t1.end() // 4
     trans.end()
     ins._send()
   }, 50)
@@ -238,65 +258,76 @@ test('stack branching - no parents', function (t) {
 
 test('stack branching - with parents', function (t) {
   var ins = new Instrumentation(mockClient(function (endpoint, data, cb) {
-    t.equal(pointerChain('_stackPrevStarted', t0), 't0')
-    t.equal(pointerChain('_stackPrevStarted', t1), 't1 -> t0')
-    t.equal(pointerChain('_stackPrevStarted', t2), 't2 -> t1 -> t0')
-    t.equal(pointerChain('_stackPrevStarted', t3), 't3 -> t1 -> t0')
-    t.equal(pointerChain('_stackPrevStarted', t4), 't4 -> t2 -> t1 -> t0')
-    t.equal(pointerChain('_stackPrevStarted', t5), 't5 -> t3 -> t1 -> t0')
-    t.equal(pointerChain('_stackPrevEnded', t0), 't0 -> t5 -> t3')
-    t.equal(pointerChain('_stackPrevEnded', t1), 't1 -> t2 -> t4')
-    t.equal(pointerChain('_stackPrevEnded', t2), 't2 -> t4')
-    t.equal(pointerChain('_stackPrevEnded', t3), 't3')
-    t.equal(pointerChain('_stackPrevEnded', t4), 't4')
-    t.equal(pointerChain('_stackPrevEnded', t5), 't5 -> t3')
+    t.equal(pointerChain('_stackPrevStarted', t0), 't0 -> transaction')
+    t.equal(pointerChain('_stackPrevStarted', t1), 't1 -> t0 -> transaction')
+    t.equal(pointerChain('_stackPrevStarted', t2), 't2 -> t1 -> t0 -> transaction')
+    t.equal(pointerChain('_stackPrevStarted', t3), 't3 -> t1 -> t0 -> transaction')
+    t.equal(pointerChain('_stackPrevStarted', t4), 't4 -> t2 -> t1 -> t0 -> transaction')
+    t.equal(pointerChain('_stackPrevStarted', t5), 't5 -> t3 -> t1 -> t0 -> transaction')
 
-    t.equal(data.traces.length, 6)
-    t.equal(data.traces[0].signature, 't0')
-    t.equal(data.traces[1].signature, 't1')
-    t.equal(data.traces[2].signature, 't2')
+    t.equal(data.traces.length, 7)
+    t.equal(data.traces[0].signature, 't4')
+    t.equal(data.traces[1].signature, 't2')
+    t.equal(data.traces[2].signature, 't1')
     t.equal(data.traces[3].signature, 't3')
-    t.equal(data.traces[4].signature, 't4')
-    t.equal(data.traces[5].signature, 't5')
-    t.deepEqual(data.traces[0].parents, [])
-    t.deepEqual(data.traces[1].parents, [])
-    t.deepEqual(data.traces[2].parents, ['t1'])
-    t.deepEqual(data.traces[3].parents, ['t0'])
-    t.deepEqual(data.traces[4].parents, ['t2', 't1'])
-    t.deepEqual(data.traces[5].parents, ['t0'])
+    t.equal(data.traces[4].signature, 't5')
+    t.equal(data.traces[5].signature, 't0')
+    t.equal(data.traces[6].signature, 'transaction')
+    t.deepEqual(data.traces[0].parents, ['transaction', 't0', 't1', 't2'])
+    t.deepEqual(data.traces[1].parents, ['transaction', 't0', 't1'])
+    t.deepEqual(data.traces[2].parents, ['transaction', 't0'])
+    t.deepEqual(data.traces[3].parents, ['transaction', 't0'])
+    t.deepEqual(data.traces[4].parents, ['transaction', 't0'])
+    t.deepEqual(data.traces[5].parents, ['transaction'])
+    t.deepEqual(data.traces[6].parents, [])
     t.end()
   }))
 
   var trans = ins.startTransaction()
-  var t0 = trans.startTrace('t0')
-  var t1 = trans.startTrace('t1')
+  var t0 = trans.startTrace('t0') // 1
+  var t1 = trans.startTrace('t1') // 2
   var t2, t3, t4, t5
 
   process.nextTick(function () {
-    t2 = trans.startTrace('t2')
+    t2 = trans.startTrace('t2') // 3
     setTimeout(function () {
-      t4 = trans.startTrace('t4')
+      t4 = trans.startTrace('t4') // 5
       setTimeout(function () {
-        t4.end()
-        t2.end()
-        t1.end()
+        t4.end() // 7
+        t2.end() // 8
+        t1.end() // 9
       }, 50)
     }, 50)
   })
   setTimeout(function () {
-    t3 = trans.startTrace('t3')
+    t3 = trans.startTrace('t3') // 4
     setTimeout(function () {
-      t5 = trans.startTrace('t5')
+      t5 = trans.startTrace('t5') // 6
       setTimeout(function () {
-        t3.end()
-        t5.end()
-        t0.end()
+        t3.end() // 10
+        t5.end() // 11
+        t0.end() // 12
         trans.end()
         ins._send()
       }, 50)
     }, 50)
   }, 25)
 })
+
+// test('same trace signature/type, different transactions', function (t) {
+//   var ins = new Instrumentation(mockClient(function (endpoint, data, cb) {
+
+//   }))
+//   var trans0 = ins.startTransaction()
+//   var trans1 = ins.startTransaction()
+//   var t0 = trans0.startTrace('sig', 'type')
+//   var t1 = trans1.startTrace('sig', 'type')
+//   t0.end()
+//   t1.end()
+//   trans0.end()
+//   trans1.end()
+//   ins._send()
+// })
 
 function mockClient (cb) {
   return {
