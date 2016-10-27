1244f172b7435ce2c7986b7675e942142559217e
Thomas Watson Steen <w@tson.dk>
2016-10-27 15:37:20 +0200

    test: fix tests for Node.js v7

--- test/instrumentation/native-promises.js
+++ test/instrumentation/native-promises.js
@@ -18,58 +18,60 @@ var test = require('tape')
 require('./_shared-promise-tests')(test, Promise, ins)
 
 // non-standard v8
-test('Promise.prototype.chain - short', function (t) {
-  t.plan(4)
-  twice(function () {
-    var trans = ins.startTransaction()
-    new Promise(function (resolve, reject) {
-      resolve('foo')
-    }).chain(function (data) {
-      t.equal(data, 'foo')
-      t.equal(ins.currentTransaction._uuid, trans._uuid)
-    }, function () {
-      t.fail('should not reject')
-    })
-  })
-})
-
-// non-standard v8
-test('Promise.prototype.chain - long', function (t) {
-  t.plan(8)
-  twice(function () {
-    var trans = ins.startTransaction()
-    new Promise(function (resolve, reject) {
-      resolve('foo')
-    }).chain(function (data) {
-      t.equal(data, 'foo')
-      t.equal(ins.currentTransaction._uuid, trans._uuid)
-      return 'bar'
-    }, function () {
-      t.fail('should not reject')
-    }).chain(function (data) {
-      t.equal(data, 'bar')
-      t.equal(ins.currentTransaction._uuid, trans._uuid)
-    }, function () {
-      t.fail('should not reject')
-    })
-  })
-})
-
-// non-standard v8
-test('Promise.accept', function (t) {
-  t.plan(4)
-  twice(function () {
-    var trans = ins.startTransaction()
-    Promise.accept('foo')
-      .then(function (data) {
+if (semver.lt(process.version, '7.0.0')) {
+  test('Promise.prototype.chain - short', function (t) {
+    t.plan(4)
+    twice(function () {
+      var trans = ins.startTransaction()
+      new Promise(function (resolve, reject) {
+        resolve('foo')
+      }).chain(function (data) {
         t.equal(data, 'foo')
         t.equal(ins.currentTransaction._uuid, trans._uuid)
-      })
-      .catch(function () {
+      }, function () {
         t.fail('should not reject')
       })
+    })
   })
-})
+
+  // non-standard v8
+  test('Promise.prototype.chain - long', function (t) {
+    t.plan(8)
+    twice(function () {
+      var trans = ins.startTransaction()
+      new Promise(function (resolve, reject) {
+        resolve('foo')
+      }).chain(function (data) {
+        t.equal(data, 'foo')
+        t.equal(ins.currentTransaction._uuid, trans._uuid)
+        return 'bar'
+      }, function () {
+        t.fail('should not reject')
+      }).chain(function (data) {
+        t.equal(data, 'bar')
+        t.equal(ins.currentTransaction._uuid, trans._uuid)
+      }, function () {
+        t.fail('should not reject')
+      })
+    })
+  })
+
+  // non-standard v8
+  test('Promise.accept', function (t) {
+    t.plan(4)
+    twice(function () {
+      var trans = ins.startTransaction()
+      Promise.accept('foo')
+        .then(function (data) {
+          t.equal(data, 'foo')
+          t.equal(ins.currentTransaction._uuid, trans._uuid)
+        })
+        .catch(function () {
+          t.fail('should not reject')
+        })
+    })
+  })
+}
 
 function twice (fn) {
   setImmediate(fn)
