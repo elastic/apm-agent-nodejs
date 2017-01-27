e24c0b36c6d30e51996e52c889220a0d861535a6
Kmaschta <Kmaschta@users.noreply.github.com>
2017-01-27 10:57:03 +0100

    core: add OPBEAT_CONFIG_FILE environment variable (#117)
    When using the `opbeat/start` script, we look for an optional config file in the root of your app. Using this environment variable, you can now specify an alternative path for the config file, e.g:
    
        OPBEAT_CONFIG_FILE="build/opbeat.js" node -r opbeat/start app.js

--- start.js
+++ start.js
@@ -4,7 +4,7 @@ var path = require('path')
 var fs = require('fs')
 var opbeat = require('./')
 
-var filepath = path.resolve('opbeat.js')
+var filepath = path.resolve(process.env.OPBEAT_CONFIG_FILE || 'opbeat.js')
 
 if (fs.existsSync(filepath)) opbeat.start(require(filepath))
 else opbeat.start()
