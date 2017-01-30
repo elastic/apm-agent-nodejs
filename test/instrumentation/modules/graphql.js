6a1897456f148f5493f4c5abf1c0878a50d54fea
Thomas Watson Steen <w@tson.dk>
2017-01-30 18:25:29 +0100

    graphql: trace GraphQL queries

---
+++ test/instrumentation/modules/graphql.js
@@ -0,0 +1,129 @@
+'use strict'
+
+var agent = require('../../..').start({
+  appId: 'test',
+  organizationId: 'test',
+  secretToken: 'test',
+  captureExceptions: false
+})
+
+var semver = require('semver')
+if (semver.lt(process.version, '1.0.0')) process.exit()
+
+var test = require('tape')
+var graphql = require('graphql')
+
+test('graphql.graphql', function (t) {
+  resetAgent(done(t))
+
+  var schema = graphql.buildSchema('type Query { hello: String }')
+  var root = {hello: function () {
+    return 'Hello world!'
+  }}
+  var query = '{ hello }'
+
+  agent.startTransaction('foo')
+
+  graphql.graphql(schema, query, root).then(function (response) {
+    agent.endTransaction()
+    t.deepEqual(response, {data: {hello: 'Hello world!'}})
+    agent._instrumentation._queue._flush()
+  })
+})
+
+test('graphql.execute', function (t) {
+  resetAgent(done(t))
+
+  var schema = graphql.buildSchema('type Query { hello: String }')
+  var root = {hello: function () {
+    return 'Hello world!'
+  }}
+  var query = '{ hello }'
+  var source = new graphql.Source(query)
+  var documentAST = graphql.parse(source)
+
+  agent.startTransaction('foo')
+
+  graphql.execute(schema, documentAST, root).then(function (response) {
+    agent.endTransaction()
+    t.deepEqual(response, {data: {hello: 'Hello world!'}})
+    agent._instrumentation._queue._flush()
+  })
+})
+
+// { transactions:
+//    [ { transaction: 'foo',
+//        result: undefined,
+//        kind: 'request',
+//        timestamp: '2017-01-30T16:15:00.000Z',
+//        durations: [ 6.560766 ] } ],
+//   traces:
+//    { groups:
+//       [ { transaction: 'foo',
+//           signature: 'GraphQL: hello',
+//           kind: 'db.graphql.execute',
+//           transaction_kind: 'request',
+//           timestamp: '2017-01-30T16:15:00.000Z',
+//           parents: [ 'transaction' ],
+//           extra: { _frames: [Object] } },
+//         { transaction: 'foo',
+//           signature: 'transaction',
+//           kind: 'transaction',
+//           transaction_kind: 'request',
+//           timestamp: '2017-01-30T16:15:00.000Z',
+//           parents: [],
+//           extra: { _frames: [Object] } } ],
+//      raw:
+//       [ [ 6.560766,
+//           [ 0, 1.392375, 3.968823 ],
+//           [ 1, 0, 6.560766 ],
+//           { extra: [Object], user: {} } ] ] } }
+function done (t) {
+  return function (endpoint, headers, data, cb) {
+    t.equal(data.transactions.length, 1)
+    t.equal(data.transactions[0].transaction, 'foo')
+    t.equal(data.transactions[0].kind, 'request')
+
+    t.equal(data.traces.groups.length, 2)
+
+    t.equal(data.traces.groups[0].kind, 'db.graphql.execute')
+    t.equal(data.traces.groups[0].transaction_kind, 'request')
+    t.deepEqual(data.traces.groups[0].parents, ['transaction'])
+    t.equal(data.traces.groups[0].signature, 'GraphQL: hello')
+    t.equal(data.traces.groups[0].transaction, 'foo')
+
+    t.equal(data.traces.groups[1].kind, 'transaction')
+    t.equal(data.traces.groups[1].transaction_kind, 'request')
+    t.deepEqual(data.traces.groups[1].parents, [])
+    t.equal(data.traces.groups[1].signature, 'transaction')
+    t.equal(data.traces.groups[1].transaction, 'foo')
+
+    var totalTraces = data.traces.raw[0].length - 2
+    var totalTime = data.traces.raw[0][0]
+
+    t.equal(data.traces.raw.length, 1)
+    t.equal(totalTraces, 2)
+
+    for (var i = 1; i < totalTraces + 1; i++) {
+      t.equal(data.traces.raw[0][i].length, 3)
+      t.ok(data.traces.raw[0][i][0] >= 0, 'group index should be >= 0')
+      t.ok(data.traces.raw[0][i][0] < data.traces.groups.length, 'group index should be within allowed range')
+      t.ok(data.traces.raw[0][i][1] >= 0)
+      t.ok(data.traces.raw[0][i][2] <= totalTime)
+    }
+
+    t.equal(data.traces.raw[0][totalTraces][1], 0, 'root trace should start at 0')
+    t.equal(data.traces.raw[0][totalTraces][2], data.traces.raw[0][0], 'root trace should last to total time')
+
+    t.deepEqual(data.transactions[0].durations, [data.traces.raw[0][0]])
+
+    t.end()
+  }
+}
+
+function resetAgent (cb) {
+  agent._instrumentation._queue._clear()
+  agent._instrumentation.currentTransaction = null
+  agent._httpClient = { request: cb || function () {} }
+  agent.captureError = function (err) { throw err }
+}
