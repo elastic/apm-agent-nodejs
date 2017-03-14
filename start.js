ae00d0f12ac988844a34d8788313e20e42548478
Thomas Watson <w@tson.dk>
2017-03-14 11:25:33 +0100

    core: read config file when calling opbeat.start()
    Previously the config file was only used when requireing the special
    start.js file. Now it's also used when calling the regular start
    function.

--- start.js
+++ start.js
@@ -1,12 +1,3 @@
 'use strict'
 
-var path = require('path')
-var fs = require('fs')
-var opbeat = require('./')
-
-var filepath = path.resolve(process.env.OPBEAT_CONFIG_FILE || 'opbeat.js')
-
-if (fs.existsSync(filepath)) opbeat.start(require(filepath))
-else opbeat.start()
-
-module.exports = opbeat
+module.exports = require('./').start()
