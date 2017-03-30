26a441c72426b4549d8c66d6e019572b8efcada7
Thomas Watson <w@tson.dk>
2017-03-30 21:28:25 +0200

    test: improve source map test coverage

---
+++ test/sourcemaps/fixtures/lib/error-inline-broken.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// Just a little prefixing line
+var generateError = function generateError() {
+  var msg = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'foo';
+  return new Error(msg);
+};
+
+module.exports = generateError;
+
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzW9uIjozLCJzb3VyY2VzIjpbIi4uL3NyYy9lcnJvci5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOztBQUFBO0FBQ0EsSUFBTSxnQkFBZ0IsU0FBaEIsYUFBZ0I7QUFBQSxNQUFDLEdBQUQsdUVBQU8sS0FBUDtBQUFBLFNBQWlCLElBQUksS0FBSixDQUFVLEdBQVYsQ0FBakI7QUFBQSxDQUF0Qjs7QUFFQSxPQUFPLE9BQVAsR0FBaUIsYUFBakIiLCJmaWxlIjoiZXJyb3ItaW5saW5lLmpzIiwic291cmNlc0NvbnRlbnQiOlsiLy8gSnVzdCBhIGxpdHRsZSBwcmVmaXhpbmcgbGluZVxuY29uc3QgZ2VuZXJhdGVFcnJvciA9IChtc2cgPSAnZm9vJykgPT4gbmV3IEVycm9yKG1zZylcblxubW9kdWxlLmV4cG9ydHMgPSBnZW5lcmF0ZUVycm9yXG4iXX0=
--- test/sourcemaps/index.js
+++ test/sourcemaps/index.js
@@ -12,24 +12,77 @@ var opbeat = require('../../').start({
 })
 
 test('source map inlined', function (t) {
-  onError(t, assertFound)
+  onError(t, assertSourceFound)
   opbeat.captureError(require('./fixtures/lib/error-inline')())
 })
 
 test('source map linked', function (t) {
   t.test('source mapped source code embedded', function (t) {
-    onError(t, assertFound)
+    onError(t, assertSourceFound)
     opbeat.captureError(require('./fixtures/lib/error-src-embedded')())
   })
 
   t.test('source mapped source code on disk', function (t) {
-    onError(t, assertFound)
+    onError(t, assertSourceFound)
     opbeat.captureError(require('./fixtures/lib/error')())
   })
 
   t.test('source mapped source code not found', function (t) {
-    onError(t, assertNotFound)
+    onError(t, assertSourceNotFound)
     opbeat.captureError(require('./fixtures/lib/error-src-missing')())
+  })
+})
+
+test('fails', function (t) {
+  t.test('inlined source map broken', function (t) {
+    onError(t, function (t, data) {
+      t.equal(data.message, 'Error: foo')
+      t.deepEqual(data.exception, {type: 'Error', value: 'foo'})
+      t.equal(data.culprit, 'generateError (test/sourcemaps/fixtures/lib/error-inline-broken.js)')
+
+      var frame = data.stacktrace.frames.reverse()[0]
+      t.equal(frame.filename, 'test/sourcemaps/fixtures/lib/error-inline-broken.js')
+      t.equal(frame.lineno, 6)
+      t.equal(frame.function, 'generateError')
+      t.equal(frame.in_app, __dirname.indexOf('node_modules') === -1)
+      t.equal(frame.abs_path, path.join(__dirname, 'fixtures', 'lib', 'error-inline-broken.js'))
+      t.equal(frame.context_line, '  return new Error(msg);')
+    })
+    opbeat.captureError(require('./fixtures/lib/error-inline-broken')())
+  })
+
+  t.test('linked source map not found', function (t) {
+    onError(t, function (t, data) {
+      t.equal(data.message, 'Error: foo')
+      t.deepEqual(data.exception, {type: 'Error', value: 'foo'})
+      t.equal(data.culprit, 'generateError (test/sourcemaps/fixtures/lib/error-map-missing.js)')
+
+      var frame = data.stacktrace.frames.reverse()[0]
+      t.equal(frame.filename, 'test/sourcemaps/fixtures/lib/error-map-missing.js')
+      t.equal(frame.lineno, 6)
+      t.equal(frame.function, 'generateError')
+      t.equal(frame.in_app, __dirname.indexOf('node_modules') === -1)
+      t.equal(frame.abs_path, path.join(__dirname, 'fixtures', 'lib', 'error-map-missing.js'))
+      t.equal(frame.context_line, '  return new Error(msg);')
+    })
+    opbeat.captureError(require('./fixtures/lib/error-map-missing')())
+  })
+
+  t.test('linked source map broken', function (t) {
+    onError(t, function (t, data) {
+      t.equal(data.message, 'Error: foo')
+      t.deepEqual(data.exception, {type: 'Error', value: 'foo'})
+      t.equal(data.culprit, 'generateError (test/sourcemaps/fixtures/lib/error-broken.js)')
+
+      var frame = data.stacktrace.frames.reverse()[0]
+      t.equal(frame.filename, 'test/sourcemaps/fixtures/lib/error-broken.js')
+      t.equal(frame.lineno, 6)
+      t.equal(frame.function, 'generateError')
+      t.equal(frame.in_app, __dirname.indexOf('node_modules') === -1)
+      t.equal(frame.abs_path, path.join(__dirname, 'fixtures', 'lib', 'error-broken.js'))
+      t.equal(frame.context_line, '  return new Error(msg);')
+    })
+    opbeat.captureError(require('./fixtures/lib/error-broken')())
   })
 })
 
@@ -40,7 +93,7 @@ function onError (t, assert) {
   }}
 }
 
-function assertFound (t, data) {
+function assertSourceFound (t, data) {
   t.equal(data.message, 'Error: foo')
   t.deepEqual(data.exception, {type: 'Error', value: 'foo'})
   t.equal(data.culprit, 'generateError (test/sourcemaps/fixtures/src/error.js)')
@@ -56,7 +109,7 @@ function assertFound (t, data) {
   t.deepEqual(frame.post_context, ['', 'module.exports = generateError', ''])
 }
 
-function assertNotFound (t, data) {
+function assertSourceNotFound (t, data) {
   t.equal(data.message, 'Error: foo')
   t.deepEqual(data.exception, {type: 'Error', value: 'foo'})
   t.equal(data.culprit, 'generateError (test/sourcemaps/fixtures/src/not/found.js)')
