dc3fc71bd11ef1ea0a8d5d1e4546f728371511ad
Thomas Watson <w@tson.dk>
2017-04-20 10:36:03 +0200

    feat(elasticsearch): add simple Elasticsearch instrumentation
    This adds traces for all Elasticsearch queries. The query is logged as
    well, but for now the index is not logged as that isn't easily available
    from the Elasticsearch driver.
    
    Example of new Elasticsearch trace:
    
    {
      name: 'Elasticsearch: POST /_search',
      type: 'db.elasticsearch.request',
      start: 2.241679,
      duration: 8.883929,
      stacktrace: [...],
      context: {
        db: { statement: '{"q":"pants"}', type: 'elasticsearch' }
      }
    }

--- test/pretest.sh
+++ test/pretest.sh
@@ -3,6 +3,7 @@
 if [ "$TRAVIS" != "true" ]; then
   postgres -D /usr/local/var/postgres >/tmp/postgres.log 2>&1 &
   mongod --fork --config /usr/local/etc/mongod.conf >/tmp/mongod.log 2>&1
+  elasticsearch -p /tmp/elasticsearch.pid --daemonize && echo 'waiting for elasticsearch to start...' && wait-on tcp:9200
   redis-server /usr/local/etc/redis.conf --daemonize yes
   mysql.server start
 fi
