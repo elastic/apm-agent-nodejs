devarajchidambaram/opbeat-node

refactor(instrumentation): rename transaction._uuid to transaction.id

lib/instrumentation/http-shared.js
@@ -85,18 +85,18 @@ exports.traceOutgoingRequest = function (agent, moduleName) {
   return function (orig) {
     return function () {
       var trace = agent.buildTrace()
-      var uuid = trace && trace.transaction._uuid
+      var id = trace && trace.transaction.id
 
-      debug('intercepted call to %s.request %o', moduleName, { uuid: uuid })
+      debug('intercepted call to %s.request %o', moduleName, {id: id})
 
       var req = orig.apply(this, arguments)
       if (!trace) return req
       if (req._headers.host === agent.apiHost) {
-        debug('ignore %s request to opbeat server %o', moduleName, { uuid: uuid })
+        debug('ignore %s request to opbeat server %o', moduleName, {id: id})
         return req
       } else {
         var protocol = req.agent && req.agent.protocol
-        debug('request details: %o', { protocol: protocol, host: req._headers.host, uuid: uuid })
+        debug('request details: %o', {protocol: protocol, host: req._headers.host, id: id})
       }
 
       var name = req.method + ' ' + req._headers.host + url.parse(req.path).pathname
@@ -106,11 +106,11 @@ exports.traceOutgoingRequest = function (agent, moduleName) {
       return req
 
       function onresponse (res) {
-        debug('intercepted http.ClientRequest response event %o', { uuid: uuid })
+        debug('intercepted http.ClientRequest response event %o', {id: id})
         shimmer.wrap(res, 'emit', function wrapEmit (original) {
           return function wrappedEmit (event) {
             if (event === 'end') {
-              debug('intercepted http.IncomingMessage end event %o', { uuid: uuid })
+              debug('intercepted http.IncomingMessage end event %o', {id: id})
               trace.end()
             }
             return original.apply(this, arguments)

elastic/apm-agent-nodejs

lib/instrumentation/http-shared.js
@@ -84,18 +84,18 @@ exports.traceOutgoingRequest = function (agent, moduleName) {
   return function (orig) {
     return function () {
       var trace = agent.buildTrace()
-      var uuid = trace && trace.transaction._uuid
+      var id = trace && trace.transaction.id
 
-      debug('intercepted call to %s.request %o', moduleName, { uuid: uuid })
+      debug('intercepted call to %s.request %o', moduleName, {id: id})
 
       var req = orig.apply(this, arguments)
       if (!trace) return req
       if (req._headers.host === agent.apiHost) {
-        debug('ignore %s request to intake API %o', moduleName, { uuid: uuid })
+        debug('ignore %s request to intake API %o', moduleName, {id: id})
         return req
       } else {
         var protocol = req.agent && req.agent.protocol
-        debug('request details: %o', { protocol: protocol, host: req._headers.host, uuid: uuid })
+        debug('request details: %o', {protocol: protocol, host: req._headers.host, id: id})
       }
 
       var name = req.method + ' ' + req._headers.host + url.parse(req.path).pathname
@@ -105,7 +105,7 @@ exports.traceOutgoingRequest = function (agent, moduleName) {
       return req
 
       function onresponse (res) {
-        debug('intercepted http.ClientRequest response event %o', { uuid: uuid })
+        debug('intercepted http.ClientRequest response event %o', {id: id})
 
         // Inspired by:
         // https://github.com/nodejs/node/blob/9623ce572a02632b7596452e079bba066db3a429/lib/events.js#L258-L274
@@ -126,7 +126,7 @@ exports.traceOutgoingRequest = function (agent, moduleName) {
         }
 
         function onEnd () {
-          debug('intercepted http.IncomingMessage end event %o', { uuid: uuid })
+          debug('intercepted http.IncomingMessage end event %o', {id: id})
           trace.end()
         }
       }