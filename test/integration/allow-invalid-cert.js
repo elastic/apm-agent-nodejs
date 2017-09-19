b3c9260bb7174ee5c621cf1cea585f8e5ed305bf
Thomas Watson <w@tson.dk>
2017-09-19 17:24:16 +0200

    feat(tls): add validateServerCert config option (#32)
    By default the agent will validate the SSL/TLS certificates used by the
    APM Server (if running on HTTPS). This validation can now be turned off
    by setting the `validateServerCert` option to `false`. This is
    particularly useful if the APM Server is using self-signed certificates.

---
+++ test/integration/allow-invalid-cert.js
@@ -0,0 +1,33 @@
+'use strict'
+
+var getPort = require('get-port')
+
+getPort().then(function (port) {
+  var agent = require('../../').start({
+    appName: 'test',
+    serverUrl: 'https://localhost:' + port,
+    validateServerCert: false
+  })
+
+  var https = require('https')
+  var pem = require('https-pem')
+  var test = require('tape')
+
+  test('should allow self signed certificate', function (t) {
+    t.plan(2)
+
+    var server = https.createServer(pem, function (req, res) {
+      t.pass('server received client request')
+      res.end()
+    })
+
+    server.listen(port, function () {
+      agent.captureError(new Error('boom!'), function () {
+        server.close()
+        t.pass('agent.captureError callback called')
+      })
+    })
+  })
+}, function (err) {
+  throw err
+})
