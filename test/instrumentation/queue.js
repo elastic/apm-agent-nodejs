3ea77e268bd77758f36b048c793494ea4bd1c9b5
Thomas Watson <w@tson.dk>
2017-10-03 13:04:53 -0700

    feat(queue): add maxQueueSize config option
    Use the maxQueueSize option to force a flush of the queue when it
    reaches a certain size.

--- test/instrumentation/queue.js
+++ test/instrumentation/queue.js
@@ -3,16 +3,28 @@
 var test = require('tape')
 var Queue = require('../../lib/instrumentation/queue')
 
+test('maxQueueSize', function (t) {
+  var opts = {
+    maxQueueSize: 5,
+    flushInterval: 1e6
+  }
+
+  var queue = new Queue(opts, function (arr) {
+    t.deepEqual(arr, [0, 1, 2, 3, 4])
+    t.end()
+  })
+
+  for (var n = 0; n < 9; n++) queue.add(n)
+})
+
 test('queue flush isolation', function (t) {
   var flush = 0
-  var queue = new Queue(function (arr) {
+  var queue = new Queue({maxQueueSize: 1}, function (arr) {
     t.equal(arr.length, 1)
     t.equal(arr[0], ++flush)
     if (flush === 2) t.end()
   })
 
   queue.add(1)
-  queue._flush()
   queue.add(2)
-  queue._flush()
 })
