530f3a550c9ef81ad68fb065fd940df0a267f8b6
Stephen Belanger <admin@stephenbelanger.com>
2017-12-06 03:01:02 -0800

    test(*): add code coverage reporting (#86)

--- test/script/docker/run_tests.sh
+++ test/script/docker/run_tests.sh
@@ -9,6 +9,10 @@ fi
 
 npm_cache="$HOME/.npm"
 docker_npm_cache="/home/node/.npm"
+nyc_output=`pwd`"/coverage"
+docker_nyc_output="/app/coverage"
+nyc_report_output=`pwd`"/.nyc_output"
+docker_nyc_report_output="/app/.nyc_output"
 
 NODE_VERSION=$1
 if [[ ! -z $2  ]]; then
@@ -16,6 +20,8 @@ if [[ ! -z $2  ]]; then
   CMD='npm i test-all-versions &&
        export PATH=./node_modules/.bin:$PATH &&
        tav --quiet'
+elif [[ -n $COVERAGE ]]; then
+  CMD='npm run report-coverage'
 else
   CMD='npm test'
 fi
@@ -24,11 +30,13 @@ docker-compose build --pull --build-arg NODE_VERSION=$1 node_tests
 docker-compose run \
   -e NODE_VERSION=${NODE_VERSION} -e TAV=${TAV_VERSIONS} -e CI=true \
   -v ${npm_cache}:${docker_npm_cache} \
+  -v ${nyc_output}:${docker_nyc_output} \
+  -v ${nyc_report_output}:${docker_nyc_report_output} \
   --rm node_tests \
   /bin/bash \
   -c "npm config set cache ${docker_npm_cache} --global
-      npm install 
+      npm install
       if [[ ${NODE_VERSION} == '8' ]]; then npm install -g npm@4; fi;
-			node --version  
-			npm --version  
+			node --version
+			npm --version
       ${CMD}"
