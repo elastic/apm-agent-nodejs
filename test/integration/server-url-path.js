efe50f7ddc3172289168a6cedf18805d3598e0d2
Thomas Watson <w@tson.dk>
2017-12-15 22:22:45 +0100

    feat(conf): allow serverUrl to contain a sub-path (#116)
    Fixes #115

---
+++ test/integration/server-url-path.js
@@ -0,0 +1,29 @@
+'use strict'
+
+var getPort = require('get-port')
+
+getPort().then(function (port) {
+  var agent = require('../../').start({
+    appName: 'test',
+    serverUrl: 'http://localhost:' + port + '/sub',
+    captureExceptions: false
+  })
+
+  var http = require('http')
+  var test = require('tape')
+
+  test('should not sample', function (t) {
+    var server = http.createServer(function (req, res) {
+      t.equal(req.url, '/sub/v1/errors')
+      res.end()
+      server.close()
+      t.end()
+    })
+
+    server.listen(port, function () {
+      agent.captureError(new Error('foo'))
+    })
+  })
+}, function (err) {
+  throw err
+})
