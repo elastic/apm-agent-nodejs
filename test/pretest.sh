bb1816fe28c0af6620570821100adc086415d8d4
Thomas Watson <w@tson.dk>
2018-01-02 16:57:48 +0100

    test(*): always use wait-on in node_modules (#136)

--- test/pretest.sh
+++ test/pretest.sh
@@ -3,7 +3,7 @@
 if [[ "$CI" != "true" ]]; then
   pg_ctl -D /usr/local/var/postgres start
   mongod --fork --config /usr/local/etc/mongod.conf --pidfilepath /tmp/mongod.pid >/tmp/mongod.log 2>&1
-  elasticsearch -p /tmp/elasticsearch.pid --daemonize && echo 'waiting for elasticsearch to start...' && wait-on tcp:9200
+  elasticsearch -p /tmp/elasticsearch.pid --daemonize && echo 'waiting for elasticsearch to start...' && ./node_modules/.bin/wait-on tcp:9200
   redis-server /usr/local/etc/redis.conf --daemonize yes
   mysql.server start
 fi
