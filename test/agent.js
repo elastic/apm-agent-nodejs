4d0ce0e47a6edaf8ea563dd8668b02989c532842
Thomas Watson <w@tson.dk>
2018-01-14 15:29:04 +0100

    refactor(test): move agent config tests into separate file

--- test/agent.js
+++ test/agent.js
@@ -1,16 +1,13 @@
 'use strict'
 
-var os = require('os')
-var util = require('util')
 var http = require('http')
 var zlib = require('zlib')
 var test = require('tape')
 var nock = require('nock')
 var getPort = require('get-port')
-var isRegExp = require('core-util-is').isRegExp
 var isError = require('core-util-is').isError
+var Agent = require('./_agent')
 var request = require('../lib/request')
-var Agent = require('../lib/agent')
 
 var opts = {
   appName: 'some-app-name',
@@ -19,190 +16,18 @@ var opts = {
   logLevel: 'error'
 }
 
-var optionFixtures = [
-  ['appName', 'APP_NAME'],
-  ['secretToken', 'SECRET_TOKEN'],
-  ['appVersion', 'APP_VERSION'],
-  ['logLevel', 'LOG_LEVEL', 'info'],
-  ['hostname', 'HOSTNAME', os.hostname()],
-  ['stackTraceLimit', 'STACK_TRACE_LIMIT', 50],
-  ['captureExceptions', 'CAPTURE_EXCEPTIONS', true],
-  ['instrument', 'INSTRUMENT', true],
-  ['asyncHooks', 'ASYNC_HOOKS', true],
-  ['ff_captureFrame', 'FF_CAPTURE_FRAME', false]
-]
-
-var falsyValues = [false, 0, '', '0', 'false', 'no', 'off', 'disabled']
-var truthyValues = [true, 1, '1', 'true', 'yes', 'on', 'enabled']
-
 var skipBody = function () { return '*' }
-var uncaughtExceptionListeners = process._events.uncaughtException
-var agent
-
-var setup = function () {
-  clean()
-  uncaughtExceptionListeners = process._events.uncaughtException
-  process.removeAllListeners('uncaughtException')
-  agent = new Agent()
-}
-
-var clean = function () {
-  global._elastic_apm_initialized = null
-  process._events.uncaughtException = uncaughtExceptionListeners
-  if (agent) agent._filters = []
-}
-
-optionFixtures.forEach(function (fixture) {
-  if (fixture[1]) {
-    test('should be configurable by envrionment variable ELASTIC_APM_' + fixture[1], function (t) {
-      setup()
-      var bool = typeof fixture[2] === 'boolean'
-      var value = bool ? (fixture[2] ? '0' : '1') : 'custom-value'
-      process.env['ELASTIC_APM_' + fixture[1]] = value
-      agent.start()
-      t.equal(agent._conf[fixture[0]], bool ? !fixture[2] : value)
-      delete process.env['ELASTIC_APM_' + fixture[1]]
-      t.end()
-    })
-
-    test('should overwrite ELASTIC_APM_' + fixture[1] + ' by option property ' + fixture[0], function (t) {
-      setup()
-      var opts = {}
-      var bool = typeof fixture[2] === 'boolean'
-      var value1 = bool ? (fixture[2] ? '0' : '1') : 'overwriting-value'
-      var value2 = bool ? (fixture[2] ? '1' : '0') : 'custom-value'
-      opts[fixture[0]] = value1
-      process.env['ELASTIC_APM_' + fixture[1]] = value2
-      agent.start(opts)
-      t.equal(agent._conf[fixture[0]], bool ? !fixture[2] : value1)
-      delete process.env['ELASTIC_APM_' + fixture[1]]
-      t.end()
-    })
-  }
-
-  test('should default ' + fixture[0] + ' to ' + fixture[2], function (t) {
-    setup()
-    agent.start()
-    t.equal(agent._conf[fixture[0]], fixture[2])
-    t.end()
-  })
-})
-
-falsyValues.forEach(function (val) {
-  test('should be disabled by envrionment variable ELASTIC_APM_ACTIVE set to: ' + util.inspect(val), function (t) {
-    setup()
-    process.env.ELASTIC_APM_ACTIVE = val
-    agent.start({ appName: 'foo', secretToken: 'baz' })
-    t.equal(agent._conf.active, false)
-    delete process.env.ELASTIC_APM_ACTIVE
-    t.end()
-  })
-})
-
-truthyValues.forEach(function (val) {
-  test('should be enabled by envrionment variable ELASTIC_APM_ACTIVE set to: ' + util.inspect(val), function (t) {
-    setup()
-    process.env.ELASTIC_APM_ACTIVE = val
-    agent.start({ appName: 'foo', secretToken: 'baz' })
-    t.equal(agent._conf.active, true)
-    delete process.env.ELASTIC_APM_ACTIVE
-    t.end()
-  })
-})
-
-test('should overwrite ELASTIC_APM_ACTIVE by option property active', function (t) {
-  setup()
-  var opts = { appName: 'foo', secretToken: 'baz', active: false }
-  process.env.ELASTIC_APM_ACTIVE = '1'
-  agent.start(opts)
-  t.equal(agent._conf.active, false)
-  delete process.env.ELASTIC_APM_ACTIVE
-  t.end()
-})
-
-test('should default active to true if required options have been specified', function (t) {
-  setup()
-  agent.start({ appName: 'foo', secretToken: 'baz' })
-  t.equal(agent._conf.active, true)
-  t.end()
-})
-
-test('should default active to false if required options have not been specified', function (t) {
-  setup()
-  agent.start()
-  t.equal(agent._conf.active, false)
-  t.end()
-})
-
-test('should force active to false if required options have not been specified', function (t) {
-  setup()
-  agent.start({ active: true })
-  t.equal(agent._conf.active, false)
-  t.end()
-})
-
-test('should default to empty request blacklist arrays', function (t) {
-  setup()
-  agent.start()
-  t.equal(agent._conf.ignoreUrlStr.length, 0)
-  t.equal(agent._conf.ignoreUrlRegExp.length, 0)
-  t.equal(agent._conf.ignoreUserAgentStr.length, 0)
-  t.equal(agent._conf.ignoreUserAgentRegExp.length, 0)
-  t.end()
-})
-
-test('should separate strings and regexes into their own blacklist arrays', function (t) {
-  setup()
-  agent.start({
-    ignoreUrls: ['str1', /regex1/],
-    ignoreUserAgents: ['str2', /regex2/]
-  })
-
-  t.deepEqual(agent._conf.ignoreUrlStr, ['str1'])
-  t.deepEqual(agent._conf.ignoreUserAgentStr, ['str2'])
-
-  t.equal(agent._conf.ignoreUrlRegExp.length, 1)
-  t.ok(isRegExp(agent._conf.ignoreUrlRegExp[0]))
-  t.equal(agent._conf.ignoreUrlRegExp[0].toString(), '/regex1/')
-
-  t.equal(agent._conf.ignoreUserAgentRegExp.length, 1)
-  t.ok(isRegExp(agent._conf.ignoreUserAgentRegExp[0]))
-  t.equal(agent._conf.ignoreUserAgentRegExp[0].toString(), '/regex2/')
-
-  t.end()
-})
-
-test('missing appName => inactive', function (t) {
-  setup()
-  agent.start()
-  t.equal(agent._conf.active, false)
-  t.end()
-})
-
-test('invalid appName => inactive', function (t) {
-  setup()
-  agent.start({appName: 'foo&bar'})
-  t.equal(agent._conf.active, false)
-  t.end()
-})
-
-test('valid appName => active', function (t) {
-  setup()
-  agent.start({appName: 'fooBAR0123456789_- '})
-  t.equal(agent._conf.active, true)
-  t.end()
-})
 
 test('#setUserContext()', function (t) {
   t.test('no active transaction', function (t) {
-    setup()
+    var agent = Agent()
     agent.start()
     t.equal(agent.setUserContext({foo: 1}), false)
     t.end()
   })
 
   t.test('active transaction', function (t) {
-    setup()
+    var agent = Agent()
     agent.start()
     var trans = agent.startTransaction()
     t.equal(agent.setUserContext({foo: 1}), true)
@@ -213,14 +38,14 @@ test('#setUserContext()', function (t) {
 
 test('#setCustomContext()', function (t) {
   t.test('no active transaction', function (t) {
-    setup()
+    var agent = Agent()
     agent.start()
     t.equal(agent.setCustomContext({foo: 1}), false)
     t.end()
   })
 
   t.test('active transaction', function (t) {
-    setup()
+    var agent = Agent()
     agent.start()
     var trans = agent.startTransaction()
     t.equal(agent.setCustomContext({foo: 1}), true)
@@ -231,14 +56,14 @@ test('#setCustomContext()', function (t) {
 
 test('#setTag()', function (t) {
   t.test('no active transaction', function (t) {
-    setup()
+    var agent = Agent()
     agent.start()
     t.equal(agent.setTag('foo', 1), false)
     t.end()
   })
 
   t.test('active transaction', function (t) {
-    setup()
+    var agent = Agent()
     agent.start()
     var trans = agent.startTransaction()
     t.equal(agent.setTag('foo', 1), true)
@@ -249,7 +74,7 @@ test('#setTag()', function (t) {
 
 test('#captureError()', function (t) {
   t.test('with callback', function (t) {
-    setup()
+    var agent = Agent()
     agent.start(opts)
     var scope = nock('http://localhost:8200')
       .filteringRequestBody(skipBody)
@@ -263,7 +88,7 @@ test('#captureError()', function (t) {
   })
 
   t.test('without callback', function (t) {
-    setup()
+    var agent = Agent()
     agent.start(opts)
     var scope = nock('http://localhost:8200')
       .filteringRequestBody(skipBody)
@@ -279,7 +104,7 @@ test('#captureError()', function (t) {
   })
 
   t.test('should send a plain text message to the server', function (t) {
-    setup()
+    var agent = Agent()
     agent.start(opts)
     var scope = nock('http://localhost:8200')
       .filteringRequestBody(skipBody)
@@ -293,7 +118,7 @@ test('#captureError()', function (t) {
   })
 
   t.test('should use `param_message` as well as `message` if given an object as 1st argument', function (t) {
-    setup()
+    var agent = Agent()
     agent.start(opts)
     var oldErrorsFn = request.errors
     request.errors = function (agent, errors, cb) {
@@ -307,7 +132,7 @@ test('#captureError()', function (t) {
   })
 
   t.test('should send an Error to server', function (t) {
-    setup()
+    var agent = Agent()
     agent.start(opts)
     var scope = nock('http://localhost:8200')
       .filteringRequestBody(skipBody)
@@ -322,7 +147,7 @@ test('#captureError()', function (t) {
 
   t.test('should adhere to default stackTraceLimit', function (t) {
     getPort().then(function (port) {
-      setup()
+      var agent = Agent()
       agent.start(Object.assign(
         {serverUrl: 'http://localhost:' + port},
         opts
@@ -359,7 +184,7 @@ test('#captureError()', function (t) {
 
   t.test('should adhere to custom stackTraceLimit', function (t) {
     getPort().then(function (port) {
-      setup()
+      var agent = Agent()
       agent.start(Object.assign(
         {stackTraceLimit: 5, serverUrl: 'http://localhost:' + port},
         opts
@@ -395,7 +220,7 @@ test('#captureError()', function (t) {
   })
 
   t.test('should merge context', function (t) {
-    setup()
+    var agent = Agent()
     agent.start({
       appName: 'foo',
       secretToken: 'baz',
@@ -435,7 +260,7 @@ test('#captureError()', function (t) {
 
 test('#handleUncaughtExceptions()', function (t) {
   t.test('should add itself to the uncaughtException event list', function (t) {
-    setup()
+    var agent = Agent()
     t.equal(process._events.uncaughtException, undefined)
     agent.start(opts)
     agent.handleUncaughtExceptions()
@@ -444,7 +269,7 @@ test('#handleUncaughtExceptions()', function (t) {
   })
 
   t.test('should not add more than one listener for the uncaughtException event', function (t) {
-    setup()
+    var agent = Agent()
     agent.start(opts)
     agent.handleUncaughtExceptions()
     var before = process._events.uncaughtException.length
@@ -454,7 +279,7 @@ test('#handleUncaughtExceptions()', function (t) {
   })
 
   t.test('should send an uncaughtException to server', function (t) {
-    setup()
+    var agent = Agent()
 
     var scope = nock('http://localhost:8200')
       .filteringRequestBody(skipBody)
---
+++ test/config.js
@@ -0,0 +1,164 @@
+'use strict'
+
+var os = require('os')
+var util = require('util')
+var test = require('tape')
+var isRegExp = require('core-util-is').isRegExp
+var Agent = require('./_agent')
+
+var optionFixtures = [
+  ['appName', 'APP_NAME'],
+  ['secretToken', 'SECRET_TOKEN'],
+  ['appVersion', 'APP_VERSION'],
+  ['logLevel', 'LOG_LEVEL', 'info'],
+  ['hostname', 'HOSTNAME', os.hostname()],
+  ['stackTraceLimit', 'STACK_TRACE_LIMIT', 50],
+  ['captureExceptions', 'CAPTURE_EXCEPTIONS', true],
+  ['instrument', 'INSTRUMENT', true],
+  ['asyncHooks', 'ASYNC_HOOKS', true],
+  ['ff_captureFrame', 'FF_CAPTURE_FRAME', false]
+]
+
+var falsyValues = [false, 0, '', '0', 'false', 'no', 'off', 'disabled']
+var truthyValues = [true, 1, '1', 'true', 'yes', 'on', 'enabled']
+
+optionFixtures.forEach(function (fixture) {
+  if (fixture[1]) {
+    test('should be configurable by envrionment variable ELASTIC_APM_' + fixture[1], function (t) {
+      var agent = Agent()
+      var bool = typeof fixture[2] === 'boolean'
+      var value = bool ? (fixture[2] ? '0' : '1') : 'custom-value'
+      process.env['ELASTIC_APM_' + fixture[1]] = value
+      agent.start()
+      t.equal(agent._conf[fixture[0]], bool ? !fixture[2] : value)
+      delete process.env['ELASTIC_APM_' + fixture[1]]
+      t.end()
+    })
+
+    test('should overwrite ELASTIC_APM_' + fixture[1] + ' by option property ' + fixture[0], function (t) {
+      var agent = Agent()
+      var opts = {}
+      var bool = typeof fixture[2] === 'boolean'
+      var value1 = bool ? (fixture[2] ? '0' : '1') : 'overwriting-value'
+      var value2 = bool ? (fixture[2] ? '1' : '0') : 'custom-value'
+      opts[fixture[0]] = value1
+      process.env['ELASTIC_APM_' + fixture[1]] = value2
+      agent.start(opts)
+      t.equal(agent._conf[fixture[0]], bool ? !fixture[2] : value1)
+      delete process.env['ELASTIC_APM_' + fixture[1]]
+      t.end()
+    })
+  }
+
+  test('should default ' + fixture[0] + ' to ' + fixture[2], function (t) {
+    var agent = Agent()
+    agent.start()
+    t.equal(agent._conf[fixture[0]], fixture[2])
+    t.end()
+  })
+})
+
+falsyValues.forEach(function (val) {
+  test('should be disabled by envrionment variable ELASTIC_APM_ACTIVE set to: ' + util.inspect(val), function (t) {
+    var agent = Agent()
+    process.env.ELASTIC_APM_ACTIVE = val
+    agent.start({ appName: 'foo', secretToken: 'baz' })
+    t.equal(agent._conf.active, false)
+    delete process.env.ELASTIC_APM_ACTIVE
+    t.end()
+  })
+})
+
+truthyValues.forEach(function (val) {
+  test('should be enabled by envrionment variable ELASTIC_APM_ACTIVE set to: ' + util.inspect(val), function (t) {
+    var agent = Agent()
+    process.env.ELASTIC_APM_ACTIVE = val
+    agent.start({ appName: 'foo', secretToken: 'baz' })
+    t.equal(agent._conf.active, true)
+    delete process.env.ELASTIC_APM_ACTIVE
+    t.end()
+  })
+})
+
+test('should overwrite ELASTIC_APM_ACTIVE by option property active', function (t) {
+  var agent = Agent()
+  var opts = { appName: 'foo', secretToken: 'baz', active: false }
+  process.env.ELASTIC_APM_ACTIVE = '1'
+  agent.start(opts)
+  t.equal(agent._conf.active, false)
+  delete process.env.ELASTIC_APM_ACTIVE
+  t.end()
+})
+
+test('should default active to true if required options have been specified', function (t) {
+  var agent = Agent()
+  agent.start({ appName: 'foo', secretToken: 'baz' })
+  t.equal(agent._conf.active, true)
+  t.end()
+})
+
+test('should default active to false if required options have not been specified', function (t) {
+  var agent = Agent()
+  agent.start()
+  t.equal(agent._conf.active, false)
+  t.end()
+})
+
+test('should force active to false if required options have not been specified', function (t) {
+  var agent = Agent()
+  agent.start({ active: true })
+  t.equal(agent._conf.active, false)
+  t.end()
+})
+
+test('should default to empty request blacklist arrays', function (t) {
+  var agent = Agent()
+  agent.start()
+  t.equal(agent._conf.ignoreUrlStr.length, 0)
+  t.equal(agent._conf.ignoreUrlRegExp.length, 0)
+  t.equal(agent._conf.ignoreUserAgentStr.length, 0)
+  t.equal(agent._conf.ignoreUserAgentRegExp.length, 0)
+  t.end()
+})
+
+test('should separate strings and regexes into their own blacklist arrays', function (t) {
+  var agent = Agent()
+  agent.start({
+    ignoreUrls: ['str1', /regex1/],
+    ignoreUserAgents: ['str2', /regex2/]
+  })
+
+  t.deepEqual(agent._conf.ignoreUrlStr, ['str1'])
+  t.deepEqual(agent._conf.ignoreUserAgentStr, ['str2'])
+
+  t.equal(agent._conf.ignoreUrlRegExp.length, 1)
+  t.ok(isRegExp(agent._conf.ignoreUrlRegExp[0]))
+  t.equal(agent._conf.ignoreUrlRegExp[0].toString(), '/regex1/')
+
+  t.equal(agent._conf.ignoreUserAgentRegExp.length, 1)
+  t.ok(isRegExp(agent._conf.ignoreUserAgentRegExp[0]))
+  t.equal(agent._conf.ignoreUserAgentRegExp[0].toString(), '/regex2/')
+
+  t.end()
+})
+
+test('missing appName => inactive', function (t) {
+  var agent = Agent()
+  agent.start()
+  t.equal(agent._conf.active, false)
+  t.end()
+})
+
+test('invalid appName => inactive', function (t) {
+  var agent = Agent()
+  agent.start({appName: 'foo&bar'})
+  t.equal(agent._conf.active, false)
+  t.end()
+})
+
+test('valid appName => active', function (t) {
+  var agent = Agent()
+  agent.start({appName: 'fooBAR0123456789_- '})
+  t.equal(agent._conf.active, true)
+  t.end()
+})
