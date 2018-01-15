c5f48a3dc58116ff0f168a21afeca5e7545ee4dd
Thomas Watson <w@tson.dk>
2018-01-15 13:06:16 +0100

    refactor(instrumentation): encode transactions as they are added to the queue
    Fixes #182

--- test/instrumentation/trace.js
+++ test/instrumentation/trace.js
@@ -1,5 +1,7 @@
 'use strict'
 
+process.env.ELASTIC_APM_TEST = true
+
 var test = require('tape')
 var mockAgent = require('./_agent')
 var mockInstrumentation = require('./_instrumentation')
@@ -78,4 +80,107 @@ test('#offsetTime() - sub trace', function (t) {
     trace.end()
     trans.end()
   }, 50)
+})
+
+test('#_encode() - un-started', function (t) {
+  var trans = new Transaction(agent)
+  var trace = new Trace(trans)
+  trace._encode(function (err, payload) {
+    t.equal(err.message, 'cannot encode un-started trace')
+    t.end()
+  })
+})
+
+test('#_encode() - un-ended', function (t) {
+  var trans = new Transaction(agent)
+  var trace = new Trace(trans)
+  trace.start()
+  trace._encode(function (err, payload) {
+    t.equal(err.message, 'cannot encode un-ended trace')
+    t.end()
+  })
+})
+
+test('#_encode() - ended unnamed', function myTest1 (t) {
+  var trans = new Transaction(agent)
+  var trace = new Trace(trans)
+  trace.start()
+  trace.end()
+  trace._encode(function (err, payload) {
+    t.error(err)
+    t.deepEqual(Object.keys(payload), ['name', 'type', 'start', 'duration', 'stacktrace'])
+    t.equal(payload.name, 'unnamed')
+    t.equal(payload.type, 'custom')
+    t.ok(payload.start > 0)
+    t.ok(payload.duration > 0)
+    t.ok(Array.isArray(payload.stacktrace))
+    t.ok(payload.stacktrace.length > 0)
+    t.equal(payload.stacktrace[0].function, 'myTest1')
+    t.equal(payload.stacktrace[0].abs_path, __filename)
+    payload.stacktrace.forEach(function (frame) {
+      t.deepEqual(Object.keys(frame), ['filename', 'lineno', 'function', 'in_app', 'abs_path'])
+      t.equal(typeof frame.filename, 'string')
+      t.ok(Number.isFinite(frame.lineno))
+      t.equal(typeof frame.function, 'string')
+      t.equal(typeof frame.in_app, 'boolean')
+      t.equal(typeof frame.abs_path, 'string')
+    })
+    t.end()
+  })
+})
+
+test('#_encode() - ended named', function myTest2 (t) {
+  var trans = new Transaction(agent)
+  var trace = new Trace(trans)
+  trace.start('foo', 'bar')
+  trace.end()
+  trace._encode(function (err, payload) {
+    t.error(err)
+    t.deepEqual(Object.keys(payload), ['name', 'type', 'start', 'duration', 'stacktrace'])
+    t.equal(payload.name, 'foo')
+    t.equal(payload.type, 'bar')
+    t.ok(payload.start > 0)
+    t.ok(payload.duration > 0)
+    t.ok(Array.isArray(payload.stacktrace))
+    t.ok(payload.stacktrace.length > 0)
+    t.equal(payload.stacktrace[0].function, 'myTest2')
+    t.equal(payload.stacktrace[0].abs_path, __filename)
+    payload.stacktrace.forEach(function (frame) {
+      t.deepEqual(Object.keys(frame), ['filename', 'lineno', 'function', 'in_app', 'abs_path'])
+      t.equal(typeof frame.filename, 'string')
+      t.ok(Number.isFinite(frame.lineno))
+      t.equal(typeof frame.function, 'string')
+      t.equal(typeof frame.in_app, 'boolean')
+      t.equal(typeof frame.abs_path, 'string')
+    })
+    t.end()
+  })
+})
+
+test('#_encode() - truncated', function myTest3 (t) {
+  var trans = new Transaction(agent)
+  var trace = new Trace(trans)
+  trace.start('foo', 'bar')
+  trace.truncate()
+  trace._encode(function (err, payload) {
+    t.error(err)
+    t.deepEqual(Object.keys(payload), ['name', 'type', 'start', 'duration', 'stacktrace'])
+    t.equal(payload.name, 'foo')
+    t.equal(payload.type, 'bar.truncated')
+    t.ok(payload.start > 0)
+    t.ok(payload.duration > 0)
+    t.ok(Array.isArray(payload.stacktrace))
+    t.ok(payload.stacktrace.length > 0)
+    t.equal(payload.stacktrace[0].function, 'myTest3')
+    t.equal(payload.stacktrace[0].abs_path, __filename)
+    payload.stacktrace.forEach(function (frame) {
+      t.deepEqual(Object.keys(frame), ['filename', 'lineno', 'function', 'in_app', 'abs_path'])
+      t.equal(typeof frame.filename, 'string')
+      t.ok(Number.isFinite(frame.lineno))
+      t.equal(typeof frame.function, 'string')
+      t.equal(typeof frame.in_app, 'boolean')
+      t.equal(typeof frame.abs_path, 'string')
+    })
+    t.end()
+  })
 })
