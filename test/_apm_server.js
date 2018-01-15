d3f28c18824b4e81319e5aa406c213ee6ece3de4
Thomas Watson <w@tson.dk>
2018-01-15 20:59:12 +0100

    refactor(test): don't mock APM Server as much (#180)
    Don't mock the APM Server in the agent and request tests.

---
+++ test/_apm_server.js
@@ -0,0 +1,56 @@
+'use strict'
+
+var http = require('http')
+var zlib = require('zlib')
+var util = require('util')
+var EventEmitter = require('events')
+var getPort = require('get-port')
+var Agent = require('./_agent')
+
+var defaultAgentOpts = {
+  appName: 'some-app-name',
+  captureExceptions: false,
+  logLevel: 'error'
+}
+
+module.exports = APMServer
+
+util.inherits(APMServer, EventEmitter)
+
+function APMServer (agentOpts) {
+  if (!(this instanceof APMServer)) return new APMServer(agentOpts)
+  var self = this
+
+  this.agent = Agent()
+
+  EventEmitter.call(this)
+
+  getPort().then(function (port) {
+    self.agent.start(Object.assign(
+      defaultAgentOpts,
+      {serverUrl: 'http://localhost:' + port},
+      agentOpts
+    ))
+
+    var server = http.createServer(function (req, res) {
+      self.emit('request', req, res)
+
+      var buffers = []
+      var gunzip = zlib.createGunzip()
+      var unzipped = req.pipe(gunzip)
+
+      unzipped.on('data', buffers.push.bind(buffers))
+      unzipped.on('end', function () {
+        res.end()
+        server.close()
+        self.emit('body', JSON.parse(Buffer.concat(buffers)))
+      })
+    })
+
+    self.emit('server', server)
+
+    server.listen(port, function () {
+      self.emit('listening', port)
+    })
+  })
+}
--- test/request.js
+++ test/request.js
@@ -1,189 +1,208 @@
 'use strict'
 
 var os = require('os')
-var zlib = require('zlib')
 var test = require('tape')
-var nock = require('nock')
-var Agent = require('../lib/agent')
+var http = require('http')
+var Agent = require('./_agent')
+var APMServer = require('./_apm_server')
 var request = require('../lib/request')
 
 var agentVersion = require('../package.json').version
 
-var opts = {
-  appName: 'some-app-name',
-  secretToken: 'secret',
-  appVersion: 'my-app-version',
-  captureExceptions: false,
-  logLevel: 'error'
-}
-
 test('#errors()', function (t) {
   t.test('envelope', function (t) {
-    global._elastic_apm_initialized = null
-    var agent = new Agent()
-    agent.start(opts)
-    agent._httpClient.request = function (endpoint, headers, data, cb) {
-      t.equal(endpoint, 'errors')
-      t.equal(data.errors.length, 1)
-      assertRoot(t, data)
-      t.end()
-    }
-    request.errors(agent, [{}])
+    t.plan(13)
+    var errors = [{}]
+    APMServer()
+      .on('listening', function () {
+        request.errors(this.agent, errors)
+      })
+      .on('request', validateErrorRequest(t))
+      .on('body', function (body) {
+        assertRoot(t, body)
+        t.deepEqual(body.errors, errors)
+        t.end()
+      })
   })
 
   t.test('non-string log.message', function (t) {
-    global._elastic_apm_initialized = null
-    var agent = new Agent()
-    agent.start(opts)
-    agent._httpClient.request = function () {
-      t.end()
-    }
-    request.errors(agent, [{log: {message: 1}}])
+    t.plan(13)
+    var errors = [{log: {message: 1}}]
+    APMServer()
+      .on('listening', function () {
+        request.errors(this.agent, errors)
+      })
+      .on('request', validateErrorRequest(t))
+      .on('body', function (body) {
+        assertRoot(t, body)
+        t.deepEqual(body.errors, errors)
+        t.end()
+      })
   })
 
   t.test('non-string exception.message', function (t) {
-    global._elastic_apm_initialized = null
-    var agent = new Agent()
-    agent.start(opts)
-    agent._httpClient.request = function () {
-      t.end()
-    }
-    request.errors(agent, [{exception: {message: 1}}])
+    t.plan(13)
+    var errors = [{exception: {message: 1}}]
+    APMServer()
+      .on('listening', function () {
+        request.errors(this.agent, errors)
+      })
+      .on('request', validateErrorRequest(t))
+      .on('body', function (body) {
+        assertRoot(t, body)
+        t.deepEqual(body.errors, errors)
+        t.end()
+      })
   })
 
   t.test('non-string culprit', function (t) {
-    global._elastic_apm_initialized = null
-    var agent = new Agent()
-    agent.start(opts)
-    agent._httpClient.request = function () {
-      t.end()
-    }
-    request.errors(agent, [{culprit: 1}])
+    t.plan(13)
+    var errors = [{culprit: 1}]
+    APMServer()
+      .on('listening', function () {
+        request.errors(this.agent, errors)
+      })
+      .on('request', validateErrorRequest(t))
+      .on('body', function (body) {
+        assertRoot(t, body)
+        t.deepEqual(body.errors, errors)
+        t.end()
+      })
   })
 
   t.test('successful request', function (t) {
-    global._elastic_apm_initialized = null
-    var agent = new Agent()
-    agent.start(opts)
-
+    t.plan(13)
     var errors = [{context: {custom: {foo: 'bar'}}}]
-    var payload = request._envelope(agent)
-    payload.errors = errors
-    var body = JSON.stringify(payload)
+    APMServer()
+      .on('listening', function () {
+        request.errors(this.agent, errors)
+      })
+      .on('request', validateErrorRequest(t))
+      .on('body', function (body) {
+        assertRoot(t, body)
+        t.deepEqual(body.errors, errors)
+        t.end()
+      })
+  })
 
-    zlib.gzip(body, function (err, buffer) {
-      t.error(err)
-      var scope = nock('http://localhost:8200')
-        .filteringRequestBody(function (body) {
-          t.equal(body, buffer.toString('hex'))
-          return 'ok'
-        })
-        .post('/v1/errors', 'ok')
-        .reply(200)
+  t.test('bad request', function (t) {
+    t.plan(2)
+
+    var server = http.createServer(function (req, res) {
+      t.ok(true, 'should make request')
+      res.statusCode = 500
+      res.end()
+    })
+
+    server.listen(function () {
+      var agent = Agent()
+      agent.start({
+        appName: 'foo',
+        serverUrl: 'http://localhost:' + server.address().port
+      })
+      var errors = [{context: {custom: {foo: 'bar'}}}]
       request.errors(agent, errors, function () {
-        scope.done()
+        server.close()
+        t.ok(true, 'should call callback')
         t.end()
       })
     })
   })
 
-  t.test('bad request', function (t) {
-    var errors = [{context: {custom: {foo: 'bar'}}}]
-    global._elastic_apm_initialized = null
-    var agent = new Agent()
-    agent.start(opts)
-    var scope = nock('http://localhost:8200')
-      .filteringRequestBody(function () { return '*' })
-      .post('/v1/errors', '*')
-      .reply(500)
-    request.errors(agent, errors, function () {
-      scope.done()
-      t.end()
-    })
-  })
-
   t.test('should use filters if provided', function (t) {
-    t.plan(3)
-    global._elastic_apm_initialized = null
-    var agent = new Agent()
-    agent.addFilter(function (data) {
-      var error = data.errors[0]
-      t.equal(++error.context.custom.order, 1)
-      return data
-    })
-    agent.addFilter(function (data) {
-      var error = data.errors[0]
-      t.equal(++error.context.custom.order, 2)
-      return {errors: [{owned: true}]}
-    })
-    agent.start(opts)
-    agent._httpClient.request = function (endpoint, headers, data, cb) {
-      t.deepEqual(data, {errors: [{owned: true}]})
-      t.end()
-    }
+    t.plan(5)
     var errors = [{context: {custom: {order: 0}}}]
-    request.errors(agent, errors)
+    APMServer()
+      .on('listening', function () {
+        this.agent.addFilter(function (data) {
+          var error = data.errors[0]
+          t.equal(++error.context.custom.order, 1)
+          return data
+        })
+        this.agent.addFilter(function (data) {
+          var error = data.errors[0]
+          t.equal(++error.context.custom.order, 2)
+          return {errors: [{owned: true}]}
+        })
+        request.errors(this.agent, errors)
+      })
+      .on('request', validateErrorRequest(t))
+      .on('body', function (body) {
+        t.deepEqual(body, {errors: [{owned: true}]})
+        t.end()
+      })
   })
 
   t.test('should abort if any filter returns falsy', function (t) {
-    global._elastic_apm_initialized = null
-    var agent = new Agent()
-    agent.addFilter(function () {})
-    agent.addFilter(function () {
-      t.fail('should not 2nd filter')
-    })
-    agent.start(opts)
-    agent._httpClient.request = function () {
-      t.fail('should not send error to intake')
-    }
+    t.plan(1)
     var errors = [{exception: {message: 'foo'}}]
-    request.errors(agent, errors, function () {
-      t.end()
-    })
+    var server
+    APMServer()
+      .on('server', function (_server) {
+        server = _server
+      })
+      .on('listening', function () {
+        this.agent.addFilter(function () {})
+        this.agent.addFilter(function () {
+          t.fail('should not 2nd filter')
+        })
+        request.errors(this.agent, errors, function () {
+          server.close()
+          t.ok(true, 'should call callback')
+          t.end()
+        })
+      })
+      .on('request', function () {
+        t.fail('should not send error to intake')
+      })
   })
 
   t.test('should anonymize the http Authorization header by default', function (t) {
-    global._elastic_apm_initialized = null
-    var agent = new Agent()
-    agent.start(opts)
-
-    agent._httpClient.request = function (endpoint, headers, data, cb) {
-      t.equal(data.errors.length, 1)
-      t.equal(data.errors[0].context.request.headers.authorization, '[REDACTED]')
-      t.end()
-    }
-
+    t.plan(14)
     var errors = [{context: {request: {headers: {authorization: 'secret'}}}}]
-    request.errors(agent, errors)
+    APMServer()
+      .on('listening', function () {
+        request.errors(this.agent, errors)
+      })
+      .on('request', validateErrorRequest(t))
+      .on('body', function (body) {
+        assertRoot(t, body)
+        t.equal(body.errors.length, 1)
+        t.equal(body.errors[0].context.request.headers.authorization, '[REDACTED]')
+        t.end()
+      })
   })
 
   t.test('should not anonymize the http Authorization header if disabled', function (t) {
-    global._elastic_apm_initialized = null
-    var agent = new Agent()
-    agent.start(Object.assign({filterHttpHeaders: false}, opts))
-
-    agent._httpClient.request = function (endpoint, headers, data, cb) {
-      t.equal(data.errors.length, 1)
-      t.equal(data.errors[0].context.request.headers.authorization, 'secret')
-      t.end()
-    }
-
+    t.plan(14)
     var errors = [{context: {request: {headers: {authorization: 'secret'}}}}]
-    request.errors(agent, errors)
+    APMServer({filterHttpHeaders: false})
+      .on('listening', function () {
+        request.errors(this.agent, errors)
+      })
+      .on('request', validateErrorRequest(t))
+      .on('body', function (body) {
+        assertRoot(t, body)
+        t.equal(body.errors.length, 1)
+        t.equal(body.errors[0].context.request.headers.authorization, 'secret')
+        t.end()
+      })
   })
 })
 
 test('#transactions()', function (t) {
-  global._elastic_apm_initialized = null
-  var agent = new Agent()
-  agent.start(opts)
-  agent._httpClient.request = function (endpoint, headers, data, cb) {
-    t.equal(endpoint, 'transactions')
-    assertRoot(t, data)
-    t.equal(data.transactions.length, 1)
-    t.end()
-  }
-  request.transactions(agent, [{traces: []}])
+  t.plan(13)
+  var transactions = [{traces: []}]
+  APMServer()
+    .on('listening', function () {
+      request.transactions(this.agent, transactions)
+    })
+    .on('request', validateTransactionsRequest(t))
+    .on('body', function (body) {
+      assertRoot(t, body)
+      t.deepEqual(body.transactions, transactions)
+      t.end()
+    })
 })
 
 function assertRoot (t, payload) {
@@ -196,10 +215,23 @@ function assertRoot (t, payload) {
   t.ok(payload.app.argv.length >= 2)
   t.deepEqual(payload.app.runtime, {name: 'node', version: process.version})
   t.deepEqual(payload.app.agent, {name: 'nodejs', version: agentVersion})
-  t.equal(payload.app.version, 'my-app-version')
   t.deepEqual(payload.system, {
     hostname: os.hostname(),
     architecture: process.arch,
     platform: process.platform
   })
+}
+
+function validateErrorRequest (t) {
+  return function (req) {
+    t.equal(req.method, 'POST', 'should be a POST request')
+    t.equal(req.url, '/v1/errors', 'should be sent to the errors endpoint')
+  }
+}
+
+function validateTransactionsRequest (t) {
+  return function (req) {
+    t.equal(req.method, 'POST', 'should be a POST request')
+    t.equal(req.url, '/v1/transactions', 'should be sent to the transactions endpoint')
+  }
 }
