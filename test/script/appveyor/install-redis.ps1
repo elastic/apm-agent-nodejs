c0fe7fed868abacb96d652ca434dafcbbebcc28d
Thomas Watson <w@tson.dk>
2018-01-23 14:55:51 +0100

    test(ci): test on Windows using AppVeyor

---
+++ test/script/appveyor/install-redis.ps1
@@ -0,0 +1,14 @@
+Write-Host "Downloading Redis..." -ForegroundColor Cyan
+$redisRoot = "$env:SYSTEMDRIVE\Redis"
+$zipPath = "$($env:USERPROFILE)\redis-2.8.19.zip"
+(New-Object Net.WebClient).DownloadFile('https://github.com/MSOpenTech/redis/releases/download/win-2.8.19/redis-2.8.19.zip', $zipPath)
+7z x $zipPath -y -o"$redisRoot" | Out-Null
+del $zipPath
+
+Write-Host "Installing Redis as a Windows service..."
+& "$redisRoot\redis-server.exe" --service-install
+
+Write-Host "Starting Redis service..."
+& "$redisRoot\redis-server.exe" --service-start
+
+Write-Host "Redis installed" -ForegroundColor Green
