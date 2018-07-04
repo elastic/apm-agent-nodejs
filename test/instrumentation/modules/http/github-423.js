c92ad0674d5dea2142f099b06a5f13c858087fc7
Thomas Watson <w@tson.dk>
2018-07-04 09:55:39 +0200

    fix: ensure correct streaming when using mimic-response (#429)
    The `mimic-response` module exposes a single function with the following
    API:
    
        mimicResponse(fromStream, toStream)
    
    If someone is calling `mimicResponse` where the `fromStream` have had
    its emitter functions bound using `bindEmitter(fromStream)`, the
    `toStream` will wrongly bind all its event listeners to the
    `fromStream`.
    
    This becomes especially problematic in case the data is transformed
    either in the `toStream` or in between `fromStream` and `toStream`, eg:
    
        fromStream.pipe(zlib.createGunzip()).pipe(toStream)
    
    In those cases, the data emitted by `toStream` will actually be the
    un-transformed data coming from the `fromStream`.
    
    This commit patches `mimic-response` so that the event emitters of
    `toStream` runs in the expected context.

---
+++ test/instrumentation/modules/http/github-423.js
@@ -0,0 +1,48 @@
+'use strict'
+
+require('../../../..').start({
+  serviceName: 'test',
+  captureExceptions: false
+})
+
+var http = require('http')
+var zlib = require('zlib')
+var fs = require('fs')
+var got = require('got')
+var test = require('tape')
+
+var fileSize = fs.readFileSync(__filename, 'utf8').length
+
+test('https://github.com/elastic/apm-agent-nodejs/issues/423', function (t) {
+  // Start dummy remote server to fetch gzip'ed data from
+  var remote = http.createServer(function (req, res) {
+    res.setHeader('Content-Encoding', 'gzip')
+    fs.createReadStream(__filename).pipe(zlib.createGzip()).pipe(res)
+  })
+
+  remote.listen(function () {
+    var port = remote.address().port
+    var url = 'http://localhost:' + port
+
+    // Start simple server that performs got-request on every request
+    var server = http.createServer(function (req, res) {
+      got(url).then(function (response) {
+        t.equal(response.body.length, fileSize, 'body should be expected size')
+        t.equal(response.body.slice(0, 12), '\'use strict\'', 'body should be uncompressed')
+        res.end()
+      })
+    })
+
+    server.listen(function () {
+      var port = server.address().port
+      var url = 'http://localhost:' + port
+
+      http.get(url, function (res) {
+        res.resume()
+        server.close()
+        remote.close()
+        t.end()
+      })
+    })
+  })
+})
