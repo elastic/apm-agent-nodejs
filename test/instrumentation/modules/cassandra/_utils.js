4450fe6cb4aacf35f772b8ba204a95cd56de3ff9
Stephen Belanger <stephen.belanger@elastic.co>
2018-07-05 18:38:38 -0700

    feat(cassandra): instrumentation

---
+++ test/instrumentation/modules/cassandra/_utils.js
@@ -0,0 +1,32 @@
+const cassandra = require('cassandra-driver')
+
+module.exports = function makeClient (t, opts) {
+  const options = Object.assign({
+    contactPoints: ['localhost']
+  }, opts)
+
+  const client = new cassandra.Client(options)
+
+  t.on('end', () => {
+    client.shutdown()
+  })
+
+  if (!options.keyspace) {
+    return Promise.resolve(client)
+  }
+
+  const keyspace = options.keyspace
+  const query1 = `
+    CREATE KEYSPACE IF NOT EXISTS ${keyspace} WITH replication = {
+      'class': 'SimpleStrategy',
+      'replication_factor': 1
+    };
+  `
+  const query2 = `
+    CREATE TABLE IF NOT EXISTS ${keyspace}.${keyspace}(id uuid,text varchar,PRIMARY KEY(id));
+  `
+
+  return client.execute(query1)
+    .then(() => client.execute(query2))
+    .then(() => client)
+}
