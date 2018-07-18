8afe3d3901787d0274293c5abea2c555850076dd
Thomas Watson <w@tson.dk>
2018-07-18 21:07:18 +0200

    test: re-add the old hapi test exceptions

--- test/instrumentation/modules/hapi.js
+++ test/instrumentation/modules/hapi.js
@@ -13,8 +13,8 @@ var semver = require('semver')
 // hapi 17+ requires Node.js 8.9.0 or higher
 if (semver.lt(process.version, '8.9.0') && semver.gte(pkg.version, '17.0.0')) process.exit()
 
-// hapi does not work on early versions of Node.js 10 because of https://github.com/nodejs/node/issues/20516
-if (semver.satisfies(process.version, '>=10.0.0-rc <10.4.0')) process.exit()
+// hapi does not work on Node.js 10 because of https://github.com/nodejs/node/issues/20516
+if (semver.gte(process.version, '10.0.0-rc')) process.exit()
 
 var http = require('http')
 
