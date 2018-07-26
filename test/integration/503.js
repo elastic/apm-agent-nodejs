eb63d92d46c8ba2aec8d2f201353c33b019ae0c3
Thomas Watson <w@tson.dk>
2018-07-26 18:59:37 +0200

    test: ensure APM Server 503's are handled (#466)

---
+++ test/integration/503.js
@@ -0,0 +1,32 @@
+'use strict'
+
+var getPort = require('get-port')
+
+getPort().then(function (port) {
+  var agent = require('../../').start({
+    serviceName: 'test',
+    serverUrl: 'http://localhost:' + port,
+    captureExceptions: false
+  })
+
+  var http = require('http')
+  var test = require('tape')
+
+  test('should not throw on 503', function (t) {
+    var server = http.createServer(function (req, res) {
+      res.statusCode = 503
+      res.setHeader('Content-Type', 'application/json')
+      res.end('{"error":"something"}')
+    })
+
+    server.listen(port, function () {
+      agent.captureError(new Error('foo'), function (err) {
+        t.error(err)
+        t.end()
+        server.close()
+      })
+    })
+  })
+}, function (err) {
+  throw err
+})
