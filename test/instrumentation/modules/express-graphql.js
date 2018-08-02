d9a0b3d20532573b4ccc94ef75946a32f61b92f1
Thomas Watson <w@tson.dk>
2018-08-02 19:32:25 +0200

    feat: support APM Server intake API version 2 (#465)
    Closes #356

--- test/instrumentation/modules/express-graphql.js
+++ test/instrumentation/modules/express-graphql.js
@@ -14,6 +14,8 @@ var querystring = require('querystring')
 var graphqlHTTP = require('express-graphql')
 var test = require('tape')
 
+var mockClient = require('../../_mock_http_client')
+
 test('POST /graphql', function (t) {
   resetAgent(done(t, 'hello'))
 
@@ -156,25 +158,25 @@ test('POST /graphql - sort multiple queries', function (t) {
 })
 
 function done (t, query) {
-  return function (endpoint, headers, data, cb) {
+  return function (data, cb) {
     t.equal(data.transactions.length, 1)
+    t.equal(data.spans.length, 1)
 
     var trans = data.transactions[0]
+    var span = data.spans[0]
 
     t.equal(trans.name, query + ' (/graphql)')
     t.equal(trans.type, 'request')
-    t.equal(trans.spans.length, 1)
-    t.equal(trans.spans[0].name, 'GraphQL: ' + query)
-    t.equal(trans.spans[0].type, 'db.graphql.execute')
-    t.ok(trans.spans[0].start + trans.spans[0].duration < trans.duration)
+    t.equal(span.name, 'GraphQL: ' + query)
+    t.equal(span.type, 'db.graphql.execute')
+    t.ok(span.start + span.duration < trans.duration)
 
     t.end()
   }
 }
 
 function resetAgent (cb) {
-  agent._instrumentation._queue._clear()
   agent._instrumentation.currentTransaction = null
-  agent._httpClient = { request: cb || function () {} }
+  agent._apmServer = mockClient(2, cb)
   agent.captureError = function (err) { throw err }
 }
