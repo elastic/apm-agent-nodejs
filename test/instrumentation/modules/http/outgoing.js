68be8ce209aece03a9a31ed1a4cc680af4af438a
Stephen Belanger <stephen.belanger@elastic.co>
2018-09-06 16:10:08 -0700

    feat(http): support trace-context
    Send Elastic-APM-traceparent header in outgoing requests,
    and adopt the header for trace context on incoming requests.

--- test/instrumentation/modules/http/outgoing.js
+++ test/instrumentation/modules/http/outgoing.js
@@ -6,6 +6,7 @@ var test = require('tape')
 
 var echoServer = require('./_echo_server_util').echoServer
 var mockClient = require('../../../_mock_http_client')
+var TraceContext = require('.././../../../lib/instrumentation/trace-context')
 
 var transports = [['http', require('http')], ['https', require('https')]]
 
@@ -15,21 +16,27 @@ transports.forEach(function (tuple) {
 
   test(name + '.request', function (t) {
     echoServer(name, function (cp, port) {
-      resetAgent(function (data) {
-        t.equal(data.transactions.length, 1)
-        t.equal(data.spans.length, 1)
-        t.equal(data.spans[0].name, 'GET localhost:' + port + '/')
+      resetAgent(function (data, ...args) {
+        t.equal(data.transactions.length, 1, 'has one transaction')
+        t.equal(data.spans.length, 1, 'has one span')
+        t.equal(data.spans[0].name, 'GET localhost:' + port + '/', 'has expected span name')
         t.end()
         cp.kill()
       })
 
-      agent.startTransaction()
+      var trans = agent.startTransaction()
       var req = transport.request({ port: port, rejectUnauthorized: false }, function (res) {
         res.on('end', function () {
           agent.endTransaction()
         })
         res.resume()
       })
+      const expected = TraceContext.fromString(trans.context.toString())
+      const received = TraceContext.fromString(req.getHeader('Elastic-APM-traceparent'))
+      t.equal(received.version, expected.version, 'traceparent header has matching version')
+      t.equal(received.traceId, expected.traceId, 'traceparent header has matching traceId')
+      t.ok(/^[\da-f]{16}$/.test(expected.id), 'traceparent header has valid id')
+      t.equal(received.flags, expected.flags, 'traceparent header has matching flags')
       req.end()
     })
   })
