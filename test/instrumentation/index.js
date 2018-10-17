d0a32ad4b501b0325de09b758055deeb6286523f
Stephen Belanger <admin@stephenbelanger.com>
2018-10-17 07:04:05 -0700

    feat: add support for Distributed Tracing (#538)

--- test/instrumentation/index.js
+++ test/instrumentation/index.js
@@ -292,8 +292,8 @@ test('errors should not have a transaction id if no transaction is present', fun
 test('errors should have a transaction id - non-ended transaction', function (t) {
   resetAgent(1, function (data) {
     t.equal(data.errors.length, 1)
-    t.deepEqual(data.errors[0].transaction, { id: trans.id })
-    t.equal(typeof data.errors[0].transaction.id, 'string')
+    t.equal(data.errors[0].transaction_id, trans.id)
+    t.equal(typeof data.errors[0].transaction_id, 'string')
     t.end()
   })
   agent.captureError = origCaptureError
@@ -306,8 +306,8 @@ test('errors should have a transaction id - ended transaction', function (t) {
     t.equal(data.transactions.length, 1)
     t.equal(data.errors.length, 1)
     const trans = data.transactions[0]
-    t.deepEqual(data.errors[0].transaction, { id: trans.id })
-    t.equal(typeof data.errors[0].transaction.id, 'string')
+    t.equal(data.errors[0].transaction_id, trans.id)
+    t.equal(typeof data.errors[0].transaction_id, 'string')
     t.end()
   })
   agent.captureError = origCaptureError
@@ -532,6 +532,125 @@ test('bind', function (t) {
       emitter.emit('foo')
     })
   })
+})
+
+test('nested spans', function (t) {
+  resetAgent(6, function (data) {
+    t.equal(data.transactions.length, 1)
+    t.equal(data.spans.length, 5)
+
+    const trans = data.transactions[0]
+    t.equal(trans.name, 'foo')
+    t.equal(trans.type, 'custom')
+    t.equal(trans.span_count.started, 5)
+
+    const s0 = findObjInArray(data.spans, 'name', 's0')
+    t.equal(s0.parent_id, trans.id, 's0 should directly descend from the transaction')
+    t.equal(s0.trace_id, trans.trace_id, 's0 has same trace_id as transaction')
+    t.equal(s0.transaction_id, trans.id, 's0 transaction_id matches transaction id')
+
+    const s1 = findObjInArray(data.spans, 'name', 's1')
+    t.equal(s1.parent_id, trans.id, 's1 should directly descend from the transaction')
+    t.equal(s1.trace_id, trans.trace_id, 's1 has same trace_id as transaction')
+    t.equal(s1.transaction_id, trans.id, 's1 transaction_id matches transaction id')
+
+    const s01 = findObjInArray(data.spans, 'name', 's01')
+    t.equal(s01.parent_id, s0.id, 's01 should descend from s0')
+    t.equal(s01.trace_id, trans.trace_id, 's01 has same trace_id as transaction')
+    t.equal(s01.transaction_id, trans.id, 's01 transaction_id matches transaction id')
+
+    const s11 = findObjInArray(data.spans, 'name', 's11')
+    t.equal(s11.parent_id, s1.id, 's11 should descend from s1')
+    t.equal(s11.trace_id, trans.trace_id, 's11 has same trace_id as transaction')
+    t.equal(s11.transaction_id, trans.id, 's11 transaction_id matches transaction id')
+
+    const s12 = findObjInArray(data.spans, 'name', 's12')
+    t.equal(s12.parent_id, s1.id, 's12 should descend from s1')
+    t.equal(s12.trace_id, trans.trace_id, 's12 has same trace_id as transaction')
+    t.equal(s12.transaction_id, trans.id, 's12 transaction_id matches transaction id')
+
+    t.end()
+  })
+  var ins = agent._instrumentation
+
+  var trans = ins.startTransaction('foo')
+  var count = 0
+  function done () {
+    s1.end()
+    if (++count === 2) {
+      trans.end()
+    }
+  }
+
+  var s0 = startSpan(ins, 's0')
+  process.nextTick(function () {
+    process.nextTick(function () {
+      var s01 = startSpan(ins, 's01')
+      process.nextTick(function () {
+        s01.end()
+        done()
+      })
+    })
+    s0.end()
+  })
+
+  var s1 = startSpan(ins, 's1')
+  process.nextTick(function () {
+    var s11 = startSpan(ins, 's11')
+    process.nextTick(function () {
+      s11.end()
+      done()
+    })
+  })
+
+  // Will adopt the t1 span as its parent,
+  // because no new span has been created.
+  process.nextTick(function () {
+    var s12 = startSpan(ins, 's12')
+    process.nextTick(function () {
+      s12.end()
+      done()
+    })
+  })
+})
+
+test('nested transactions', function (t) {
+  resetAgent(4, function (data) {
+    t.equal(data.transactions.length, 2)
+    t.equal(data.spans.length, 2)
+
+    const t0 = findObjInArray(data.transactions, 'name', 't0')
+    t.equal(t0.type, 'custom')
+    t.equal(t0.span_count.started, 1)
+
+    const s0 = findObjInArray(data.spans, 'name', 's0')
+    t.equal(s0.parent_id, t0.id, 's0 should directly descend from the transaction')
+    t.equal(s0.trace_id, t0.trace_id, 't0 has same trace_id as transaction')
+    t.equal(s0.transaction_id, t0.id, 't0 transaction_id matches transaction id')
+
+    const t1 = findObjInArray(data.transactions, 'name', 't1')
+    t.equal(t1.type, 'custom')
+    t.equal(t1.span_count.started, 1)
+    t.equal(t1.parent_id, t0.id, 't1 should directly descend from the t0')
+    t.equal(t1.trace_id, t0.trace_id, 't1 has same trace_id as transaction')
+
+    const s1 = findObjInArray(data.spans, 'name', 's1')
+    t.equal(s1.parent_id, t1.id, 's1 should directly descend from the transaction')
+    t.equal(s1.trace_id, t1.trace_id, 't1 has same trace_id as transaction')
+    t.equal(s1.transaction_id, t1.id, 't1 transaction_id matches transaction id')
+
+    t.end()
+  })
+  var ins = agent._instrumentation
+
+  var t0 = ins.startTransaction('t0')
+  var s0 = startSpan(ins, 's0')
+  var t1 = ins.startTransaction('t1', null, t0.context.toString())
+  var s1 = startSpan(ins, 's1')
+  s1.end()
+  t1.end()
+  s0.end()
+  t0.end()
 })
 
 function startSpan (ins, name, type) {
