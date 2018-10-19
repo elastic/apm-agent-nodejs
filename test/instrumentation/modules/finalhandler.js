f200ba42268dd098894bc73694197080cc38259a
Stephen Belanger <stephen.belanger@elastic.co>
2018-10-19 15:47:44 -0700

    feat(finalhandler): capture errors

---
+++ test/instrumentation/modules/finalhandler.js
@@ -0,0 +1,108 @@
+'use strict'
+
+var agent = require('../../..').start({
+  serviceName: 'test',
+  secretToken: 'test',
+  captureExceptions: true
+})
+
+var http = require('http')
+
+var express = require('express')
+var finalhandler = require('finalhandler')
+var test = require('tape')
+
+function makeTest (makeServer) {
+  return function (t) {
+    t.plan(7)
+
+    resetAgent(function (endpoint, headers, data, cb) {
+      t.equal(data.transactions.length, 1, 'has a transaction')
+
+      var trans = data.transactions[0]
+      t.equal(trans.name, 'GET /', 'transaction name is GET /')
+      t.equal(trans.type, 'request', 'transaction type is request')
+    })
+
+    var request
+    var error = new Error('wat')
+    var captureError = agent.captureError
+    agent.captureError = function (err, data) {
+      t.equal(err, error, 'has the expected error')
+      t.ok(data, 'captured data with error')
+      t.equal(data.request, request, 'captured data has the request object')
+    }
+    t.on('end', function () {
+      agent.captureError = captureError
+    })
+
+    var server = makeServer(error, req => {
+      request = req
+    })
+
+    server.listen(function () {
+      var port = server.address().port
+      http.get(`http://localhost:${port}`, res => {
+        t.equal(res.statusCode, 500)
+        res.resume()
+        res.on('end', () => {
+          server.close()
+          agent.flush()
+        })
+      })
+    })
+  }
+}
+
+test('basic http', makeTest((error, setRequest) => {
+  return http.createServer((req, res) => {
+    var done = finalhandler(req, res)
+    agent.setTransactionName('GET /')
+    setRequest(req)
+    done(error)
+  })
+}))
+
+test('express done', makeTest((error, setRequest) => {
+  var app = express()
+
+  app.get('/', (req, res, next) => {
+    setRequest(req)
+    next(error)
+  })
+
+  return http.createServer(app)
+}))
+
+test('express throw', makeTest((error, setRequest) => {
+  var app = express()
+
+  app.get('/', (req, res, next) => {
+    setRequest(req)
+    throw error
+  })
+
+  return http.createServer(app)
+}))
+
+test('express with error handler', makeTest((error, setRequest) => {
+  var app = express()
+
+  app.get('/', (req, res, next) => {
+    setRequest(req)
+    next(error)
+  })
+
+  app.use((error, req, res, next) => {
+    res.status(500).json({ error: error.message })
+  })
+
+  return http.createServer(app)
+}))
+
+function resetAgent (cb) {
+  agent._instrumentation._queue._clear()
+  agent._instrumentation.currentTransaction = null
+  agent._httpClient = { request: cb || function () { } }
+  agent.captureError = function (err) { throw err }
+}
