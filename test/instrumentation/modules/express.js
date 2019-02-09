06fc1658345b5bfb93236aba2615a2c46713677a
Stephen Belanger <admin@stephenbelanger.com>
2019-02-09 13:27:16 -0800

    fix(express): ensure correct transaction names (#842)
    Fixes #443

--- test/instrumentation/modules/express.js
+++ test/instrumentation/modules/express.js
@@ -14,7 +14,7 @@ var test = require('tape')
 var mockClient = require('../../_mock_http_client')
 
 test('error intercept', function (t) {
-  t.plan(7)
+  t.plan(8)
 
   resetAgent(function (data) {
     t.equal(data.transactions.length, 1, 'has a transaction')
@@ -49,30 +49,18 @@ test('error intercept', function (t) {
   })
 
   var server = app.listen(function () {
-    var port = server.address().port
-    var opts = {
-      method: 'GET',
-      port: port,
-      path: '/'
-    }
-    var req = http.request(opts, function (res) {
-      var chunks = []
-      res.on('data', chunks.push.bind(chunks))
-      res.on('end', function () {
-        const body = Buffer.concat(chunks).toString()
-        t.equal(body, JSON.stringify({
-          error: error.message
-        }), 'got correct body from error handler middleware')
-        server.close()
-        agent.flush()
-      })
+    get(server, '/', (err, body) => {
+      t.error(err)
+      const expected = JSON.stringify({ error: error.message })
+      t.equal(body, expected, 'got correct body from error handler middleware')
+      server.close()
+      agent.flush()
     })
-    req.end()
   })
 })
 
 test('ignore 404 errors', function (t) {
-  t.plan(4)
+  t.plan(5)
 
   resetAgent(function (data) {
     t.equal(data.transactions.length, 1, 'has a transaction')
@@ -98,28 +86,17 @@ test('ignore 404 errors', function (t) {
   })
 
   var server = app.listen(function () {
-    var port = server.address().port
-    var opts = {
-      method: 'GET',
-      port: port,
-      path: '/'
-    }
-    var req = http.request(opts, function (res) {
-      var chunks = []
-      res.on('data', chunks.push.bind(chunks))
-      res.on('end', function () {
-        const body = Buffer.concat(chunks).toString()
-        t.equal(body, 'not found', 'got correct body from error handler middleware')
-        server.close()
-        agent.flush()
-      })
+    get(server, '/', (err, body) => {
+      t.error(err)
+      t.equal(body, 'not found', 'got correct body from error handler middleware')
+      server.close()
+      agent.flush()
     })
-    req.end()
   })
 })
 
 test('ignore invalid errors', function (t) {
-  t.plan(4)
+  t.plan(5)
 
   resetAgent(function (data) {
     t.equal(data.transactions.length, 1, 'has a transaction')
@@ -149,25 +126,111 @@ test('ignore invalid errors', function (t) {
   })
 
   var server = app.listen(function () {
-    var port = server.address().port
-    var opts = {
-      method: 'GET',
-      port: port,
-      path: '/'
-    }
-    var req = http.request(opts, function (res) {
-      var chunks = []
-      res.on('data', chunks.push.bind(chunks))
-      res.on('end', function () {
-        const body = Buffer.concat(chunks).toString()
-        t.equal(body, 'done', 'got correct body from error handler middleware')
-        server.close()
-        agent.flush()
-      })
+    get(server, '/', (err, body) => {
+      t.error(err)
+      t.equal(body, 'done', 'got correct body from error handler middleware')
+      server.close()
+      agent.flush()
     })
-    req.end()
   })
 })
+
+test('do not inherit past route names', function (t) {
+  t.plan(5)
+
+  resetAgent(function (data) {
+    t.equal(data.transactions.length, 1, 'has a transaction')
+
+    var trans = data.transactions[0]
+    t.equal(trans.name, 'GET /', 'transaction name is GET /')
+    t.equal(trans.type, 'request', 'transaction type is request')
+  })
+
+  var captureError = agent.captureError
+  agent.captureError = function (_, data) {
+    t.fail('should not capture invalid errors')
+  }
+  t.on('end', function () {
+    agent.captureError = captureError
+  })
+
+  var app = express()
+  app.set('env', 'production')
+
+  app.get('/', function (req, res, next) {
+    req.message = 'done'
+    next()
+  })
+
+  app.use(function (req, res) {
+    res.status(200).send(req.message)
+  })
+
+  var server = app.listen(function () {
+    get(server, '/', (err, body) => {
+      t.error(err)
+      t.equal(body, 'done', 'got correct body from error handler middleware')
+      server.close()
+      agent.flush()
+    })
+  })
+})
+
+test('sub-routers include base path', function (t) {
+  t.plan(5)
+
+  resetAgent(function (data) {
+    t.equal(data.transactions.length, 1, 'has a transaction')
+
+    var trans = data.transactions[0]
+    t.equal(trans.name, 'GET /hello/:name', 'transaction name is GET /hello/:name')
+    t.equal(trans.type, 'request', 'transaction type is request')
+  })
+
+  var captureError = agent.captureError
+  agent.captureError = function (_, data) {
+    t.fail('should not capture invalid errors')
+  }
+  t.on('end', function () {
+    agent.captureError = captureError
+  })
+
+  var router = express.Router()
+  router.get('/:name', (req, res) => {
+    res.end(`hello, ${req.params.name}`)
+  })
+
+  var app = express()
+  app.set('env', 'production')
+  app.use('/hello', router)
+
+  var server = app.listen(function () {
+    get(server, '/hello/world', (err, body) => {
+      t.error(err)
+      t.equal(body, 'hello, world', 'got correct body')
+      server.close()
+      agent.flush()
+    })
+  })
+})
+
+function get (server, path, cb) {
+  var port = server.address().port
+  var opts = {
+    method: 'GET',
+    port: port,
+    path
+  }
+  var req = http.request(opts, function (res) {
+    var chunks = []
+    res.setEncoding('utf8')
+    res.on('error', cb)
+    res.on('data', chunks.push.bind(chunks))
+    res.on('end', () => cb(null, chunks.join('')))
+  })
+  req.on('error', cb)
+  req.end()
+}
 
 function resetAgent (cb) {
   agent._instrumentation.currentTransaction = null
