41993e67c5faceafc08a39448cfa51d7a553d2ae
Thomas Watson <w@tson.dk>
2019-03-25 22:58:34 +0100

    fix(ts): move inlined types into namespaces

--- index.d.ts
+++ index.d.ts
@@ -12,9 +12,9 @@ declare class Agent implements Taggable, StartSpanFn {
   isStarted (): boolean;
 
   // Data collection hooks
-  middleware: { connect (): ErrorHandleFunction };
-  lambda (handler: Handler): Handler;
-  lambda (type: string, handler: Handler): Handler;
+  middleware: { connect (): Connect.ErrorHandleFunction };
+  lambda (handler: AwsLambda.Handler): AwsLambda.Handler;
+  lambda (type: string, handler: AwsLambda.Handler): AwsLambda.Handler;
   handleUncaughtExceptions (
     fn: (err: Error) => void
   ): void;
@@ -210,71 +210,69 @@ interface StartSpanFn {
 }
 
 // Inlined from @types/aws-lambda - start
+declare namespace AwsLambda {
+  interface CognitoIdentity {
+    cognitoIdentityId: string;
+    cognitoIdentityPoolId: string;
+  }
 
-interface CognitoIdentity {
-  cognitoIdentityId: string;
-  cognitoIdentityPoolId: string;
+  interface ClientContext {
+    client: ClientContextClient;
+    custom?: any;
+    env: ClientContextEnv;
+  }
+
+  interface ClientContextClient {
+    installationId: string;
+    appTitle: string;
+    appVersionName: string;
+    appVersionCode: string;
+    appPackageName: string;
+  }
+
+  interface ClientContextEnv {
+    platformVersion: string;
+    platform: string;
+    make: string;
+    model: string;
+    locale: string;
+  }
+
+  type Callback<TResult = any> = (error?: Error | null | string, result?: TResult) => void;
+
+  interface Context {
+    // Properties
+    callbackWaitsForEmptyEventLoop: boolean;
+    functionName: string;
+    functionVersion: string;
+    invokedFunctionArn: string;
+    memoryLimitInMB: number;
+    awsRequestId: string;
+    logGroupName: string;
+    logStreamName: string;
+    identity?: CognitoIdentity;
+    clientContext?: ClientContext;
+
+    // Functions
+    getRemainingTimeInMillis(): number;
+
+    // Functions for compatibility with earlier Node.js Runtime v0.10.42
+    // For more details see http://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-using-old-runtime.html#nodejs-prog-model-oldruntime-context-methods
+    done(error?: Error, result?: any): void;
+    fail(error: Error | string): void;
+    succeed(messageOrObject: any): void;
+    succeed(message: string, object: any): void;
+  }
+
+  type Handler<TEvent = any, TResult = any> = (
+    event: TEvent,
+    context: Context,
+    callback: Callback<TResult>,
+  ) => void | Promise<TResult>;
 }
-
-interface ClientContext {
-  client: ClientContextClient;
-  custom?: any;
-  env: ClientContextEnv;
-}
-
-interface ClientContextClient {
-  installationId: string;
-  appTitle: string;
-  appVersionName: string;
-  appVersionCode: string;
-  appPackageName: string;
-}
-
-interface ClientContextEnv {
-  platformVersion: string;
-  platform: string;
-  make: string;
-  model: string;
-  locale: string;
-}
-
-type Callback<TResult = any> = (error?: Error | null | string, result?: TResult) => void;
-
-interface Context {
-  // Properties
-  callbackWaitsForEmptyEventLoop: boolean;
-  functionName: string;
-  functionVersion: string;
-  invokedFunctionArn: string;
-  memoryLimitInMB: number;
-  awsRequestId: string;
-  logGroupName: string;
-  logStreamName: string;
-  identity?: CognitoIdentity;
-  clientContext?: ClientContext;
-
-  // Functions
-  getRemainingTimeInMillis(): number;
-
-  // Functions for compatibility with earlier Node.js Runtime v0.10.42
-  // For more details see http://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-using-old-runtime.html#nodejs-prog-model-oldruntime-context-methods
-  done(error?: Error, result?: any): void;
-  fail(error: Error | string): void;
-  succeed(messageOrObject: any): void;
-  succeed(message: string, object: any): void;
-}
-
-type Handler<TEvent = any, TResult = any> = (
-  event: TEvent,
-  context: Context,
-  callback: Callback<TResult>,
-) => void | Promise<TResult>;
-
-// Inlined from @types/aws-lambda - end
 
 // Inlined from @types/connect - start
-
-type NextFunction = (err?: any) => void;
-type ErrorHandleFunction = (err: any, req: IncomingMessage, res: ServerResponse, next: NextFunction) => void;
-
-// Inlined from @types/connect - end
+declare namespace Connect {
+  type NextFunction = (err?: any) => void;
+  type ErrorHandleFunction = (err: any, req: IncomingMessage, res: ServerResponse, next: NextFunction) => void;
+}
