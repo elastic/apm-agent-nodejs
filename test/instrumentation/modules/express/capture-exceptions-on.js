ab6f75a321069ecbe9f101a07611b1b8e3350cfd
Thomas Watson <w@tson.dk>
2019-06-11 22:08:19 +0200

    fix(express): don't swallow error handling middleware (#1111)
    If `captureExceptions` was set to `false` and error was thrown inside of
    an Express application, any user-provided error handling middleware would
    not be called.
    
    Fixes #1007

---
+++ test/instrumentation/modules/express/capture-exceptions-on.js
@@ -0,0 +1,53 @@
+'use strict'
+
+const agent = require('../../../..').start({
+  captureExceptions: true,
+  metricsInterval: 0
+})
+
+const { exec } = require('child_process')
+
+const express = require('express')
+const test = require('tape')
+
+const mockClient = require('../../../_mock_http_client')
+
+test('use given error handler middleware if captureExceptions: true', function (t) {
+  t.plan(7)
+
+  onAPMData(function (data) {
+    t.equal(data.transactions.length, 1, 'has a transaction')
+    t.equal(data.errors.length, 1, 'has an error')
+    t.equal(data.errors[0].exception.message, err.message)
+  })
+
+  const err = new Error('foo')
+
+  const app = express()
+  app.get('/', function (req, res) {
+    throw err
+  })
+  app.use(function (_err, req, res, next) {
+    t.equal(_err, err)
+    res.send('hello from error handler')
+  })
+
+  const server = app.listen(function () {
+    get(server, (err, stdout, stderr) => {
+      t.error(err)
+      t.equal(stdout, 'hello from error handler')
+      t.equal(stderr, '')
+      server.close()
+    })
+  })
+})
+
+function get (server, cb) {
+  // use curl instead of http.get so the agent doesn't try to track the outgoing request
+  // -s: Silent mode. Don't output progress on stderr
+  exec(`curl -s localhost:${server.address().port}`, cb)
+}
+
+function onAPMData (cb) {
+  agent._transport = mockClient(2, cb)
+}
