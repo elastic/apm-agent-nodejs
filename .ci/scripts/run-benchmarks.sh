88752eedf66152c6e1052ab47c74f240cae54859
Victor Martinez <VictorMartinezRubio@gmail.com>
2019-06-20 15:03:06 +0100

    ci(jenkins): nvm is already provided in the bare metal server: https://github.com/elastic/infra/blob/master/ansible/roles/apm_deps/meta/main.yml

--- .ci/scripts/run-benchmarks.sh
+++ .ci/scripts/run-benchmarks.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -xueo pipefail
 
+# This particular configuration is required to be installed in the baremetal
+nvm install node
+nvm ls-remote
+nvm use node
+
 npm config list
 npm install
 
