0a04001b86b1fa52bd4cf7fd7e62e8d628a5fa23
Victor Martinez <VictorMartinezRubio@gmail.com>
2019-06-20 15:14:42 +0100

    ci(jenkins): nvm is not provided by default, let's install it within the ci script

--- .ci/scripts/run-benchmarks.sh
+++ .ci/scripts/run-benchmarks.sh
@@ -2,6 +2,11 @@
 set -xueo pipefail
 
 # This particular configuration is required to be installed in the baremetal
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+command -v nvm
+
 nvm install node
 nvm ls-remote
 nvm use node
