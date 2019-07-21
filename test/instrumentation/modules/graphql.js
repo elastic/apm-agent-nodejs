33091c3d40b6b4172b95e976b1518c20149277e7
Sibelius Seraphini <sibeliusseraphini@gmail.com>
2019-07-21 10:17:26 -0300

    fix(test): fix trans.type on tests

--- test/instrumentation/modules/graphql.js
+++ test/instrumentation/modules/graphql.js
@@ -67,7 +67,7 @@ test('graphql.graphql - transaction ended', function (t) {
     var trans = data.transactions[0]
 
     t.equal(trans.name, 'foo')
-    t.equal(trans.type, 'custom')
+    t.equal(trans.type, 'graphql')
   })
 
   var schema = graphql.buildSchema('type Query { hello: String }')
@@ -117,7 +117,7 @@ test('graphql.execute - transaction ended', function (t) {
     var trans = data.transactions[0]
 
     t.equal(trans.name, 'foo')
-    t.equal(trans.type, 'custom')
+    t.equal(trans.type, 'graphql')
   })
 
   var schema = graphql.buildSchema('type Query { hello: String }')
@@ -199,7 +199,7 @@ function done (t, spanNameSuffix) {
     var span = data.spans[0]
 
     t.equal(trans.name, 'foo')
-    t.equal(trans.type, 'custom')
+    t.equal(trans.type, 'graphql')
     t.equal(span.name, 'GraphQL: ' + spanNameSuffix)
     t.equal(span.type, 'db.graphql.execute')
 
