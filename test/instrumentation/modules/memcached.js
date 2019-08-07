58399b8085c1127d684b9d457c260a9ff5271358
Michael Fautley <michael.f@superbalist.com>
2019-08-07 11:23:55 +0200

    fix: sort memcached test spans

--- test/instrumentation/modules/memcached.js
+++ test/instrumentation/modules/memcached.js
@@ -13,27 +13,32 @@ test(function (t) {
   resetAgent(function (data) {
     t.equal(data.transactions.length, 1)
     t.equal(data.spans.length, 7)
-    t.equal(data.spans[0].name, 'memcached.set')
-    t.equal(data.spans[0].type, 'db.memcached.set')
-    t.equal(data.spans[0].context.db.statement, 'set foo')
-    t.equal(data.spans[1].name, 'memcached.get')
-    t.equal(data.spans[1].type, 'db.memcached.get')
-    t.equal(data.spans[1].context.db.statement, 'get foo')
-    t.equal(data.spans[2].name, 'memcached.replace')
-    t.equal(data.spans[2].type, 'db.memcached.replace')
-    t.equal(data.spans[2].context.db.statement, 'replace foo')
-    t.equal(data.spans[3].name, 'memcached.get')
-    t.equal(data.spans[3].type, 'db.memcached.get')
-    t.equal(data.spans[3].context.db.statement, 'get foo')
-    t.equal(data.spans[4].name, 'memcached.touch')
-    t.equal(data.spans[4].type, 'db.memcached.touch')
-    t.equal(data.spans[4].context.db.statement, 'touch foo')
-    t.equal(data.spans[5].name, 'memcached.delete')
-    t.equal(data.spans[5].type, 'db.memcached.delete')
-    t.equal(data.spans[5].context.db.statement, 'delete foo')
-    t.equal(data.spans[6].name, 'memcached.get')
-    t.equal(data.spans[6].type, 'db.memcached.get')
-    t.equal(data.spans[6].context.db.statement, 'get foo')
+
+    const spans = data.spans.sort((a, b) => {
+      return a.timestamp - b.timestamp
+    })
+
+    t.equal(spans[0].name, 'memcached.set')
+    t.equal(spans[0].type, 'db.memcached.set')
+    t.equal(spans[0].context.db.statement, 'set foo')
+    t.equal(spans[1].name, 'memcached.get')
+    t.equal(spans[1].type, 'db.memcached.get')
+    t.equal(spans[1].context.db.statement, 'get foo')
+    t.equal(spans[2].name, 'memcached.replace')
+    t.equal(spans[2].type, 'db.memcached.replace')
+    t.equal(spans[2].context.db.statement, 'replace foo')
+    t.equal(spans[3].name, 'memcached.get')
+    t.equal(spans[3].type, 'db.memcached.get')
+    t.equal(spans[3].context.db.statement, 'get foo')
+    t.equal(spans[4].name, 'memcached.touch')
+    t.equal(spans[4].type, 'db.memcached.touch')
+    t.equal(spans[4].context.db.statement, 'touch foo')
+    t.equal(spans[5].name, 'memcached.delete')
+    t.equal(spans[5].type, 'db.memcached.delete')
+    t.equal(spans[5].context.db.statement, 'delete foo')
+    t.equal(spans[6].name, 'memcached.get')
+    t.equal(spans[6].type, 'db.memcached.get')
+    t.equal(spans[6].context.db.statement, 'get foo')
     t.end()
   })
   var Memcached = require('memcached')
@@ -68,7 +73,7 @@ test(function (t) {
   })
 })
 
-function resetAgent (cb) {
+function resetAgent(cb) {
   agent._instrumentation.currentTransaction = null
   agent._transport = mockClient(8, cb)
   agent.captureError = function (err) { throw err }
