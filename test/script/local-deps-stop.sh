6096fbdcf935b3cd2ac668bec7bdebb7aca94f9f
mikefaut <30619604+mikefaut@users.noreply.github.com>
2019-08-23 10:41:27 +0200

    feat(memcached): instrument memcached v2.2.0 and above (#1144)

--- test/script/local-deps-stop.sh
+++ test/script/local-deps-stop.sh
@@ -4,5 +4,6 @@ pg_ctl -D /usr/local/var/postgres stop
 kill `cat /tmp/mongod.pid`
 kill `cat /tmp/elasticsearch.pid`
 kill `cat /tmp/cassandra.pid`
+kill `cat /tmp/memcached.pid`
 redis-cli shutdown
 mysql.server stop
