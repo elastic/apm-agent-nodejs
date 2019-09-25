30bc58d739137b8e13eaf033661e9928f024b015
Victor Martinez <VictorMartinezRubio@gmail.com>
2019-09-25 16:06:45 +0100

    chore: let's print the env

--- test/script/appveyor/install-java.ps1
+++ test/script/appveyor/install-java.ps1
@@ -1,5 +1,8 @@
 # Abort with non zero exit code on errors
 $ErrorActionPreference = "Stop"
 
+Write-Host "Environment..." -ForegroundColor Cyan
+Get-ChildItem Env: | Sort Name
+
 Write-Host "Installing java..."
 & choco install jdk8 -y --no-progress -r --version 8.0.211
