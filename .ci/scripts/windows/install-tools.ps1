49ac3c6808f73e907d2a163748a03a3e4bda3d05
Victor Martinez <VictorMartinezRubio@gmail.com>
2019-09-30 14:35:40 +0100

    ci(jenkins): env list not required

--- .ci/scripts/windows/install-tools.ps1
+++ .ci/scripts/windows/install-tools.ps1
@@ -1,8 +1,6 @@
 # Abort with non zero exit code on errors
 $ErrorActionPreference = "Stop"
 
-Get-ChildItem Env: | Sort Name | Format-Table -Wrap -AutoSize
-
 Write-Host "Getting latest Nodejs version..."
 $Version = $(choco list nodejs --by-id-only --all) | Select-String -Pattern "nodejs $env:VERSION" | %{$_.ToString().split(" ")[1]} | sort {[version] $_} | Select-Object -Last 1
 
