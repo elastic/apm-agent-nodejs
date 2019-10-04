7ebcf97c2a1431a9d1e2b4d022d802ba4ab8ecf9
Victor Martinez <VictorMartinezRubio@gmail.com>
2019-10-04 15:37:30 +0100

    ci(jenkins): skip reboot

--- .ci/scripts/windows/install-tools.ps1
+++ .ci/scripts/windows/install-tools.ps1
@@ -8,4 +8,4 @@ Write-Host "Installing Nodejs..."
 & choco install nodejs --no-progress -y --version "$Version"
 
 Write-Host "Installing JDK8..."
-& choco install jdk8 --no-progress -y --force -params 'installdir=c:\\java'
+& choco install jdk8 --no-progress -y --force --ignore-detected-reboot -params 'installdir=c:\\java'
