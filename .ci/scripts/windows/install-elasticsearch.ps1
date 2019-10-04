88154f75208c4a1e1414e70c095b19ce2ae965e9
Victor Martinez <VictorMartinezRubio@gmail.com>
2019-10-04 14:44:00 +0100

    Revert "ci(jenkins): without the infra"
    This reverts commit 42d8c2d5128339362dce3c673b5282948a80f5a3.

---
+++ .ci/scripts/windows/install-elasticsearch.ps1
@@ -0,0 +1,31 @@
+# Abort with non zero exit code on errors
+$ErrorActionPreference = "Stop"
+
+Write-Host "Preparing to download and install Elasticsearch..." -ForegroundColor Cyan
+$esVersion = "7.3.2"
+$downloadUrl = "https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-$($esVersion)-windows-x86_64.zip"
+$zipPath = "$($env:USERPROFILE)\elasticsearch-$esVersion.zip"
+$extractRoot = "$env:SYSTEMDRIVE\Elasticsearch"
+$esRoot = "$extractRoot\elasticsearch-$esVersion"
+[Environment]::SetEnvironmentVariable("JAVA_HOME",$null,"User")
+Write-Host "Environment..." -ForegroundColor Cyan
+Get-ChildItem Env: | Sort Name | Format-Table -Wrap -AutoSize
+
+Write-Host "Downloading Elasticsearch..."
+(New-Object Net.WebClient).DownloadFile($downloadUrl, $zipPath)
+7z x $zipPath -y -o"$extractRoot" | Out-Null
+del $zipPath
+Get-ChildItem $esRoot | where {$_.Attributes -match'Directory'}
+
+Write-Host "Installing Elasticsearch as a Windows service..."
+& "$esRoot\bin\elasticsearch-service.bat" install
+
+Write-Host "Starting Elasticsearch service..."
+& "$esRoot\bin\elasticsearch-service.bat" start
+
+do {
+  Write-Host "Waiting for Elasticsearch service to bootstrap..."
+  sleep 1
+} until(Test-NetConnection localhost -Port 9200 | ? { $_.TcpTestSucceeded } )
+
+Write-Host "Elasticsearch installed" -ForegroundColor Green
