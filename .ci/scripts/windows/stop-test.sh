dd2a260c0726760f09158a2e72c31836b714fee0
Victor Martinez <VictorMartinezRubio@gmail.com>
2019-12-15 11:37:09 +0000

    ci(jenkins): redirect logs to a file

--- .ci/scripts/windows/stop-test.sh
+++ .ci/scripts/windows/stop-test.sh
@@ -5,6 +5,12 @@ NODE_VERSION=${1:?Nodejs version missing NODE_VERSION is not set}
 
 NODE_VERSION=${NODE_VERSION} docker-compose \
   --no-ansi \
+  -f .ci/docker/docker-compose-all.yml \
+  logs \
+  --timestamps > docker-compose-logs.txt
+
+NODE_VERSION=${NODE_VERSION} docker-compose \
+  --no-ansi \
   --log-level ERROR \
   -f .ci/docker/docker-compose-all.yml \
   down -v
