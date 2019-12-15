c21dc480331d7a5a6e808d1be65442facb8cd7e2
Victor Martinez <VictorMartinezRubio@gmail.com>
2019-12-15 13:29:20 +0000

    ci(jenkins): docker for windows unrequired

--- .ci/scripts/windows/docker/postgres/entrypoint.ps1
+++
@@ -1,8 +0,0 @@
-Write-Host "Wait a few seconds until the postgres is ready..."
-sleep 5
-Write-Host "Create users for remote access..."
-psql -U postgres -c "CREATE ROLE jenkins WITH LOGIN SUPERUSER INHERIT CREATEDB CREATEROLE REPLICATION;"
-
-Write-Host "Show postgres log output in the console..."
-# Keep running the container with the event logs in the stdout.
-Get-Content log/postgresql.log -Wait
