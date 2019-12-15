dcb584c46c7ad3a5ccb938caa6ffea54295ce924
Victor Martinez <VictorMartinezRubio@gmail.com>
2019-12-15 11:41:21 +0000

    ci(jenkins): otherwise uses nodejs.commandline

--- .ci/scripts/windows/install-tools.ps1
+++ .ci/scripts/windows/install-tools.ps1
@@ -5,12 +5,8 @@ Write-Host "List environment variables ..."
 Get-ChildItem Env: | Sort Name | Format-Table -Wrap -AutoSize
 
 Write-Host("Getting latest Nodejs version for {0} ..." -f $env:VERSION)
-choco list nodejs --by-id-only --all
-choco list nodejs --by-id-only --all | Select-String -Pattern "nodejs $env:VERSION"
-& choco list nodejs --by-id-only --all
-& choco list nodejs --by-id-only --all | Select-String -Pattern "nodejs $env:VERSION"
-
-$Version = $(choco list nodejs --by-id-only --all) | Select-String -Pattern "nodejs $env:VERSION" | %{$_.ToString().split(" ")[1]} | sort {[version] $_} | Select-Object -Last 1
+& choco list nodejs --exact --by-id-only --all | Select-String -Pattern "nodejs $env:VERSION"
+$Version = $(choco list nodejs --exact --by-id-only --all) | Select-String -Pattern "nodejs $env:VERSION" | %{$_.ToString().split(" ")[1]} | sort {[version] $_} | Select-Object -Last 1
 
 Write-Host("Installing Nodejs {0} ..." -f $Version)
 & choco install nodejs --no-progress -y --version "$Version"
