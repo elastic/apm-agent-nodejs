731f14476b2ba8c5921550dc6bb2ac4a8daa4bc7
Victor Martinez <VictorMartinezRubio@gmail.com>
2019-12-15 10:35:51 +0000

    ci(jenkins): run docker services in linux

---
+++ .ci/scripts/windows/stop-test.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -exo pipefail
+
+NODE_VERSION=${1:?Nodejs version missing NODE_VERSION is not set}
+
+NODE_VERSION=${NODE_VERSION} docker-compose \
+  --no-ansi \
+  --log-level ERROR \
+  -f .ci/docker/docker-compose-all.yml \
+  down -v
