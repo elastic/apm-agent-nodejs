01c747ccdce6c559efc3a7a2b90be68ece0f285f
Victor Martinez <VictorMartinezRubio@gmail.com>
2019-12-15 10:40:02 +0000

    ci(jenkins): add verbose for the install in windows

--- .ci/scripts/windows/install-tools.ps1
+++ .ci/scripts/windows/install-tools.ps1
@@ -1,11 +1,12 @@
 # Abort with non zero exit code on errors
 $ErrorActionPreference = "Stop"
 
-Write-Host "List environment variables..."
+Write-Host "List environment variables ..."
 Get-ChildItem Env: | Sort Name | Format-Table -Wrap -AutoSize
 
-Write-Host "Getting latest Nodejs version..."
+Write-Host("Getting latest Nodejs version for {0} ..." -f $env:VERSION)
+choco list nodejs --by-id-only --all | Select-String -Pattern "nodejs $env:VERSION"
 $Version = $(choco list nodejs --by-id-only --all) | Select-String -Pattern "nodejs $env:VERSION" | %{$_.ToString().split(" ")[1]} | sort {[version] $_} | Select-Object -Last 1
 
-Write-Host "Installing Nodejs..."
+Write-Host("Installing Nodejs {0} ..." -f $Version)
 & choco install nodejs --no-progress -y --version "$Version"
