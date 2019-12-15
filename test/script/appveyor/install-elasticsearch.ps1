324c61817f2f71f99b38461bdacbc2249878eedd
Victor Martinez <VictorMartinezRubio@gmail.com>
2019-12-15 15:37:16 +0000

    ci(jenkins): reset scripts to master version

--- test/script/appveyor/install-elasticsearch.ps1
+++ test/script/appveyor/install-elasticsearch.ps1
@@ -2,20 +2,16 @@
 $ErrorActionPreference = "Stop"
 
 Write-Host "Preparing to download and install Elasticsearch..." -ForegroundColor Cyan
-$esVersion = "7.3.2"
-$downloadUrl = "https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-$($esVersion)-windows-x86_64.zip"
+$esVersion = "6.1.2"
+$downloadUrl = "https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-$($esVersion).zip"
 $zipPath = "$($env:USERPROFILE)\elasticsearch-$esVersion.zip"
 $extractRoot = "$env:SYSTEMDRIVE\Elasticsearch"
 $esRoot = "$extractRoot\elasticsearch-$esVersion"
-[Environment]::SetEnvironmentVariable("JAVA_HOME",$null,"User")
-Write-Host "Environment..." -ForegroundColor Cyan
-Get-ChildItem Env: | Sort Name | Format-Table -Wrap -AutoSize
 
 Write-Host "Downloading Elasticsearch..."
 (New-Object Net.WebClient).DownloadFile($downloadUrl, $zipPath)
 7z x $zipPath -y -o"$extractRoot" | Out-Null
 del $zipPath
-Get-ChildItem $esRoot | where {$_.Attributes -match'Directory'}
 
 Write-Host "Installing Elasticsearch as a Windows service..."
 & "$esRoot\bin\elasticsearch-service.bat" install
--- test/script/appveyor/install-redis.ps1
+++ test/script/appveyor/install-redis.ps1
@@ -1,14 +1,10 @@
 # Abort with non zero exit code on errors
 $ErrorActionPreference = "Stop"
 
-Write-Host "Preparing to download and install Redis..." -ForegroundColor Cyan
-$redisVersion = "2.8.19"
-$redisRoot = "$env:SYSTEMDRIVE\Redis"
-$zipPath = "$($env:USERPROFILE)\redis-$redisVersion.zip"
-$downloadUrl = "https://github.com/MSOpenTech/redis/releases/download/win-$redisVersion/redis-$redisVersion.zip"
-
 Write-Host "Downloading Redis..." -ForegroundColor Cyan
-(New-Object Net.WebClient).DownloadFile($downloadUrl, $zipPath)
+$redisRoot = "$env:SYSTEMDRIVE\Redis"
+$zipPath = "$($env:USERPROFILE)\redis-2.8.19.zip"
+(New-Object Net.WebClient).DownloadFile('https://github.com/MSOpenTech/redis/releases/download/win-2.8.19/redis-2.8.19.zip', $zipPath)
 7z x $zipPath -y -o"$redisRoot" | Out-Null
 del $zipPath
 
