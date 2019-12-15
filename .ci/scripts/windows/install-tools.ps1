8e8bbe1430e3f2e55c6fef7447193a232b8e2261
Victor Martinez <VictorMartinezRubio@gmail.com>
2019-12-15 11:08:39 +0000

    ci(jenkins): debug the ps1

--- .ci/scripts/windows/install-tools.ps1
+++ .ci/scripts/windows/install-tools.ps1
@@ -5,7 +5,11 @@ Write-Host "List environment variables ..."
 Get-ChildItem Env: | Sort Name | Format-Table -Wrap -AutoSize
 
 Write-Host("Getting latest Nodejs version for {0} ..." -f $env:VERSION)
+choco list nodejs --by-id-only --all
 choco list nodejs --by-id-only --all | Select-String -Pattern "nodejs $env:VERSION"
+& choco list nodejs --by-id-only --all
+& choco list nodejs --by-id-only --all | Select-String -Pattern "nodejs $env:VERSION"
+
 $Version = $(choco list nodejs --by-id-only --all) | Select-String -Pattern "nodejs $env:VERSION" | %{$_.ToString().split(" ")[1]} | sort {[version] $_} | Select-Object -Last 1
 
 Write-Host("Installing Nodejs {0} ..." -f $Version)
