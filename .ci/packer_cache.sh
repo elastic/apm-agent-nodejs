29c2288b8bbb7ef9c7410fe486a3b117b2a56548
Victor Martinez <VictorMartinezRubio@gmail.com>
2020-02-10 15:10:33 +0000

    ci(jenkins): retag cached docker image

--- .ci/packer_cache.sh
+++ .ci/packer_cache.sh
@@ -2,6 +2,16 @@
 
 source /usr/local/bin/bash_standard_lib.sh
 
+grep "-" .ci/.jenkins_nodejs.yml | cut -d'-' -f2- | \
+while read -r version;
+do
+    transformedVersion=$(echo "${version}" | sed 's#"##g' | cut -d":" -f2)
+    imageName="apm-agent-nodejs"
+    registryImageName="docker.elastic.co/observability-ci/${imageName}:${transformedVersion}"
+    (retry 2 docker pull "${registryImageName}")
+    docker tag "${registryImageName}" "node:${transformedVersion}"
+done
+
 docker-compose \
   --no-ansi \
   --log-level ERROR \
