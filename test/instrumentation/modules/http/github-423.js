d1c08fde9e3fd898bb273acf2ae9d1f093dee6ff
Thomas Watson <w@tson.dk>
2020-03-17 20:09:06 +0100

    more-debug

--- test/instrumentation/modules/http/github-423.js
+++ test/instrumentation/modules/http/github-423.js
@@ -15,15 +15,19 @@ var fs = require('fs')
 
 var got = require('got')
 var test = require('tape')
+var eventDebug = require('event-debug')
 
 var fileSize = fs.readFileSync(__filename, 'utf8').length
 
 test('https://github.com/elastic/apm-agent-nodejs/issues/423', function (t) {
   // Start dummy remote server to fetch gzip'ed data from
   var remote = http.createServer(function (req, res) {
+    eventDebug(req, 'server1: req')
+    eventDebug(res, 'server1: res')
     res.setHeader('Content-Encoding', 'gzip')
     fs.createReadStream(__filename).pipe(zlib.createGzip()).pipe(res)
   })
+  eventDebug(remote, 'server1')
 
   remote.listen(function () {
     var port = remote.address().port
@@ -31,23 +35,28 @@ test('https://github.com/elastic/apm-agent-nodejs/issues/423', function (t) {
 
     // Start simple server that performs got-request on every request
     var server = http.createServer(function (req, res) {
+      eventDebug(req, 'server2: req')
+      eventDebug(res, 'server2: res')
       got(url).then(function (response) {
         t.equal(response.body.length, fileSize, 'body should be expected size')
         t.equal(response.body.slice(0, 12), '\'use strict\'', 'body should be uncompressed')
         res.end()
       })
     })
+    eventDebug(remote, 'server2')
 
     server.listen(function () {
       var port = server.address().port
       var url = 'http://localhost:' + port
 
-      http.get(url, function (res) {
+      var req = http.get(url, function (res) {
+        eventDebug(res, 'outgoing req: res')
         res.resume()
         server.close()
         remote.close()
         t.end()
       })
+      eventDebug(req, 'outgoing req')
     })
   })
 })
