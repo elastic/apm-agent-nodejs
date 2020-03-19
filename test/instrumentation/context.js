be6f1382dc6a33e658c9e5d0da535295c7399aca
vigneshshanmugam <vignesh.shanmugam22@gmail.com>
2020-03-19 13:05:00 +0100

    feat: enrich spans with destination info

---
+++ test/instrumentation/context.js
@@ -0,0 +1,62 @@
+'use strict'
+
+var test = require('tape')
+var { getDestination } = require('../../lib/instrumentation/context')
+
+test('#getDestination', function (t) {
+  t.test('username and pass', (t) => {
+    const url = 'http://user:pass@testing.local:1234/path?query'
+    t.deepEqual(getDestination(url, 'external'), {
+      service: {
+        name: 'http://testing.local:1234',
+        resource: 'testing.local:1234',
+        type: 'external'
+      },
+      address: 'testing.local',
+      port: 1234
+    })
+    t.end()
+  })
+
+  t.test('https default port', (t) => {
+    const url = 'https://www.elastic.co:443/products/apm'
+    t.deepEqual(getDestination(url, 'external'), {
+      service: {
+        name: 'https://www.elastic.co',
+        resource: 'www.elastic.co:443',
+        type: 'external'
+      },
+      address: 'www.elastic.co',
+      port: 443
+    })
+    t.end()
+  })
+
+  t.test('ipv6', (t) => {
+    const url = 'http://[::1]'
+    t.deepEqual(getDestination(url, 'external'), {
+      service: {
+        name: 'http://[::1]',
+        resource: '[::1]:80',
+        type: 'external'
+      },
+      address: '::1',
+      port: 80
+    })
+    t.end()
+  })
+
+  t.test('ipv6 default scheme and port', (t) => {
+    const url = 'https://[::1]:80/'
+    t.deepEqual(getDestination(url, 'external'), {
+      service: {
+        name: 'https://[::1]:80',
+        resource: '[::1]:80',
+        type: 'external'
+      },
+      address: '::1',
+      port: 80
+    })
+    t.end()
+  })
+})
