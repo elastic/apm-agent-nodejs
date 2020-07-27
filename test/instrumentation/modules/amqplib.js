6c81e4afcec1b502c1b52fbb59726efa76540c2f
Jérémy Levilain <jeremy.levilain@epitech.eu>
2020-07-27 15:31:41 +0200

    feat(amqplib): add more test, prepare callback version support

--- test/instrumentation/modules/amqplib.js
+++ test/instrumentation/modules/amqplib.js
@@ -9,30 +9,93 @@ var agent = require('../../..').start({
 })
 
 var amqplib = require('amqplib')
+// var amqplibCallback = require('amqplib/callback_api')
 var test = require('tape')
 
 var mockClient = require('../../_mock_http_client')
 
-test('instrument simple message', function (t) {
+test('instrument simple message - publish method (promise)', function (t) {
   resetAgent(done(t))
 
   amqplib.connect(`amqp://${process.env.RABBITMQ_HOST || '127.0.0.1'}`).then(function (connection) {
-    agent.startTransaction('foo', 'bar')
-
     connection.createChannel().then(function (channel) {
-      channel.publish('foo', 'bar', Buffer.from('helloworld', 'utf8'))
+      channel.assertExchange('my_exchange', 'fanout', { durable: false }).then(function () {
+        agent.startTransaction('foo', 'bar')
+        channel.publish('my_exchange', 'my_routing_key', Buffer.from('helloworld', 'utf8'))
+        agent.endTransaction()
 
-      agent.endTransaction()
-
-      connection.close().then(function () {
-        agent.flush()
+        channel.close().then(function () {
+          connection.close().then(function () {
+            agent.flush()
+          })
+        })
       })
     })
   })
 })
 
+test('instrument simple message - sendToQueue method (promise)', function (t) {
+  resetAgent(done(t))
+
+  amqplib.connect(`amqp://${process.env.RABBITMQ_HOST || '127.0.0.1'}`).then(function (connection) {
+    connection.createChannel().then(function (channel) {
+      channel.assertQueue('my_queue', { durable: false }).then(function () {
+        agent.startTransaction('foo', 'bar')
+        channel.sendToQueue('my_queue', Buffer.from('helloworld', 'utf8'))
+        agent.endTransaction()
+
+        channel.close().then(function () {
+          connection.close().then(function () {
+            agent.flush()
+          })
+        })
+      })
+    })
+  })
+})
+
+// test('instrument simple message - publish method (callback)', function (t) {
+//   resetAgent(done(t))
+
+//   amqplibCallback.connect(`amqp://${process.env.RABBITMQ_HOST || '127.0.0.1'}`, function (err, connection) {
+//     connection.createChannel(function (err, channel) {
+//       channel.assertExchange('my_exchange', 'fanout', { durable: false }, function () {
+//         agent.startTransaction('foo', 'bar')
+//         channel.publish('my_exchange', 'my_routing_key', Buffer.from('helloworld', 'utf8'))
+//         agent.endTransaction()
+
+//         channel.close(function () {
+//           connection.close(function () {
+//             agent.flush()
+//           })
+//         })
+//       })
+//     })
+//   })
+// })
+
+// test('instrument simple message - sendToQueue method (callback)', function (t) {
+//   resetAgent(done(t))
+
+//   amqplibCallback.connect(`amqp://${process.env.RABBITMQ_HOST || '127.0.0.1'}`, function (err, connection) {
+//     connection.createChannel(function (err, channel) {
+//       channel.assertQueue('my_queue', { durable: false }, function (err) {
+//         agent.startTransaction('foo', 'bar')
+//         channel.sendToQueue('my_queue', Buffer.from('helloworld', 'utf8'))
+//         agent.endTransaction()
+
+//         channel.close(function (err) {
+//           connection.close(function (err) {
+//             agent.flush()
+//           })
+//         })
+//       })
+//     })
+//   })
+// })
+
 function done (t) {
-  return function (data, cb) {
+  return function (data) {
     t.strictEqual(data.transactions.length, 1)
     t.strictEqual(data.spans.length, 1)
 
