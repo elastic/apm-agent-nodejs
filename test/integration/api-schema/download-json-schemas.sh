6f4c2fdaf808f8223b1a0a278d21e8bde5c145de
Alan Storm <astorm@users.noreply.github.com>
2020-11-09 11:14:15 -0800

    fix: remove gtar dep (#1862)
    * fix: remove gtar dep
    
    * fix: clarifying usage
    
    * fix: best nit ever

--- test/integration/api-schema/download-json-schemas.sh
+++ test/integration/api-schema/download-json-schemas.sh
@@ -1,4 +1,3 @@
-
 #!/usr/bin/env bash
 
 set -ex
@@ -6,16 +5,20 @@ if ! [ -n "$1" ]; then
     echo "USAGE: ./download-json-schemas.sh /path/to/folder"
     echo ""
     echo "Downloads the APM Server Schema files to the specified folder"
+    echo ""
+    echo "Sources from: https://codeload.github.com/elastic/apm-server/"
     exit 1
 fi
 
+# download_schema </path/to/download/to> <branch>
 download_schema()
 {
     rm -rf ${1} && mkdir -p ${1}
     for run in 1 2 3 4 5
     do
-        if [ -x "$(command -v gtar)" ]; then
-            curl --silent --fail https://codeload.github.com/elastic/apm-server/tar.gz/${2} | gtar xzvf - --wildcards --directory=${1} --strip-components=1 "*/docs/spec/*"
+        if [[ "$OSTYPE" == "darwin"* ]]; then
+            # MacOS tar doesn't need --wildcards, that's how it behaves by default
+            curl --silent --fail https://codeload.github.com/elastic/apm-server/tar.gz/${2} | tar xzvf - --directory=${1} --strip-components=1 "*/docs/spec/*"
         else
             curl --silent --fail https://codeload.github.com/elastic/apm-server/tar.gz/${2} | tar xzvf - --wildcards --directory=${1} --strip-components=1 "*/docs/spec/*"
         fi
