ea2df4ef97de7bab8e98b2af2d9237f09a771a0b
Alan Storm <alan.storm@elastic.co>
2020-11-29 21:43:40 -0800

    fix: inevitable lint

--- test/sanitize-field-names.js
+++ test/sanitize-field-names.js
@@ -6,25 +6,25 @@ const {
   removeKeysFromPostedFormVariables
 } = require('../lib/filters/sanitize-field-names')
 
-test('removeKeysFromObject unit tests',function(t){
+test('removeKeysFromObject unit tests', function (t) {
   t.ok(removeKeysFromObject, 'can import function')
 
   const obj1 = {
-    'one':'two',
-    'three':'four',
-    'five':'six'
+    one: 'two',
+    three: 'four',
+    five: 'six'
   }
-  removeKeysFromObject(obj1,[/th.*ee/])
+  removeKeysFromObject(obj1, [/th.*ee/])
   t.ok(!obj1.three, 'key three removed from object')
   t.equals(obj1.one, 'two', 'key one remains in ibject')
   t.equals(obj1.five, 'six', 'key five remains in object')
 
   const obj2 = {
-    'one':'two',
-    'three':'four',
-    'five':'six'
+    one: 'two',
+    three: 'four',
+    five: 'six'
   }
-  removeKeysFromObject(obj2,[/th.*ee/,/three/,/.*five/])
+  removeKeysFromObject(obj2, [/th.*ee/, /three/, /.*five/])
   t.ok(!obj2.three, 'key three removed from object')
   t.equals(obj2.one, 'two', 'key one remains in ibject')
   t.ok(!obj2.five, 'key five removed from object')
@@ -32,25 +32,25 @@ test('removeKeysFromObject unit tests',function(t){
   t.end()
 })
 
-test('removeKeysFromObject tests',function(t){
+test('removeKeysFromObject tests', function (t) {
   t.ok(removeKeysFromObject, 'can import function')
 
   const obj1 = {
-    'one':'two',
-    'three':'four',
-    'five':'six'
+    one: 'two',
+    three: 'four',
+    five: 'six'
   }
-  removeKeysFromObject(obj1,[/th.*ee/])
+  removeKeysFromObject(obj1, [/th.*ee/])
   t.ok(!obj1.three, 'key three removed from object')
   t.equals(obj1.one, 'two', 'key one remains in ibject')
   t.equals(obj1.five, 'six', 'key five remains in object')
 
   const obj2 = {
-    'one':'two',
-    'three':'four',
-    'five':'six'
+    one: 'two',
+    three: 'four',
+    five: 'six'
   }
-  removeKeysFromObject(obj2,[/th.*ee/,/three/,/.*five/])
+  removeKeysFromObject(obj2, [/th.*ee/, /three/, /.*five/])
   t.ok(!obj2.three, 'key three removed from object')
   t.equals(obj2.one, 'two', 'key one remains in ibject')
   t.ok(!obj2.five, 'key five removed from object')
@@ -58,18 +58,17 @@ test('removeKeysFromObject tests',function(t){
   t.end()
 })
 
-
-test('removeKeysFromPostedFormVariables tests', function(t){
+test('removeKeysFromPostedFormVariables tests', function (t) {
   t.ok(removeKeysFromPostedFormVariables, 'can import function')
 
   const requestHeaders = {
-    'content-type':'application/x-www-form-urlencoded'
+    'content-type': 'application/x-www-form-urlencoded'
   }
   // body as parsed object
   const body1 = {
-    'one':'two',
-    'three':'four',
-    'five':'six'
+    one: 'two',
+    three: 'four',
+    five: 'six'
   }
   const result1 = removeKeysFromPostedFormVariables(
     body1,
@@ -81,7 +80,7 @@ test('removeKeysFromPostedFormVariables tests', function(t){
   t.ok(!result1.five, 'key five removed from object')
 
   // body as string
-  const body2 = `one=two&three=four&five=six`
+  const body2 = 'one=two&three=four&five=six'
   const result2 = querystring.parse(
     removeKeysFromPostedFormVariables(
       body2,
@@ -110,13 +109,13 @@ test('removeKeysFromPostedFormVariables tests', function(t){
 
   // untouched due to no application/x-www-form-urlencoded header
   const body4 = {
-    'one':'two',
-    'three':'four',
-    'five':'six'
+    one: 'two',
+    three: 'four',
+    five: 'six'
   }
   const result4 = removeKeysFromPostedFormVariables(
     body4,
-    {'content-type':'text/plain'},
+    { 'content-type': 'text/plain' },
     [/five*/]
   )
   t.equals(result4.three, 'four', 'key three removed from object')
