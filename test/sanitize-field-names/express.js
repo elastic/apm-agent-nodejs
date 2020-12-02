80f886c78c754e1712c74c856b6fa1a3f86b9a2c
Alan Storm <alan.storm@elastic.co>
2020-12-02 14:22:22 -0800

    test: test runner and fixtures for express

--- test/sanitize-field-names/express.js
+++ test/sanitize-field-names/express.js
@@ -1,62 +1,164 @@
-const agent = require('../..').start({
-  serviceName: 'test',
-  secretToken: 'test',
-  captureExceptions: false,
-  metricsInterval: 0,
-  centralConfig: false,
-  captureBody: 'all',
-  sanitizeFieldNames: ['x-powered-by']
-})
+const agent = require('../..').start(createAgentConfig())
 const test = require('tape')
 const request = require('request')
 const express = require('express')
 const bodyParser = require('body-parser')
 const mockClient = require('../_mock_http_client')
-
+const querystring = require('querystring')
 function resetAgent (cb) {
   agent._instrumentation.currentTransaction = null
   agent._transport = mockClient(1, cb)
   agent.captureError = function (err) { throw err }
 }
 
-test('express tests', function (t) {
+/**
+ * Attempts to parse a string first as JSON, then as a query string
+ */
+function getBodyAsObject(string) {
+  if(!string) {
+    return {}
+  }
+  try {
+    return JSON.parse(string)
+  } catch (e) {
+    return querystring.parse(string)
+  }
+}
+
+/**
+ * Checks that payload data meets expectations of test fixtures
+ */
+function assertHeadersAndPostedFormWithFixture(transaction, expected, t) {
+  t.ok(transaction, 'generated transaction')
+    // assert request headers here
+  for(const [header,value] of Object.entries(expected.requestHeaders.defined)) {
+    t.ok(transaction.context.request.headers[header.toLowerCase()],`header "${header}" is still set`)
+    t.equals(transaction.context.request.headers[header.toLowerCase()], value, `key "${header}" has correct value`)
+  }
+  for(const [,header] of expected.requestHeaders.undefined.entries()) {
+    t.ok(
+      !transaction.context.request.headers[header.toLowerCase()],
+      `header "${header}" is not set`
+    )
+  }
+
+  // assert response headers here
+  for(const [header,value] of Object.entries(expected.responseHeaders.defined)) {
+    t.ok(transaction.context.response.headers[header.toLowerCase()],`header "${header}" is still set`)
+    t.equals(transaction.context.response.headers[header.toLowerCase()], value, `key "${header}" has correct value`)
+  }
+  for(const [,header] of expected.responseHeaders.undefined.entries()) {
+    t.ok(
+      !transaction.context.response.headers[header.toLowerCase()],
+      `header "${header}" is not set`
+    )
+  }
+
+  // assert post/body headers here
+  const bodyAsObject = getBodyAsObject(transaction.context.request.body)
+  for(const [key,value] of Object.entries(expected.formFields.defined)) {
+    t.ok(bodyAsObject[key],`key "${key}" is still set`)
+    t.equals(bodyAsObject[key], value, `key "${key}" has correct value`)
+  }
+  for(const [,key] of expected.formFields.undefined.entries()) {
+    t.ok(!bodyAsObject[key],`key "${key}" is not set`)
+  }
+}
+
+function runTest(
+  t, expected, agentConfig, requestHeaders,responseHeaders,formFields, middleware=false
+) {
+  agent._config(agentConfig)
   const app = express()
-  app.use(bodyParser.urlencoded({ extended: false }))
-  // app.use(bodyParser.raw({type:'*/*'}))
-  // app.use(bodyParser.text({type:'*/*'}))
+  if(middleware) {
+    app.use(middleware)
+  }
 
   resetAgent((data) => {
-    t.ok('called reset agent', 'called reset agent')
     const transaction = data.transactions.pop()
-    t.ok(transaction, 'generated transaction')
-    console.log(transaction.context.request.headers)
-    console.log(JSON.stringify(transaction.context.response.headers))
-    console.log(transaction.context)
-    // t.strictEqual(data.transactions.length, 1, 'has a transaction')
-
-    // const trans = data.transactions[0]
-    // t.strictEqual(trans.name, 'GET /hello/:name', 'transaction name is GET /hello/:name')
-    // t.strictEqual(trans.type, 'request', 'transaction type is request')
+    assertHeadersAndPostedFormWithFixture(transaction, expected, t)
   })
 
   app.post('/test', (req, res) => {
     t.ok('received request', 'received request')
-    // TODO assert request headers here
+    res.header(responseHeaders)
     res.send('Hello World')
   })
 
   const server = app.listen(0, '0.0.0.0', () => {
     const url = `http://${server.address().address}:${server.address().port}/test`
-    request.post(url, { form: { thisisa: 'test', password: 'science' } }, function (error, response, body) {
-      if (error) { }
-      t.ok(body, 'received response')
-      // TODO assert request headers here
-      t.end()
+    request.post(
+      url,
+      {
+        form: formFields,
+        headers: requestHeaders
+      },
+      function (error, response, body) {
+        if (error) {
+          t.fail(error)
+        }
+        t.ok(body, 'received response')
+        t.end()
+      })
     })
-  })
 
   const done = () => {
     server.close()
   }
   t.on('end', done)
+}
+
+function createAgentConfig(values={}) {
+  const defaultAgentConfig = {
+    serviceName: 'test',
+    secretToken: 'test',
+    captureExceptions: false,
+    metricsInterval: 0,
+    centralConfig: false,
+    captureBody: 'all'
+  }
+
+  const agentConfig = Object.assign(
+    values,
+    defaultAgentConfig
+  )
+  return agentConfig
+}
+const fixtures = require('./fixtures')
+
+// const agentConfig = createAgentConfig({
+//   sanitizeFieldNames:['thi*isa']
+// })
+// const middleware = bodyParser.urlencoded({ extended: false })
+// app.use(bodyParser.urlencoded({ extended: false }))
+// app.use(bodyParser.raw({type:'*/*'}))
+// app.use(bodyParser.text({type:'*/*'}))
+
+function createMiddleware(type) {
+  if ('urlencoded' === type) {
+    return bodyParser.urlencoded({ extended: false })
+  } else if ('text' === type) {
+    return bodyParser.text({type:'*/*'})
+  } else if ('raw' === type) {
+    return bodyParser.raw({type:'*/*'})
+  }
+
+  throw new Error(`I don't know how to create a ${type} middleware`)
+}
+
+test('express test default handling', function (t) {
+  for(const [,fixture] of fixtures.entries()) {
+    test(fixture.name, function(t2){
+      runTest(
+        t2,
+        fixture.expected,
+        createAgentConfig(fixture.agentConfig),
+        fixture.input.requestHeaders,
+        fixture.input.responseHeaders,
+        fixture.input.formFields,
+        createMiddleware(fixture.bodyParsing)
+      )
+    })
+  }
+  t.end()
 })
---
+++ test/sanitize-field-names/fixtures.js
@@ -0,0 +1,311 @@
+const bodyParser = require('body-parser')
+// a fixture is a
+const f = {
+  name: '',              // the name of the test,
+  agentConfig: '',       // additional default configuration values for agent
+  middleware: '',        // what sort of body parsing to do for application/x-www-form-urlencoded
+                         // urlencoded -- test runner should parse values into an object
+                         // raw        -- test runner should provide raw Buffer object
+                         // text       -- test runner should provide string parsing
+                         // introduced to handle express middleware body parsers
+  input: {
+    requestHeaders: {},  // key/value pairs of HTTP request header values to set
+    responseHeaders: {}, // key/value pairs of HTTP response header values to set
+    formFields: {},      // key/value pairs of form data to post
+  },
+  expected: {
+    requestHeaders: {
+      defined: [],       // list of header keys we expected to be reported
+      undefined: [],     // list of header fields we expected to be sanitized
+    },
+    responseHeaders: {
+      defined: [],       // list of header keys we expected to be reported
+      undefined: [],     // list of header fields we expected to be sanitized
+    },
+    formFields: {
+      defined: [],       // list of form keys we expected to be reported
+      undefined: [],     // list of form fields we expected to be sanitized
+    },
+  }
+}
+module.exports = [
+  {
+    name: 'tests deault wildcard handling, with urlencode bodyparsing',
+    agentConfig: {},
+    bodyParsing: 'urlencoded',
+    input: {
+      requestHeaders: {
+        'password':'one',
+        'passwd':'two',
+        'pwd':'three',
+        'secret':'four',
+        'Somethingkey':'five',
+        'FOOtokenBAR':'six',
+        'ZIPsessionZAP':'seven',
+        'FOOsessionBAR':'eight',
+        'FULLOFcreditBEES':'nine',
+        'SINGcardSONG':'ten',
+        'authorization':'eleven',
+        'set-cookie':'twelve',
+        'keepmeRequest':'request'
+      },
+      responseHeaders: {
+        'password':'one',
+        'passwd':'two',
+        'pwd':'three',
+        'secret':'four',
+        'Somethingkey':'five',
+        'FOOtokenBAR':'six',
+        'ZIPsessionZAP':'seven',
+        'FOOsessionBAR':'seven',
+        'FOOsessionBAR':'eight',
+        'FULLOFcreditBEES':'nine',
+        'SINGcardSONG':'ten',
+        'authorization':'eleven',
+        'set-cookie':'twelve',
+        'keepmeResponse':'response'
+      },
+      formFields: {
+        'password':'one',
+        'passwd':'two',
+        'pwd':'three',
+        'secret':'four',
+        'Somethingkey':'five',
+        'FOOtokenBAR':'six',
+        'ZIPsessionZAP':'seven',
+        'FOOsessionBAR':'seven',
+        'FOOsessionBAR':'eight',
+        'FULLOFcreditBEES':'nine',
+        'SINGcardSONG':'ten',
+        'authorization':'eleven',
+        'set-cookie':'twelve',
+        'keepmeForm':'formFields'
+      },
+    },
+    expected: {
+      requestHeaders: {
+        undefined:[
+          'password','passwd','pwd','secret','Somethingkey','FOOtokenBAR',
+          'ZIPsessionZAP','FOOsessionBAR','FULLOFcreditBEES','SINGcardSONG',
+          'authorization','set-cookie'
+        ],
+        defined:{keepmeRequest:'request'}
+      },
+      responseHeaders: {
+        undefined:[
+          'password','passwd','pwd','secret','Somethingkey','FOOtokenBAR',
+          'ZIPsessionZAP','FOOsessionBAR','FULLOFcreditBEES','SINGcardSONG',
+          'authorization','set-cookie'
+        ],
+        defined:{keepmeResponse:'response'}
+      },
+      formFields: {
+        undefined:[
+          'password','passwd','pwd','secret','Somethingkey','FOOtokenBAR',
+          'ZIPsessionZAP','FOOsessionBAR','FULLOFcreditBEES','SINGcardSONG',
+          'authorization','set-cookie'
+        ],
+        defined:{keepmeForm:'formFields'}
+      }
+    }
+  },
+  {
+    name: 'tests deault wildcard handling, with text bodyparsing',
+    agentConfig: {},
+    bodyParsing: 'text',
+    input: {
+      requestHeaders: {
+        'password':'one',
+        'passwd':'two',
+        'pwd':'three',
+        'secret':'four',
+        'Somethingkey':'five',
+        'FOOtokenBAR':'six',
+        'ZIPsessionZAP':'seven',
+        'FOOsessionBAR':'eight',
+        'FULLOFcreditBEES':'nine',
+        'SINGcardSONG':'ten',
+        'authorization':'eleven',
+        'set-cookie':'twelve',
+        'keepmeRequest':'request'
+      },
+      responseHeaders: {
+        'password':'one',
+        'passwd':'two',
+        'pwd':'three',
+        'secret':'four',
+        'Somethingkey':'five',
+        'FOOtokenBAR':'six',
+        'ZIPsessionZAP':'seven',
+        'FOOsessionBAR':'seven',
+        'FOOsessionBAR':'eight',
+        'FULLOFcreditBEES':'nine',
+        'SINGcardSONG':'ten',
+        'authorization':'eleven',
+        'set-cookie':'twelve',
+        'keepmeResponse':'response'
+      },
+      formFields: {
+        'password':'one',
+        'passwd':'two',
+        'pwd':'three',
+        'secret':'four',
+        'Somethingkey':'five',
+        'FOOtokenBAR':'six',
+        'ZIPsessionZAP':'seven',
+        'FOOsessionBAR':'seven',
+        'FOOsessionBAR':'eight',
+        'FULLOFcreditBEES':'nine',
+        'SINGcardSONG':'ten',
+        'authorization':'eleven',
+        'set-cookie':'twelve',
+        'keepmeForm':'formFields'
+      },
+    },
+    expected: {
+      requestHeaders: {
+        undefined:[
+          'password','passwd','pwd','secret','Somethingkey','FOOtokenBAR',
+          'ZIPsessionZAP','FOOsessionBAR','FULLOFcreditBEES','SINGcardSONG',
+          'authorization','set-cookie'
+        ],
+        defined:{keepmeRequest:'request'}
+      },
+      responseHeaders: {
+        undefined:[
+          'password','passwd','pwd','secret','Somethingkey','FOOtokenBAR',
+          'ZIPsessionZAP','FOOsessionBAR','FULLOFcreditBEES','SINGcardSONG',
+          'authorization','set-cookie'
+        ],
+        defined:{keepmeResponse:'response'}
+      },
+      formFields: {
+        undefined:[
+          'password','passwd','pwd','secret','Somethingkey','FOOtokenBAR',
+          'ZIPsessionZAP','FOOsessionBAR','FULLOFcreditBEES','SINGcardSONG',
+          'authorization','set-cookie'
+        ],
+        defined:{keepmeForm:'formFields'}
+      }
+    }
+  },
+  {
+    name: 'tests deault wildcard handling, with raw/Buffer bodyparsing',
+    agentConfig: {},
+    bodyParsing: 'raw',
+    input: {
+      requestHeaders: {
+        'password':'one',
+        'passwd':'two',
+        'pwd':'three',
+        'secret':'four',
+        'Somethingkey':'five',
+        'FOOtokenBAR':'six',
+        'ZIPsessionZAP':'seven',
+        'FOOsessionBAR':'eight',
+        'FULLOFcreditBEES':'nine',
+        'SINGcardSONG':'ten',
+        'authorization':'eleven',
+        'set-cookie':'twelve',
+        'keepmeRequest':'request'
+      },
+      responseHeaders: {
+        'password':'one',
+        'passwd':'two',
+        'pwd':'three',
+        'secret':'four',
+        'Somethingkey':'five',
+        'FOOtokenBAR':'six',
+        'ZIPsessionZAP':'seven',
+        'FOOsessionBAR':'seven',
+        'FOOsessionBAR':'eight',
+        'FULLOFcreditBEES':'nine',
+        'SINGcardSONG':'ten',
+        'authorization':'eleven',
+        'set-cookie':'twelve',
+        'keepmeResponse':'response'
+      },
+      formFields: {
+        'password':'one',
+        'passwd':'two',
+        'pwd':'three',
+        'secret':'four',
+        'Somethingkey':'five',
+        'FOOtokenBAR':'six',
+        'ZIPsessionZAP':'seven',
+        'FOOsessionBAR':'seven',
+        'FOOsessionBAR':'eight',
+        'FULLOFcreditBEES':'nine',
+        'SINGcardSONG':'ten',
+        'authorization':'eleven',
+        'set-cookie':'twelve',
+        'keepmeForm':'formFields'
+      },
+    },
+    expected: {
+      requestHeaders: {
+        undefined:[
+          'password','passwd','pwd','secret','Somethingkey','FOOtokenBAR',
+          'ZIPsessionZAP','FOOsessionBAR','FULLOFcreditBEES','SINGcardSONG',
+          'authorization','set-cookie'
+        ],
+        defined:{keepmeRequest:'request'}
+      },
+      responseHeaders: {
+        undefined:[
+          'password','passwd','pwd','secret','Somethingkey','FOOtokenBAR',
+          'ZIPsessionZAP','FOOsessionBAR','FULLOFcreditBEES','SINGcardSONG',
+          'authorization','set-cookie'
+        ],
+        defined:{keepmeResponse:'response'}
+      },
+      formFields: {
+        undefined:[
+          'password','passwd','pwd','secret','Somethingkey','FOOtokenBAR',
+          'ZIPsessionZAP','FOOsessionBAR','FULLOFcreditBEES','SINGcardSONG',
+          'authorization','set-cookie'
+        ],
+        defined:{keepmeForm:'formFields'}
+      }
+    }
+  },
+
+  {
+    name: 'tests configured wildcard handling, with urlencode bodyparsing',
+    agentConfig: {
+      sanitizeFieldNames:['thi*isa']
+    },
+    bodyParsing: 'urlencoded',
+    input: {
+      requestHeaders: {
+        'password':'one',
+        'thisisa': 'test'
+      },
+      responseHeaders: {
+        'passwd':'two',
+        'thisisa': 'second test'
+      },
+      formFields: {
+        'Somethingkey':'five',
+        'thisisa': 'second test'
+      },
+    },
+    expected: {
+      // if users configure a wildcard pattern we expect that
+      // 1. the defaults won't apply
+      // 2. keys matching the wildcard will be omitted
+      requestHeaders: {
+        undefined:['thisisa'],
+        defined:{password:'one'}
+      },
+      responseHeaders: {
+        undefined:['thisisa'],
+        defined:{passwd:'two'}
+      },
+      formFields: {
+        undefined:['thisisa'],
+        defined:{Somethingkey:'five'}
+      }
+    }
+  },
+]
