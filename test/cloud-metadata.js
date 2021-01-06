60ad40050a79d8bdbef46800b58b4cd9be45db7a
Alan Storm <alan.storm@elastic.co>
2021-01-06 10:53:50 -0800

    feat: cloud metadata request

---
+++ test/cloud-metadata.js
@@ -0,0 +1,25 @@
+const tape = require('tape')
+
+const {getMetadataAws} = require('../lib/instrumentation/cloud-metadata')
+
+tape('cloud metadata', function(t) {
+    t.plan(4)
+    const host = 'localhost'
+    const validPort = 3000
+    const invalidPort = 30001
+    const protocol = 'http'
+
+    getMetadataAws(host, validPort, 1000, protocol, function(err, data){
+      t.error(err,'no errors expected')
+      t.ok(data,'returned data')
+    })
+
+    getMetadataAws(host, validPort, 0, protocol, function(err){
+      // console.log(err)
+      t.ok(err, 'expected timeout error')
+    })
+
+    getMetadataAws(host, invalidPort, 0, protocol, function(err){
+      t.ok(err, 'expected unreachable server error')
+    })
+})
