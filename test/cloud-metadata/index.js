90d92ed4885966ef873ba55309f10ce6cbef9ca0
Alan Storm <alan.storm@elastic.co>
2021-01-15 16:13:40 -0800

    feat: add v2 server tests and implementation

--- test/cloud-metadata/index.js
+++ test/cloud-metadata/index.js
@@ -2,7 +2,7 @@
 const tape = require('tape')
 
 const { CloudMetadata } = require('../../lib/cloud-metadata')
-const { getMetadataAws } = require('../../lib/cloud-metadata/aws')
+const { getMetadataAwsV1, getMetadataAwsV2 } = require('../../lib/cloud-metadata/aws')
 
 const { createTestServer, loadFixtureData } = require('./_lib')
 
@@ -24,37 +24,37 @@ const providerConfig = {
   }
 }
 
-// tape('cloud metadata: main function returns aws IMDSv2 data', function (t) {
-//   // t.plan helps ensure our callback is only called onces,
-//   // even though the "socket ping then real network request"
-//   // approach creates the potential for lots of errors
-//   t.plan(2)
+tape('cloud metadata: main function returns data', function (t) {
+  // t.plan helps ensure our callback is only called onces,
+  // even though the "socket ping then real network request"
+  // approach creates the potential for lots of errors
+  t.plan(2)
 
-//   const provider = 'aws-IMDSv2'
-//   const fixtureName = 'default aws fixture'
-//   const serverAws = createTestServer(provider, fixtureName)
-//   const config = Object.assign({}, providerConfig)
-//   const agent = {
-//     _conf: {
-//       cloudProvider: 'auto'
-//     }
-//   }
-//   const listener = serverAws.listen(0, function () {
-//     config.aws.port = listener.address().port
-//     config.gcp.port = listener.address().port
-//     config.azure.port = listener.address().port
+  const provider = 'aws'
+  const fixtureName = 'default aws fixture'
+  const serverAws = createTestServer(provider, fixtureName)
+  const config = Object.assign({}, providerConfig)
+  const agent = {
+    _conf: {
+      cloudProvider: 'auto'
+    }
+  }
+  const listener = serverAws.listen(0, function () {
+    config.aws.port = listener.address().port
+    config.gcp.port = listener.address().port
+    config.azure.port = listener.address().port
 
-//     const cloudMetadata = new CloudMetadata(agent)
-//     cloudMetadata.getCloudMetadata(
-//       providerConfig,
-//       function (err, metadata) {
-//         t.error(err, 'no errors expected')
-//         t.ok(metadata, 'returned data')
-//         listener.close()
-//       }
-//     )
-//   })
-// })
+    const cloudMetadata = new CloudMetadata(agent)
+    cloudMetadata.getCloudMetadata(
+      providerConfig,
+      function (err, metadata) {
+        t.error(err, 'no errors expected')
+        t.ok(metadata, 'returned data')
+        listener.close()
+      }
+    )
+  })
+})
 
 tape('cloud metadata: main function returns aws data', function (t) {
   // t.plan helps ensure our callback is only called onces,
@@ -102,7 +102,7 @@ tape('aws metadata: returns valid data', function (t) {
   const protocol = 'http'
   const listener = serverAws.listen(0, function () {
     const port = listener.address().port
-    getMetadataAws(host, port, 100, 1000, protocol, function (err, metadata) {
+    getMetadataAwsV1(host, port, 100, 1000, protocol, function (err, metadata) {
       t.error(err, 'no errors expected')
       t.ok(metadata, 'returned data')
       t.equals(metadata.account.id, fixture.response.accountId, 'found expected metadata for account.id')
@@ -126,7 +126,7 @@ tape('aws metadata: if socket ping times out', function (t) {
   const protocol = 'http'
   const listener = serverAws.listen(0, function () {
     const validPort = listener.address().port
-    getMetadataAws(host, validPort, 0, 1000, protocol, function (err) {
+    getMetadataAwsV1(host, validPort, 0, 1000, protocol, function (err) {
       t.ok(err, 'expected timeout error')
     })
     listener.close()
@@ -141,7 +141,7 @@ tape('aws metadata: if server is not there', function (t) {
   const host = 'localhost'
   const invalidPort = 30001
   const protocol = 'http'
-  getMetadataAws(host, invalidPort, 100, 1000, protocol, function (err) {
+  getMetadataAwsV1(host, invalidPort, 100, 1000, protocol, function (err) {
     t.ok(err, 'expected unreachable server error')
   })
 })
@@ -238,4 +238,65 @@ tape('cloud metadata: agent configuration wiring', function (t) {
   t.ok(!cloudMetadataInvalid.shouldFetchAzure(), 'invalid configuration should NOT fetch azure')
 
   t.end()
+})
+
+tape('aws metadata: IMDSv2 returns valid data', function (t) {
+  // t.plan helps ensure our callback is only called onces,
+  // even though the "socket ping then real network request"
+  // approach creates the potential for lots of errors
+  t.plan(8)
+
+  const provider = 'aws-IMDSv2'
+  const fixtureName = 'default aws fixture'
+  const serverAws = createTestServer(provider, fixtureName)
+  const fixture = loadFixtureData(provider, fixtureName)
+
+  const host = 'localhost'
+  const protocol = 'http'
+  const listener = serverAws.listen(0, function () {
+    const port = listener.address().port
+    getMetadataAwsV2(host, port, 100, 1000, protocol, function (err, metadata) {
+      t.error(err, 'no errors expected')
+      t.ok(metadata, 'returned data')
+      t.equals(metadata.account.id, fixture.response.accountId, 'found expected metadata for account.id')
+      t.equals(metadata.instance.id, fixture.response.instanceId, 'found expected metadata for')
+      t.equals(metadata.availability_zone, fixture.response.availabilityZone, 'found expected metadata for')
+      t.equals(metadata.machine.type, fixture.response.instanceType, 'found expected metadata for')
+      t.equals(metadata.provider, 'aws', `found expected metadata for ${t.name}`)
+      t.equals(metadata.region, fixture.response.region, 'found expected metadata for')
+      listener.close()
+    })
+  })
+})
+
+tape('cloud metadata: main function returns aws IMDSv2 data', function (t) {
+  // t.plan helps ensure our callback is only called onces,
+  // even though the "socket ping then real network request"
+  // approach creates the potential for lots of errors
+  t.plan(2)
+
+  const provider = 'aws-IMDSv2'
+  const fixtureName = 'default aws fixture'
+  const serverAws = createTestServer(provider, fixtureName)
+  const config = Object.assign({}, providerConfig)
+  const agent = {
+    _conf: {
+      cloudProvider: 'auto'
+    }
+  }
+  const listener = serverAws.listen(0, function () {
+    config.aws.port = listener.address().port
+    config.gcp.port = listener.address().port
+    config.azure.port = listener.address().port
+
+    const cloudMetadata = new CloudMetadata(agent)
+    cloudMetadata.getCloudMetadata(
+      providerConfig,
+      function (err, metadata) {
+        t.error(err, 'no errors expected')
+        t.ok(metadata, 'returned data')
+        listener.close()
+      }
+    )
+  })
 })
