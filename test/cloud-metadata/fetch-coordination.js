4830ba2f5995947e5131630be281b59f543db6d1
Alan Storm <alan.storm@elastic.co>
2021-01-17 10:44:27 -0800

    test: tests for coordination object

---
+++ test/cloud-metadata/fetch-coordination.js
@@ -0,0 +1,105 @@
+const tape = require('tape')
+const { FetchCoordination } = require('../../lib/cloud-metadata/fetch-coordination')
+tape.test('fetch coordination: all successful', function(t) {
+  const fetcher = new FetchCoordination;
+  fetcher.schedule(function(fetcher) {
+    // to do -- can this just be a callback?
+    setTimeout(function(){
+      fetcher.recordResult(null, 'pass1')
+    }, 10)
+  })
+
+  fetcher.schedule(function(fetcher) {
+    // to do -- can this just be a callback?
+    setTimeout(function(){
+      fetcher.recordResult(null, 'pass2')
+    }, 20)
+  })
+
+  fetcher.schedule(function(fetcher) {
+    // to do -- can this just be a callback?
+    setTimeout(function(){
+      fetcher.recordResult(null, 'pass3')
+    }, 30)
+  })
+
+  fetcher.schedule(function(fetcher) {
+    setTimeout(function(){
+      fetcher.recordResult(new Error('an error'))
+    }, 200)
+  })
+
+  fetcher.on('result', function(result){
+    t.equals('pass1', result, 'first callback\'s value is the result')
+    t.end()
+  })
+
+  fetcher.on('error', function(error) {
+    t.fail('no errors expected')
+  })
+  fetcher.start()
+})
+
+tape.test('fetch coordination: all errors', function(t) {
+  const fetcher = new FetchCoordination;
+  fetcher.schedule(function(fetcher) {
+    setTimeout(function(){
+      fetcher.recordResult(new Error('an error'))
+    }, 10)
+  })
+
+  fetcher.schedule(function(fetcher) {
+    setTimeout(function(){
+      fetcher.recordResult(new Error('an error'))
+    }, 20)
+  })
+
+  fetcher.schedule(function(fetcher) {
+    setTimeout(function(){
+      fetcher.recordResult(new Error('an error'))
+    }, 30)
+  })
+
+  fetcher.on('result', function(result){
+    t.fail('no results')
+  })
+
+  fetcher.on('error', function(error) {
+    t.ok(error)
+    t.equals(error.allErrors.length, 3, 'all three errors captured')
+    t.end()
+  })
+  fetcher.start()
+})
+
+tape.test('fetch coordination: fails to invoke callback (timeout)', function(t) {
+  const fetcher = new FetchCoordination(100);
+  fetcher.schedule(function(fetcher) {
+    setTimeout(function(){
+      fetcher.recordResult(new Error('an error'))
+    }, 10)
+  })
+
+  fetcher.schedule(function(fetcher) {
+    setTimeout(function(){
+      fetcher.recordResult(new Error('an error'))
+    }, 20)
+  })
+
+  fetcher.schedule(function(fetcher) {
+    setTimeout(function(){
+      // forgot to call the final recordResult
+    }, 30)
+  })
+
+  fetcher.on('result', function(result){
+  })
+
+  fetcher.on('error', function(error) {
+    t.ok(error)
+    t.equals(error.allErrors.length, 2, 'two of three errors captured')
+    t.equals(error.message, 'callback coordination reached timeout')
+    t.end()
+  })
+  fetcher.start()
+})
