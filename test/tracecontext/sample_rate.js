da23e968bb5502b6efd0ddaaafc48a2882d3dd58
Alan Storm <alan.storm@elastic.co>
2021-04-13 17:31:53 -0700

    fix: change prop name

--- test/tracecontext/sample_rate.js
+++ test/tracecontext/sample_rate.js
@@ -17,7 +17,7 @@ suite('Sample Rate Propagation', function (test) {
   test.test('sample rate is set', function (t) {
     agent._conf.transactionSampleRate = 0.499
     const transaction = startSampledTransaction()
-    t.equals(transaction.sample_rate, 0.499, 'sample rate set')
+    t.equals(transaction.sampleRate, 0.499, 'sample rate set')
 
     t.end()
   })
@@ -27,54 +27,54 @@ suite('Sample Rate Propagation', function (test) {
       childOf: TRACEPARENT_RECORDED,
       tracestate: 'es=s:0.7654321'
     })
-    t.equals(transaction.sample_rate, 0.7654321, 'sample rate set')
+    t.equals(transaction.sampleRate, 0.7654321, 'sample rate set')
     t.end()
   })
 
   // test that a sampled/recorded transaction produces spans and transactions
-  // with a sample_rate property equal to the sample rate
+  // with a sampleRate property equal to the sample rate
   test.test('recorded transactions have sample rate', function (t) {
     const transaction = agent.startTransaction('foo', 'bar', 'baz', 'bing', {
       childOf: TRACEPARENT_RECORDED,
       tracestate: 'es=s:0.7654321'
     })
     const span = transaction.startSpan('foo')
-    t.equals(transaction.sample_rate, 0.7654321, 'sample rate set')
-    t.equals(span.sample_rate, 0.7654321, 'sample rate set')
+    t.equals(transaction.sampleRate, 0.7654321, 'sample rate set')
+    t.equals(span.sampleRate, 0.7654321, 'sample rate set')
     t.end()
   })
 
   // test that a unsampled/unrecorded transaction produces a transaction
-  // with a sample_rate property of 0, and produces no spans
+  // with a sampleRate property of 0, and produces no spans
   test.test('unrecorded transactions have a sample rate of 0', function (t) {
     const transaction = agent.startTransaction('foo', 'bar', 'baz', 'bing', {
       childOf: TRACEPARENT_NOTRECORDED,
       tracestate: 'es=s:0'
     })
     const span = transaction.startSpan('foo')
-    t.equals(transaction.sample_rate, 0, 'sample rate set')
+    t.equals(transaction.sampleRate, 0, 'sample rate set')
     t.ok(!span, 'span not started')
     t.end()
   })
 
   // test that a sampled/recorded root transaction produces samples and
-  // transactions with a sample_rate equal to the sample rate
+  // transactions with a sampleRate equal to the sample rate
   test.test('recorded root transactions have correct sample rate', function (t) {
     agent._conf.transactionSampleRate = 1
     const transaction = agent.startTransaction('foo', 'bar', 'baz', 'bing')
     const span = transaction.startSpan('foo')
-    t.equals(transaction.sample_rate, 1, 'sample rate set')
-    t.equals(span.sample_rate, 1, 'sample rate set')
+    t.equals(transaction.sampleRate, 1, 'sample rate set')
+    t.equals(span.sampleRate, 1, 'sample rate set')
     t.end()
   })
 
   // test that an unsampled/unrecorded root  transaction produces a transaction
-  // with a sample_rate property of 0, and produces no spans
+  // with a sampleRate property of 0, and produces no spans
   test.test('unrecorded root transactions have a sample rate of 0', function (t) {
     agent._conf.transactionSampleRate = 0
     const transaction = agent.startTransaction('foo', 'bar', 'baz', 'bing')
     const span = transaction.startSpan('foo')
-    t.equals(transaction.sample_rate, 0, 'sample rate set')
+    t.equals(transaction.sampleRate, 0, 'sample rate set')
     t.ok(!span, 'no span for unsampled transactions')
     t.end()
   })
@@ -93,7 +93,7 @@ suite('Sample Rate Propagation', function (test) {
   })
 
   // Test that an invalid tracestate in a recorded transaction
-  // results in a serialized span without a sample_rate, per the spec.
+  // results in a serialized span without a sampleRate, per the spec.
   test.test('invalid tracestate with recorded transaction has no span.sample_rate', function (t) {
     const transaction = agent.startTransaction('foo', 'bar', 'baz', 'bing', {
       childOf: TRACEPARENT_RECORDED,
