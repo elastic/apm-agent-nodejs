4019318a3c3f9386daf5449b944196a4dd056a7f
Trent Mick <trent.mick@elastic.co>
2021-04-23 14:09:09 -0700

    fix: get the benchmarks run in CI to stick to a constant major ver of node (#2058)
    This also bumps that ver from the intended 12 to 14.
    
    Fixes: #2057

--- .ci/scripts/run-benchmarks.sh
+++ .ci/scripts/run-benchmarks.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
-set -ueo pipefail
+set -xueo pipefail
+
+RESULT_FILE=$1
+NODE_VERSION=$2
+if [[ -z "$RESULT_FILE" || -z "$NODE_VERSION" ]]; then
+  echo "usage: run-benchmarks.sh RESULT_FILE NODE_VERSION"
+  exit 1
+fi
 
 SCRIPTPATH=$(dirname "$0")
 source ./${SCRIPTPATH}/prepare-benchmarks-env.sh
 
-RESULT_FILE=${1:-apm-agent-benchmark-results.json}
-
-npm run bench:ci ${RESULT_FILE}
+npm run bench:ci "${RESULT_FILE}" "${NODE_VERSION}"
