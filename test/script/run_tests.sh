672a72a5c7173ed9e7d6a17e3c6e981caed3cb7a
Trent Mick <trent.mick@elastic.co>
2021-04-27 14:41:06 -0700

    avoid the openssl-config fix in NODE_OPTIONS for node v8 because of openssl support issues; should be fine as the only current need we had for it was for node:16; node v8 is EOL anyway

--- test/script/run_tests.sh
+++ test/script/run_tests.sh
@@ -65,7 +65,12 @@ fi
 # "test/instrumentation/modules/http2.js" fails if the OpenSSL SECLEVEL=2,
 # which is the case in the node:16 Docker image and could be in other
 # environments. Here we explicitly set it to SECLEVEL=0 for testing.
-export NODE_OPTIONS="$NODE_OPTIONS --openssl-config=./test/openssl-config-for-testing.cnf"
+#
+# Skip for node v8 because it results in this warning:
+#   openssl config failed: error:25066067:DSO support routines:DLFCN_LOAD:could not load the shared library
+if [[ $major_node_version -le 8 ]]; then
+  export NODE_OPTIONS="$NODE_OPTIONS --openssl-config=./test/openssl-config-for-testing.cnf"
+fi
 
 if [[ "$CI" || "$1" == "none" ]]
 then
