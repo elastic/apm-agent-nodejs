49029123f33508df503e96632625e53c61f1547b
Alan Storm <alan.storm@elastic.co>
2021-06-02 14:15:29 -0700

    test: additional test with a -2 for min duration

--- test/span-frames-min-duration.js
+++ test/span-frames-min-duration.js
@@ -82,6 +82,22 @@ tape.test(function (suite) {
   })
 
   suite.test('spanFrameMinDuration=<negative> always sets', function (t) {
+    agent._config({ logLevel: 'off', spanFramesMinDuration: '-1' })
+    const trans = agent.startTransaction()
+    const span = agent.startSpan()
+    setTimeout(function () {
+      span.end()
+      trans.end()
+      // t.ok(false,'we are parsing the callsites after we are encoded the span')
+      span._encode(function (err, data) {
+        t.error(err)
+        t.ok(data.stacktrace, 'stacktrace is set')
+        t.end()
+      })
+    }, 200)
+  })
+
+  suite.test('spanFrameMinDuration < -1 always sets', function (t) {
     agent._config({ logLevel: 'off', spanFramesMinDuration: '-2' })
     const trans = agent.startTransaction()
     const span = agent.startSpan()
