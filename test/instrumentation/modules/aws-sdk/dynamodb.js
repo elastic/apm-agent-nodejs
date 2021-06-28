c930448f535c2f232e8c5cf877ea4723ed076457
Alan Storm <alan.storm@elastic.co>
2021-06-27 19:05:52 -0700

    test: integartion tests

--- test/instrumentation/modules/aws-sdk/dynamodb.js
+++ test/instrumentation/modules/aws-sdk/dynamodb.js
@@ -1,4 +1,4 @@
-  const agent = require('../../../..').start({
+const agent = require('../../../..').start({
   serviceName: 'test',
   secretToken: 'test',
   captureExceptions: false,
@@ -17,7 +17,8 @@ const {
   getRegionFromRequest,
   getPortFromRequest,
   getStatementFromRequest,
-  getAddressFromRequest
+  getAddressFromRequest,
+  getMethodFromRequest
 } =
   require('../../../../lib/instrumentation/modules/aws-sdk/dynamodb')
 
@@ -25,7 +26,7 @@ initializeAwsSdk()
 
 function initializeAwsSdk () {
   // SDk requires a region to be set
-  AWS.config.update({ region: 'us-west' })
+  AWS.config.update({ region: 'us-west-2' })
 
   // without fake credentials the aws-sdk will attempt to fetch
   // credentials as though it was on an EC2 instance
@@ -33,29 +34,23 @@ function initializeAwsSdk () {
   process.env.AWS_SECRET_ACCESS_KEY = 'fake-2'
 }
 
-function createMockServer (xmlResponse) {
+function createMockServer (jsonResponse) {
   const app = express()
   app.use(bodyParser.urlencoded({ extended: false }))
   app.post('/', (req, res) => {
-    res.setHeader('Content-Type', 'text/xml')
-    res.send(xmlResponse)
+    res.setHeader('Content-Type', 'application/javascript')
+    res.send(jsonResponse)
   })
   return app
 }
 
-function getXmlResponse (method) {
+function getJsonResponse (method) {
   return fixtures[method].response
 }
 
 function resetAgent (cb) {
   agent._instrumentation.currentTransaction = null
   agent._transport = mockClient(cb)
-}
-
-function getParams (method, port) {
-  const params = fixtures[method].request
-  params.QueueUrl = `http://localhost:${port}/1/our-queue`
-  return params
 }
 
 tape.test('AWS DynamoDB: Unit Test Functions', function (test) {
@@ -69,9 +64,9 @@ tape.test('AWS DynamoDB: Unit Test Functions', function (test) {
     }
     t.equals(getRegionFromRequest(request), 'us-west-2')
     t.equals(getRegionFromRequest({}), undefined)
-    t.equals(getRegionFromRequest({service:null}), null)
-    t.equals(getRegionFromRequest({service:{config:null}}), null)
-    t.equals(getRegionFromRequest({service:{config:{region:null}}}), null)
+    t.equals(getRegionFromRequest({ service: null }), null)
+    t.equals(getRegionFromRequest({ service: { config: null } }), null)
+    t.equals(getRegionFromRequest({ service: { config: { region: null } } }), null)
     t.equals(getRegionFromRequest(), undefined)
     t.equals(getRegionFromRequest(null), null)
     t.end()
@@ -87,9 +82,9 @@ tape.test('AWS DynamoDB: Unit Test Functions', function (test) {
     }
     t.equals(getPortFromRequest(request), 443)
     t.equals(getPortFromRequest({}), undefined)
-    t.equals(getPortFromRequest({service:null}), null)
-    t.equals(getPortFromRequest({service:{endpoint:null}}), null)
-    t.equals(getPortFromRequest({service:{endpoint:{port:null}}}), null)
+    t.equals(getPortFromRequest({ service: null }), null)
+    t.equals(getPortFromRequest({ service: { endpoint: null } }), null)
+    t.equals(getPortFromRequest({ service: { endpoint: { port: null } } }), null)
     t.equals(getPortFromRequest(), undefined)
     t.equals(getPortFromRequest(null), null)
     t.end()
@@ -104,9 +99,9 @@ tape.test('AWS DynamoDB: Unit Test Functions', function (test) {
     }
     t.equals(getStatementFromRequest(request), 'foo = :bar')
     t.equals(getStatementFromRequest({}), undefined)
-    t.equals(getStatementFromRequest({operation:null}), undefined)
-    t.equals(getStatementFromRequest({operation:'query',params:{}}), undefined)
-    t.equals(getStatementFromRequest({operation:'query',params:{KeyConditionExpression:null}}), undefined)
+    t.equals(getStatementFromRequest({ operation: null }), undefined)
+    t.equals(getStatementFromRequest({ operation: 'query', params: {} }), undefined)
+    t.equals(getStatementFromRequest({ operation: 'query', params: { KeyConditionExpression: null } }), undefined)
     t.equals(getStatementFromRequest(), undefined)
     t.equals(getStatementFromRequest(null), undefined)
     t.end()
@@ -116,17 +111,30 @@ tape.test('AWS DynamoDB: Unit Test Functions', function (test) {
     const request = {
       service: {
         endpoint: {
-          host:'dynamodb.us-west-2.amazonaws.com'
+          host: 'dynamodb.us-west-2.amazonaws.com'
         }
       }
     }
     t.equals(getAddressFromRequest(request), 'dynamodb.us-west-2.amazonaws.com')
     t.equals(getAddressFromRequest({}), undefined)
-    t.equals(getAddressFromRequest({service:null}), null)
-    t.equals(getAddressFromRequest({service:{endpoint:null}}), null)
-    t.equals(getAddressFromRequest({service:{endpoint:{host:null}}}), null)
+    t.equals(getAddressFromRequest({ service: null }), null)
+    t.equals(getAddressFromRequest({ service: { endpoint: null } }), null)
+    t.equals(getAddressFromRequest({ service: { endpoint: { host: null } } }), null)
     t.equals(getAddressFromRequest(), undefined)
     t.equals(getAddressFromRequest(null), null)
+    t.end()
+  })
+
+  test.test('function getMethodFromRequest', function (t) {
+    const request = {
+      operation: 'query'
+    }
+    t.equals(getMethodFromRequest(request), 'Query')
+    t.equals(getMethodFromRequest({}), undefined)
+    t.equals(getMethodFromRequest({ operation: null }), undefined)
+    t.equals(getAddressFromRequest(), undefined)
+    t.equals(getAddressFromRequest(null), null)
+
     t.end()
   })
 })
@@ -134,37 +142,67 @@ tape.test('AWS DynamoDB: Unit Test Functions', function (test) {
 tape.test('AWS DynamoDB: End to End Test', function (test) {
   test.test('API: query', function (t) {
     const app = createMockServer(
-      getXmlResponse('query')
+      getJsonResponse('query')
     )
     const listener = app.listen(0, function () {
       resetAgent(function (data) {
-        // const [spanSqs, spanHttp] = getSqsAndOtherSpanFromData(data, t)
-
-        // t.equals(spanSqs.name, 'SQS SEND to our-queue', 'SQS span named correctly')
-        // t.equals(spanSqs.type, 'messaging', 'span type set to messaging')
-        // t.equals(spanSqs.subtype, 'sqs', 'span subtype set to sqs')
-        // t.equals(spanSqs.action, 'send', 'span action matches API method called')
-        // t.equals(spanSqs.context.destination.service.type, 'messaging', 'messaging context set')
-        // t.equals(spanSqs.context.message.queue.name, 'our-queue', 'queue name context set')
-
-        // t.equals(spanHttp.type, 'external', 'other span is for HTTP request')
+        const span = data.spans.filter((span) => span.type === 'db').pop()
+        t.equals(span.name, 'DynamoDB Query fixture-table', 'span named correctly')
+        t.equals(span.type, 'db', 'span type correctly set')
+        t.equals(span.subtype, 'dynamodb', 'span subtype set correctly')
+        t.equals(span.action, 'query', 'query set correctly')
+        t.equals(span.context.db.statement, 'id = :foo', 'statment set in context correctly')
+        t.equals(span.context.destination.service.name, 'dynamodb', 'service name in destination context')
         t.end()
       })
+      const port = listener.address().port
+      AWS.config.update({
+        endpoint: `http://localhost:${port}`
+      })
       agent.startTransaction('myTransaction')
-      // const sqs = new AWS.SQS({ apiVersion: '2012-11-05' })
-      // const params = getParams('query', listener.address().port)
-      // sqs.sendMessage(params, function (err, data) {
-      //   t.error(err)
-      //   agent.endTransaction()
-      //   listener.close()
-      // })
-      agent.endTransaction()
-      listener.close()
+      var ddb = new AWS.DynamoDB({ apiVersion: '2012-08-10' })
+      var params = {
+        TableName: 'fixture-table',
+        KeyConditionExpression: 'id = :foo',
+        ExpressionAttributeValues: {
+          ':foo': { S: '001' }
+        }
+      }
+      ddb.query(params, function (err, data) {
+        t.error(err)
+        agent.endTransaction()
+        listener.close()
+      })
     })
   })
 
-  // test.test('API: no transaction', function (t) {
-  //   t.fail()
-  //   // t.end()
-  // })
+  tape.test('AWS DynamoDB: No Transaction', function (test) {
+    test.test('API: query', function (t) {
+      const app = createMockServer(
+        getJsonResponse('query')
+      )
+      const listener = app.listen(0, function () {
+        resetAgent(function (data) {
+          t.equals(data.spans.length, 0, 'no spans without transaction')
+          t.end()
+        })
+        const port = listener.address().port
+        AWS.config.update({
+          endpoint: `http://localhost:${port}`
+        })
+        var ddb = new AWS.DynamoDB({ apiVersion: '2012-08-10' })
+        var params = {
+          TableName: 'fixture-table',
+          KeyConditionExpression: 'id = :foo',
+          ExpressionAttributeValues: {
+            ':foo': { S: '001' }
+          }
+        }
+        ddb.query(params, function (err, data) {
+          t.error(err)
+          listener.close()
+        })
+      })
+    })
+  })
 })
--- test/instrumentation/modules/aws-sdk/fixtures-dynamodb.js
+++ test/instrumentation/modules/aws-sdk/fixtures-dynamodb.js
@@ -1,7 +1,11 @@
 'use strict'
 module.exports = {
-    query: {
-        request:'',
-        response:''
+  query: {
+    response: {
+      Count: 1,
+      Items:
+                [{ id: { S: '001' }, name: { S: 'Richard Roe' }, number: { N: '1' } }],
+      ScannedCount: 1
     }
+  }
 }
