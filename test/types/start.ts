5283c324808018632b44c8497fb14fb63402fd7c
Trent Mick <trent.mick@elastic.co>
2021-07-09 14:43:04 -0700

    s/agent/apm/ as our docs tend to show for users

--- test/types/start.ts
+++ test/types/start.ts
@@ -1,7 +1,7 @@
-import agent from '../../start'
-import { IncomingMessage, ServerResponse } from 'http';
-import { Socket } from 'net';
+import apm from '../../start'
+import { IncomingMessage, ServerResponse } from 'http'
+import { Socket } from 'net'
 
 const req = new IncomingMessage(new Socket())
 const res = new ServerResponse(req)
-agent.middleware.connect()(new Error(), req, res, () => {})
+apm.middleware.connect()(new Error(), req, res, () => {})
