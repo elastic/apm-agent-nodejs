0fb8fc951791b25575602f51fff41ca2abde5821
Alan Storm <alan.storm@elastic.co>
2021-07-29 14:53:25 -0700

    feat: bumb mongo version

--- test/instrumentation/modules/mongodb.test.js
+++ test/instrumentation/modules/mongodb.test.js
@@ -8,6 +8,13 @@ const agent = require('../../..').start({
   centralConfig: false
 })
 
+// require('mongodb') is a hard crash on node 8
+const mongodbVersion = require('./node_modules/mongodb/package.json').version
+const semver = require('semver')
+if(semver.gte(mongodbVersion, '4.0.0') && semver.lt(process.version, '10.0.0')) {
+  console.log(`# SKIP mongodb@${mongodbVersion} does not support node ${process.version}`)
+  process.exit()
+}
 const MongoClient = require('mongodb').MongoClient
 const test = require('tape')
 
