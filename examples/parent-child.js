334d7ef62d20be5326370de27237c05ec10f416f
Trent Mick <trent.mick@elastic.co>
2021-08-09 17:12:10 -0700

    changes to S3 instrumentation so that its parent/child relationship works (see examples/parent-child.js case)
    We will have to do the same for other aws-sdk, for ES, and evaluate for
    other instrumentations. Note this is an improvement over current
    behaviour which doesn't handle cases like the parent/child reln above
    anyway.

--- examples/parent-child.js
+++ examples/parent-child.js
@@ -1,6 +1,6 @@
 // vim: set ts=2 sw=2:
 
-var apm = require('./').start({ // elastic-apm-node
+var apm = require('../').start({ // elastic-apm-node
   serviceName: 'parent-child',
   captureExceptions: false,
   logUncaughtExceptions: true,
@@ -14,6 +14,7 @@ var apm = require('./').start({ // elastic-apm-node
   // disableSend: true
 })
 
+const assert = require('assert').strict
 const express = require('express')
 
 const app = express()
@@ -38,10 +39,6 @@ app.get('/a', (req, res) => {
     }, 10)
   }, 10)
 })
-
-setTimeout(function () {
-  console.warn('XXX in unrelated 3s timeout: currTx is: ', apm._instrumentation.currTx())
-}, 3000)
 
 app.get('/b', (req, res) => {
   var s1 = apm.startSpan('s1')
@@ -130,12 +127,21 @@ app.get('/unended-span', (req, res) => {
 //       HTTP span
 //         a-sibling-span
 //
-// Perhaps this is fine?
+// Perhaps this is fine? Nope, it isn't.
+//
 app.get('/dario-1963', (req, res) => {
+  const { Client } = require('@elastic/elasticsearch')
+  const client = new Client({
+    node: 'http://localhost:9200',
+    auth: { username: 'admin', password: 'changeme' }
+  })
+  // Note: works fine if client.search is under a setImmediate for sep context.
+  // setImmediate(function () { ... })
   client.search({
-    index: 'kibana_sample_data_logs',
+    // index: 'kibana_sample_data_logs',
     body: { size: 1, query: { match_all: {} } }
   }, (err, _result) => {
+    console.warn('XXX in client.search cb: %s', apm._instrumentation._runCtxMgr.active())
     if (err) {
       res.send(err)
     } else {
@@ -143,12 +149,54 @@ app.get('/dario-1963', (req, res) => {
     }
   })
 
+  console.warn('XXX after client.search sync: %s', apm._instrumentation._runCtxMgr.active())
+
   // What if I add this?
-  setImmediate(function () {
+  setImmediate(function aSiblingSpanInHere () {
     var span = apm.startSpan('a-sibling-span')
     setImmediate(function () {
       span.end()
     })
+  })
+})
+
+// Want:
+//   transaction "GET /s3"
+//   `- span "span1"
+//     `- span "S3 ListBuckets"
+//       `- span "GET s3.amazonaws.com/"
+//     `- span "span3"
+//     `- span "span2"
+//
+// Eventually the HTTP span should be removed as exit spans are supported.
+app.get('/s3', (req, res) => {
+  const AWS = require('aws-sdk')
+  const s3Client = new AWS.S3({ apiVersion: '2006-03-01' })
+
+  setImmediate(function () {
+    var s1 = apm.startSpan('span1')
+
+    s3Client.listBuckets({}, function (err, _data) {
+      if (err) {
+        res.send(err)
+      } else {
+        res.send('ok')
+      }
+      s1.end()
+    })
+    assert(apm._instrumentation.currSpan() === s1)
+
+    setImmediate(function () {
+      assert(apm._instrumentation.currSpan() === s1)
+      var s2 = apm.startSpan('span2')
+      setImmediate(function () {
+        s2.end()
+      })
+    })
+
+    assert(apm._instrumentation.currSpan() === s1)
+    var s3 = apm.startSpan('span3')
+    s3.end()
   })
 })
 
