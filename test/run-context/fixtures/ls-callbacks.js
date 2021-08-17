c120b44feb601317aed42007fbf03a9b7846701f
Trent Mick <trent.mick@elastic.co>
2021-08-17 12:30:32 -0700

    add test fixtures forgotten in previous commit

---
+++ test/run-context/fixtures/ls-callbacks.js
@@ -0,0 +1,52 @@
+// Expect:
+//     transaction "ls"
+//     `- span "cwd"
+//     `- span "readdir"
+
+const apm = require('../../../').start({ // elastic-apm-node
+  captureExceptions: false,
+  captureSpanStackTraces: false,
+  metricsInterval: 0,
+  cloudProvider: 'none',
+  centralConfig: false,
+  // ^^ Boilerplate config above this line is to focus on just tracing.
+  serviceName: 'ls-callbacks'
+})
+
+const assert = require('assert').strict
+const fs = require('fs')
+
+let t1
+
+function getCwd () {
+  const s2 = apm.startSpan('cwd')
+  try {
+    return process.cwd()
+  } finally {
+    assert(apm.currentTransaction === t1)
+    assert(apm.currentSpan === s2)
+    s2.end()
+  }
+}
+
+function main () {
+  t1 = apm.startTransaction('ls')
+  assert(apm.currentTransaction === t1)
+
+  const cwd = getCwd()
+  const s3 = apm.startSpan('readdir')
+  assert(apm.currentSpan === s3)
+  fs.readdir(cwd, function (_err, entries) {
+    assert(apm.currentSpan === s3)
+    s3.end()
+    assert(apm.currentSpan === null)
+
+    console.log('entries:', entries)
+
+    assert(apm.currentTransaction === t1)
+    t1.end()
+    assert(apm.currentTransaction === null)
+  })
+}
+
+main()
