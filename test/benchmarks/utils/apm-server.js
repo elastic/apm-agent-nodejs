1207b49e8b08f9c1d7153a21dd369aa13faa1661
Trent Mick <trent.mick@elastic.co>
2021-08-18 15:47:33 -0700

    ci: avoid port 8200 for mock apm-server for benchmark runs (#2291)
    This is to avoid possible port conflict if there is a real APM server
    already running on a shared machine.
    
    Fixes: #2131

--- test/benchmarks/utils/apm-server.js
+++ test/benchmarks/utils/apm-server.js
@@ -1,3 +1,5 @@
+// A mock APM server that just accepts any request.
+// Usage: node apm-server.js PORT
 'use strict'
 
 process.on('SIGUSR2', function () {
@@ -13,4 +15,8 @@ const server = http.createServer(function (req, res) {
   req.resume()
 })
 
-server.listen(8200)
+const PORT = Number(process.argv[2])
+if (isNaN(PORT)) {
+  throw new Error(`invalid or missing PORT argument: ${process.argv[2]}`)
+}
+server.listen(PORT)
