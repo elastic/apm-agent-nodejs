elastic/apm-agent-nodejs

commit f3796250bca0ba7ec9d3dfc50d874f248f4c8c19

ci: workaround for common 'network docker_default id $id has active endpoints' jenkins error (#2184)
A relatively frequent error in Jenkins CI is this at the end of an
otherwise successful test run:
    error while removing network: network docker_default id 873ad... has active endpoints

This is an attempted workaround to force removal and to print some
possibly helpful debug info.

.ci/scripts/test.sh
@@ -250,8 +250,15 @@ docker-compose \
   --abort-on-container-exit \
   node_tests
 
-NODE_VERSION=${NODE_VERSION} docker-compose \
-  --no-ansi \
-  --log-level ERROR \
-  -f .ci/docker/${DOCKER_COMPOSE_FILE} \
-  down -v --remove-orphans
+if ! NODE_VERSION=${NODE_VERSION} docker-compose \
+    --no-ansi \
+    --log-level ERROR \
+    -f ${DOCKER_FOLDER}/${DOCKER_COMPOSE_FILE} \
+    down -v --remove-orphans; then
+  # Workaround for this commonly seen error:
+  #   error while removing network: network docker_default id $id has active endpoints
+  echo "Unexpected error in 'docker-compose down ...'. Forcing removal of unused networks."
+  docker network inspect docker_default || true
+  docker network inspect -f '{{range .Containers}}{{ .Name }} {{end}}' docker_default || true
+  docker network prune --force || true
+fi

commit a8ca8b421f6c0d02b34daa313bb3f8e2dc57e3af

ci: fix use of missing DOCKER_FOLDER var that breaks CI test cleanup
A change introduced in #2184 used the DOCKER_FOLDER var that had been removed
in earlier #2087. The result was the test cleanup's `docker-compose down ...`
failing

.ci/scripts/test.sh
@@ -255,7 +255,7 @@ docker-compose \
 if ! NODE_VERSION=${NODE_VERSION} docker-compose \
     --no-ansi \
     --log-level ERROR \
-    -f ${DOCKER_FOLDER}/${DOCKER_COMPOSE_FILE} \
+    -f .ci/docker/${DOCKER_COMPOSE_FILE} \
     down -v --remove-orphans; then
   # Workaround for this commonly seen error:
   #   error while removing network: network docker_default id $id has active endpoints