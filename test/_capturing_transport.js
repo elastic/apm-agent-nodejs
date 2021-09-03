ea21754038a5045c0af6fef8ab26bb757005882e
Trent Mick <trent.mick@elastic.co>
2021-09-03 15:57:42 -0700

    breakdown.test.js passing

---
+++ test/_capturing_transport.js
@@ -0,0 +1,77 @@
+'use strict'
+
+// An Agent transport -- i.e. the APM server client API provided by
+// elastic-apm-http-client -- that just captures all sent events.
+//
+// Usage:
+//    const testAgentOpts = {
+//      // ...
+//      transport () { return new CapturingTransport() }
+//    }
+//
+//    test('something', function (t) {
+//      const agent = new Agent().start(testAgentOpts)
+//      // Use `agent`, then assert that
+//      // `agent._transport.{spans,transactions,errors,metricsets}` are as
+//      // expected.
+//      agent.destroy()
+//      t.end()
+//    })
+//
+// Note: This is similar to _mock_http_client.js, but avoids the testing model
+// of using an expected number of sent APM events to decide when a test should
+// end.
+
+class CapturingTransport {
+  constructor () {
+    this.spans = []
+    this.transactions = []
+    this.errors = []
+    this.metricsets = []
+  }
+
+  config (opts) {}
+
+  addMetadataFilter (fn) {}
+
+  sendSpan (span, cb) {
+    this.spans.push(span)
+    if (cb) {
+      process.nextTick(cb)
+    }
+  }
+
+  sendTransaction (transaction, cb) {
+    this.transactions.push(transaction)
+    if (cb) {
+      process.nextTick(cb)
+    }
+  }
+
+  sendError (error, cb) {
+    this.errors.push(error)
+    if (cb) {
+      process.nextTick(cb)
+    }
+  }
+
+  sendMetricSet (metricset, cb) {
+    this.metricsets.push(metricset)
+    if (cb) {
+      process.nextTick(cb)
+    }
+  }
+
+  flush (cb) {
+    if (cb) {
+      process.nextTick(cb)
+    }
+  }
+
+  // Inherited from Writable, called in agent.js.
+  destroy () {}
+}
+
+module.exports = {
+  CapturingTransport
+}
