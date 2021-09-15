81fd4ff668c1bb0ecb275ecdddbae9edbc1a12aa
Trent Mick <trent.mick@elastic.co>
2021-09-15 09:21:25 -0700

    drop unneeded change to this file to reduce diff size

--- test/_mock_http_client.js
+++ test/_mock_http_client.js
@@ -20,10 +20,7 @@
 //    The `done` callback will be called with the written data (`_writes`)
 //    after a 200ms delay with no writes (the timer only starts after the
 //    first write).
-
-function noop () {}
-
-function createMockClient (expected, done) {
+module.exports = function (expected, done) {
   const timerBased = typeof expected === 'function'
   if (timerBased) done = expected
   let timer
@@ -36,34 +33,12 @@ function createMockClient (expected, done) {
       const type = Object.keys(obj)[0]
       this._writes.length++
       this._writes[type + 's'].push(obj[type])
-      // console.warn('XXX mock client "%s" write: %s', type, obj)
 
       process.nextTick(cb)
 
       if (timerBased) resetTimer()
-      else if (this._writes.length === expected) {
-        // Give a short delay for subsequent events (typically a span delayed
-        // by asynchronous `span._encode()`) to come in so a test doesn't
-        // unwittingly pass, when in fact more events than expected are
-        // produced.
-        // XXX Play with this delay? This might significantly increase test time. Not sure.
-        //    E.g. 'node test/integration/index.test.js' from 0.5s to 3.5s :/
-        //    Better solutions: (a) explicit delay when playing with spans
-        //    (b) issue #2294 to have `agent.flush()` actually flush inflight spans.
-        // XXX I've since disabled this because it breaks timing assumptions in
-        //     code using this mock client. While those assumptions are a pain,
-        //     I don't want to re-write *all* that test code now.
-        // const SHORT_DELAY = 100
-        // setTimeout(() => {
-        done(this._writes)
-        // }, SHORT_DELAY)
-      } else if (this._writes.length > expected) {
-        let summary = JSON.stringify(obj)
-        if (summary.length > 200) {
-          summary = summary.slice(0, 197) + '...'
-        }
-        throw new Error(`too many writes: unexpected write: ${summary}`)
-      }
+      else if (this._writes.length === expected) done(this._writes)
+      else if (this._writes.length > expected) throw new Error('too many writes')
     },
     sendSpan (span, cb) {
       this._write({ span }, cb)
@@ -92,4 +67,4 @@ function createMockClient (expected, done) {
   }
 }
 
-module.exports = createMockClient
+function noop () {}
