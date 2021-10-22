925043c319df7c578c19cae43162bdca290995d2
Trent Mick <trent.mick@elastic.co>
2021-10-21 17:15:59 -0700

    feat: support @elastic/elasticsearch@8
    This is a first incomplete crack at it. TAV is probably wrong. There
    are some XXXs to complete in the tests for a change in the 'request'
    diagnostic data.

---
+++ examples/trace-elasticsearch.js
@@ -0,0 +1,45 @@
+#!/usr/bin/env node --unhandled-rejections=strict
+
+// A small example showing Elastic APM tracing @elastic/elasticsearch.
+//
+// This assumes an Elasticsearch running on localhost. You can use:
+//    npm run docker:start
+// to start an Elasticsearch docker container (and other containers used for
+// testing of this project). Then `npm run docker:stop` to stop them.
+
+const apm = require('../').start({ // elastic-apm-node
+  serviceName: 'example-trace-elasticsearch'
+})
+
+const { Client } = require('@elastic/elasticsearch-canary')
+
+const client = new Client({
+  // With version 8 of the client, you can use `HttpConnection` to use the old
+  // HTTP client:
+  //   Connection: HttpConnection,
+  node: `http://${process.env.ES_HOST || 'localhost'}:9200`
+})
+
+// For tracing spans to be created, there must be an active transaction.
+// Typically, a transaction is automatically started for incoming HTTP
+// requests to a Node.js server. However, because this script is not running
+// an HTTP server, we manually start a transaction. More details at:
+// https://www.elastic.co/guide/en/apm/agent/nodejs/current/custom-transactions.html
+apm.startTransaction('t1')
+client.ping()
+  .then((res) => { console.log('ping response:', res) })
+  .catch((err) => { console.log('ping error:', err) })
+  .finally(() => { apm.endTransaction() })
+
+async function awaitStyle () {
+  apm.startTransaction('t2')
+  try {
+    const res = await client.search({ q: 'pants' })
+    console.log('search response:', res)
+  } catch (err) {
+    console.log('search error:', err)
+  } finally {
+    apm.endTransaction()
+  }
+}
+awaitStyle()
