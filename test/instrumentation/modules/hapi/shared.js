elastic/apm-agent-nodejs

commit 0e59a23adfa9b4a7c15b7dc527fffa7486dd4558

feat: support node v17 (#2380)
There were a few changes (in mongodb instrumentation) and tests to
deal with `localhost` resolving to a IPv6 due to this change in node v17
https://github.com/nodejs/node/pull/39987

test/instrumentation/modules/hapi/shared.js
@@ -7,7 +7,8 @@ module.exports = (moduleName) => {
     captureExceptions: false,
     logLevel: 'fatal',
     metricsInterval: 0,
-    centralConfig: false
+    centralConfig: false,
+    cloudProvider: 'none'
   })
 
   var isHapiIncompat = require('../../../_is_hapi_incompat')
@@ -48,7 +49,7 @@ module.exports = (moduleName) => {
     agent.captureError = originalCaptureError
 
     var server = startServer(function (err, port) {
-      t.error(err)
+      t.error(err, 'no error from startServer')
       http.get('http://localhost:' + port + '/captureError?foo=bar')
     })
   })
@@ -523,7 +524,7 @@ module.exports = (moduleName) => {
   })
 
   function makeServer (opts) {
-    var server = new Hapi.Server()
+    var server = new Hapi.Server({ host: 'localhost' })
     if (semver.satisfies(pkg.version, '<17')) {
       server.connection(opts)
     }

.ci/.jenkins_nightly_nodejs.yml
@@ -9,4 +9,4 @@
 # made, these will stop and there will be no value in testing v17 nightlies.
 #
 NODEJS_VERSION:
-  - "17"
+  - "18"

.ci/.jenkins_nodejs.yml
@@ -1,4 +1,5 @@
 NODEJS_VERSION:
+  - "17"
   - "16"
   - "16.0"
   - "14"

.ci/.jenkins_tav_nodejs.yml
@@ -1,4 +1,5 @@
 NODEJS_VERSION:
+  - "17"
   - "16"
   - "14"
   - "12"

.github/workflows/test.yml
@@ -120,6 +120,7 @@ jobs:
     strategy:
       matrix:
         node:
+          - '17'
           - '16'
           - '16.0'
           - '14'
@@ -138,4 +139,5 @@ jobs:
         node-version: ${{ matrix.node }}
     - run: docker ps  # show the services against which we'll be testing
     - run: npm install
+    - run: npm ls --all || true
     - run: npm test

lib/instrumentation/modules/mongodb.js
@@ -3,8 +3,9 @@
 const semver = require('semver')
 const { getDBDestination } = require('../context')
 
-// Match expected `<hostname>:<port>`.
-const HOSTNAME_PORT_RE = /^([^:]+):(\d+)$/
+// Match expected `<hostname>:<port>`, e.g. "mongo:27017", "::1:27017",
+// "127.0.0.1:27017".
+const HOSTNAME_PORT_RE = /^(.+):(\d+)$/
 
 module.exports = (mongodb, agent, { version, enabled }) => {
   if (!enabled) return mongodb

test/instrumentation/modules/fastify/_async-await.js
@@ -30,7 +30,7 @@ test('transaction name', function (t) {
 
   fastify.listen(0, function (err, address) {
     t.error(err)
-    address = address || 'http://localhost:' + fastify.server.address().port
+    address = 'http://localhost:' + fastify.server.address().port
     http.get(`${address}/hello/world`, function (res) {
       const chunks = []
       res.on('data', chunks.push.bind(chunks))
@@ -81,7 +81,7 @@ if (semver.gte(fastifyVersion, '2.0.0-rc')) {
 
     fastify.listen(0, function (err, address) {
       t.error(err)
-      http.get(`${address}/hello/world`, function (res) {
+      http.get(`http://localhost:${fastify.server.address().port}/hello/world`, function (res) {
         const chunks = []
         res.on('data', chunks.push.bind(chunks))
         res.on('end', function () {

test/instrumentation/modules/http2.test.js
@@ -25,7 +25,7 @@ isSecure.forEach(secure => {
   var method = secure ? 'createSecureServer' : 'createServer'
 
   test(`http2.${method} compatibility mode`, t => {
-    t.plan(15)
+    t.plan(16)
 
     // Note NODE_OPTIONS env because it sometimes has a setting relevant
     // for this test.
@@ -71,7 +71,7 @@ isSecure.forEach(secure => {
   })
 
   test(`http2.${method} stream respond`, t => {
-    t.plan(15)
+    t.plan(16)
 
     resetAgent((data) => {
       assert(t, data, secure, port)
@@ -114,7 +114,7 @@ isSecure.forEach(secure => {
   })
 
   test(`http2.${method} stream respondWithFD`, t => {
-    t.plan(16)
+    t.plan(17)
 
     resetAgent((data) => {
       assert(t, data, secure, port)
@@ -164,7 +164,7 @@ isSecure.forEach(secure => {
   })
 
   test(`http2.${method} stream respondWithFile`, t => {
-    t.plan(15)
+    t.plan(16)
 
     resetAgent((data) => {
       assert(t, data, secure, port)
@@ -278,7 +278,7 @@ isSecure.forEach(secure => {
   })
 
   test(`http2.request${secure ? ' secure' : ' '}`, t => {
-    t.plan(34)
+    t.plan(36)
 
     resetAgent(3, (data) => {
       t.strictEqual(data.transactions.length, 2)
@@ -478,17 +478,25 @@ function assertPath (t, trans, secure, port, path, httpVersion) {
       }
       break
   }
-  // Sometime between node (v8.6.0, v8.17.0] there was a fix such that
-  // socket.remoteAddress was set properly for https.
-  const expectedRemoteAddress = httpVersion === '1.1' && semver.lt(process.version, '8.17.0')
-    ? undefined : '::ffff:127.0.0.1'
+
+  // What is "expected" for transaction.context.request.socket.remote_address
+  // is a bit of a pain.
+  if (httpVersion === '1.1' && semver.lt(process.version, '8.17.0')) {
+    // Before node v8.17.0 `socket.remoteAddress` was not set for https.
+    t.pass(`skip checking on transaction.context.request.socket.remote_address on node ${process.version}`)
+  } else {
+    // With node v17 on Linux (or at least on the linux containers used for
+    // GitHub Action tests), the localhost socket.remoteAddress is "::1".
+    t.ok(trans.context.request.socket.remote_address === '::ffff:127.0.0.1' || trans.context.request.socket.remote_address === '::1',
+      'transaction.context.request.socket.remote_address is as expected: ' + trans.context.request.socket.remote_address)
+  }
+  delete trans.context.request.socket.remote_address
 
   t.deepEqual(trans.context.request, {
     http_version: httpVersion,
     method: 'GET',
     url: expectedUrl,
     socket: {
-      remote_address: expectedRemoteAddress,
       encrypted: secure
     },
     headers: expectedReqHeaders

commit 169356876f1f8a3f1b37670cee752ef188b947a6

tests: fix TAV test breakage with node 17 (hapi <=16, fastify <0.36.0) (#2387)
This was broken in #2380. Versions of hapi <=16 did not support the
'host' option to `new Server`, but instead to `server.connection`.
Also really old versions of fastify (v0.35.7) cannot even be npm installed
with the npm included in node v17. This drops support for fastify <1.0.0
and updates the TAV test matrix to only test a given fastify version
with its supported versions of node per https://www.fastify.io/docs/latest/LTS/

.tav.yml
@@ -407,8 +407,28 @@ restify-new:
     - node test/instrumentation/modules/restify/basic.test.js
     - node test/instrumentation/modules/restify/set-framework.test.js
 
+# https://www.fastify.io/docs/latest/LTS/
+# - #1086 suggests fastify@2.4.0 was a broken release, skip it.
+fastify-v1:
+  name: fastify
+  versions: '1.x'
+  node: '>=6 <12'
+  commands:
+    - node test/instrumentation/modules/fastify/fastify.test.js
+    - node test/instrumentation/modules/fastify/async-await.test.js
+    - node test/instrumentation/modules/fastify/set-framework.test.js
+fastify-v2:
+  name: fastify
+  versions: '>=2.0.0 <2.4.0 || >2.4.0 <3'
+  node: '>=6 <15'
+  commands:
+    - node test/instrumentation/modules/fastify/fastify.test.js
+    - node test/instrumentation/modules/fastify/async-await.test.js
+    - node test/instrumentation/modules/fastify/set-framework.test.js
 fastify:
-  versions: '>=0.27.0 <0.29.0 || >0.29.0 <2.4.0 || >2.4.0'
+  name: fastify
+  versions: '>=3.0.0'
+  node: '>=10'
   commands:
     - node test/instrumentation/modules/fastify/fastify.test.js
     - node test/instrumentation/modules/fastify/async-await.test.js

docs/fastify.asciidoc
@@ -121,6 +121,7 @@ include::./shared-set-up.asciidoc[tag=add-your-own-data]
 ==== Compatibility
 
 include::./shared-set-up.asciidoc[tag=compatibility-link]
+See also: https://www.fastify.io/docs/latest/LTS/[Fastify's own LTS documentation.]
 
 [float]
 [[fastify-troubleshooting]]

docs/supported-technologies.asciidoc
@@ -46,7 +46,7 @@ These are the frameworks that we officially support:
 router information for full support. We currently support the most popular Koa router called
 https://github.com/alexmingoia/koa-router[koa-router].
 |<<restify,Restify>> |>=5.2.0
-|<<fastify,Fastify>> |>=0.27.0
+|<<fastify,Fastify>> |>=1.0.0; see also https://www.fastify.io/docs/latest/LTS/[Fastify's own LTS documentation]
 |=======================================================================
 
 [float]

lib/instrumentation/modules/fastify.js
@@ -1,5 +1,8 @@
 'use strict'
 
+// Instrumentation of fastify.
+// https://www.fastify.io/docs/latest/LTS/
+
 const semver = require('semver')
 
 module.exports = function (fastify, agent, { version, enabled }) {

test/instrumentation/modules/hapi/shared.js
@@ -524,9 +524,16 @@ module.exports = (moduleName) => {
   })
 
   function makeServer (opts) {
-    var server = new Hapi.Server({ host: 'localhost' })
+    // Specify 'localhost' to avoid Hapi default of '0.0.0.0' which ties to
+    // IPv4. We want a later HTTP client request using 'localhost' to work.
+    var server
     if (semver.satisfies(pkg.version, '<17')) {
+      server = new Hapi.Server()
+      opts = opts || {}
+      opts.host = opts.host || 'localhost'
       server.connection(opts)
+    } else {
+      server = new Hapi.Server({ host: 'localhost' })
     }
     return server
   }