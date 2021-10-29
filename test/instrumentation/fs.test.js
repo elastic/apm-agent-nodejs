9084f08f5339d18d39a8c4ed530fd72c86daca05
Trent Mick <trent.mick@elastic.co>
2021-10-29 09:00:55 -0700

    fix: wrap 'fs.realpath.native' when configured with asyncHooks=false (#2403)
    Before this `fs.realpath.native` would be undefined after wrapping
    of `fs.realpath`.
    
    Fixes: #2401

---
+++ test/instrumentation/fs.test.js
@@ -0,0 +1,33 @@
+'use strict'
+
+// Some testing of run-context tracking through 'fs' methods.
+
+const apm = require('../..').start({
+  serviceName: 'test-fs',
+  captureExceptions: false,
+  metricsInterval: '0s',
+  centralConfig: false,
+  cloudProvider: 'none',
+  disableSend: true,
+  asyncHooks: false
+})
+
+const fs = require('fs')
+
+const tape = require('tape')
+
+if (typeof fs.realpath.native === 'function') {
+  // Before https://github.com/elastic/apm-agent-nodejs/issues/2401 this test
+  // would crash with asyncHooks=false
+  tape.test('fs.realpath.native', function (t) {
+    var trans = apm.startTransaction('t0')
+    var span = apm.startSpan('s1')
+    fs.realpath.native(__filename, function (err, resolvedPath) {
+      t.error(err, 'no error from fs.realpath.native')
+      t.equal(apm.currentSpan, span, 'apm.currentSpan is as expected')
+      span.end()
+      trans.end()
+      t.end()
+    })
+  })
+}
