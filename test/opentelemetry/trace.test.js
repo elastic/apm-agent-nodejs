5c1ae7a42b4ecca5e8019e7d6c079127f7b8f312
Alan Storm <alan.storm@elastic.co>
2022-01-18 19:52:56 -0800

    feat: pre-context structure

--- test/opentelemetry/trace.test.js
+++ test/opentelemetry/trace.test.js
@@ -1,17 +1,30 @@
 'use strict'
+const agent = require('../..').start({
+  serviceName: 'test',
+  secretToken: 'test',
+  captureExceptions: false,
+  metricsInterval: 0,
+  centralConfig: false,
+  transactionSampleRate: 1
+})
+
 const tape = require('tape')
 const {trace, context} = require('@opentelemetry/api')
 const { ConsoleSpanExporter, SimpleSpanProcessor } = require('@opentelemetry/sdk-trace-base');
 
-const {ElasticNodeTracerProvider} = require('../lib/opentelemetry/bridge')
-
+const ElasticNodeTracerProvider = require('../../lib/opentelemetry/elastic-node-tracer-provider')
 
 function createTestExporter() {
-  return new ConsoleSpanExporter()
+  return {
+    export:function(){}
+  }
+  // return new ConsoleSpanExporter()
 }
 
 tape.test(function(t){
-  const provider = new ElasticNodeTracerProvider();
+  const provider = new ElasticNodeTracerProvider({});
+  provider.setAgent(agent)
+
   provider.addSpanProcessor(
     new SimpleSpanProcessor(
       createTestExporter()
@@ -21,14 +34,13 @@ tape.test(function(t){
 
   const ctx = context.active()
   const tracer = trace.getTracer('foo')
-  tracer.startActiveSpan('test',{},ctx, function(span1){
-    console.log("this is a test")
+  // console.log(tracer)
+  tracer.startActiveSpan('test', {}, ctx, function(span1){
+    // console.log("this is a test")
     const ctx2 = context.active()
-    console.log(span1)
     const span2 = tracer.startSpan('test2',{},ctx2)
-    span2.end()
-    span1.end()
-    // console.log(span.end())
+    // span2.end()
+    // span1.end()
   })
 
   t.ok(true)
