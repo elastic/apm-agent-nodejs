elastic/apm-agent-nodejs

commit 520a90113a56e6df0485b16ba067934dd243a33b

feat: introduce "exit spans" and mark S3 spans as exit spans (#2552)
Per https://github.com/elastic/apm/blob/main/specs/agents/tracing-spans.md#exit-spans
the result is that HTTP child spans of S3 spans are no longer captured.

Also:
- Add http context to the S3 spans, now that the http spans are gone some
  of this info might be helpful.
- Update TAV versions for aws-sdk to something more reasonable.

Closes: #2125

lib/instrumentation/span.js
@@ -17,17 +17,20 @@ module.exports = Span
 util.inherits(Span, GenericSpan)
 
 function Span (transaction, name, ...args) {
-  const defaultChildOf = transaction._agent._instrumentation.currSpan() || transaction
   const opts = typeof args[args.length - 1] === 'object'
     ? (args.pop() || {})
     : {}
 
   if (!opts.childOf) {
+    const defaultChildOf = transaction._agent._instrumentation.currSpan() || transaction
     opts.childOf = defaultChildOf
     opts.timer = defaultChildOf._timer
   } else if (opts.childOf._timer) {
     opts.timer = opts.childOf._timer
   }
+
+  this._exitSpan = !!opts.exitSpan
+  delete opts.exitSpan
 
   GenericSpan.call(this, transaction._agent, ...args, opts)
 

.tav.yml
@@ -524,8 +524,8 @@ aws-sdk:
   # Maintenance note: This should be updated periodically using:
   #   ./dev-utils/aws-sdk-tav-versions.sh
   #
-  # Test v2.858.0, every N=31 of 159 releases, and current latest.
-  versions: '2.858.0 || 2.889.0 || 2.920.0 || 2.951.0 || 2.982.0 || 2.1013.0 || 2.1016.0 || >2.1016.0 <3'
+  # Test v2.858.0, every N=41 of 209 releases, and current latest.
+  versions: '2.858.0 || 2.899.0 || 2.940.0 || 2.981.0 || 2.1022.0 || 2.1063.0 || 2.1066.0 || >2.1066.0 <3'
   commands:
     - node test/instrumentation/modules/aws-sdk/aws4-retries.test.js
     - node test/instrumentation/modules/aws-sdk/s3.test.js

docs/agent-api.asciidoc
@@ -643,6 +643,12 @@ Sub-millisecond precision can be achieved using decimals.
 If not provided,
 the current time will be used
 
+** `exitSpan` +{type-boolean}+ Make an "exit span".
+Exit spans represent outgoing communication. They are used to create a node
+in the {kibana-ref}/service-maps.html[Service Map] and a downstream service
+in the {kibana-ref}/dependencies.html[Dependencies Table]. The provided subtype
+will be used as the downstream service name.
+
 Start and return a new custom span associated with the current active transaction.
 This is the same as getting the current transaction with `apm.currentTransaction` and,
 if a transaction was found,

docs/transaction-api.asciidoc
@@ -113,6 +113,12 @@ Sub-millisecond precision can be achieved using decimals.
 If not provided,
 the current time will be used
 
+** `exitSpan` +{type-boolean}+ Make an "exit span".
+Exit spans represent outgoing communication. They are used to create a node
+in the {kibana-ref}/service-maps.html[Service Map] and a downstream service
+in the {kibana-ref}/dependencies.html[Dependencies Table]. The provided subtype
+will be used as the downstream service name.
+
 Start and return a new custom span associated with this transaction.
 When a span is started it will measure the time until <<span-end,`span.end()`>> is called.
 

index.d.ts
@@ -347,6 +347,7 @@ declare namespace apm {
   export interface SpanOptions {
     startTime?: number;
     childOf?: Transaction | Span | string;
+    exitSpan?: boolean;
   }
 
   type CaptureBody = 'off' | 'errors' | 'transactions' | 'all';

lib/agent.js
@@ -171,7 +171,7 @@ Agent.prototype.setTransactionOutcome = function (outcome) {
   return this._instrumentation.setTransactionOutcome.apply(this._instrumentation, arguments)
 }
 
-Agent.prototype.startSpan = function (name, type, subtype, action, { childOf } = {}) {
+Agent.prototype.startSpan = function (name, type, subtype, action, { startTime, childOf, exitSpan } = {}) {
   return this._instrumentation.startSpan.apply(this._instrumentation, arguments)
 }
 

lib/instrumentation/http-shared.js
@@ -146,12 +146,12 @@ exports.traceOutgoingRequest = function (agent, moduleName, method) {
 
       if (!options.headers) options.headers = {}
 
-      // Attempt to use the span context as a traceparent header.
-      // If the transaction is unsampled the span will not exist,
-      // however a traceparent header must still be propagated
-      // to indicate requested services should not be sampled.
-      // Use the transaction context as the parent, in this case.
-      var parent = span || ins.currTransaction()
+      // W3C trace-context propagation.
+      // There are a number of reasons why `span` might be null: child of an
+      // exit span, `transactionMaxSpans` was hit, unsampled transaction, etc.
+      // If so, then fallback to the current run context's span or transaction,
+      // if any.
+      var parent = span || parentRunContext.currSpan() || parentRunContext.currTransaction()
       if (parent && parent._context) {
         const headerValue = parent._context.toTraceParentString()
         const traceStateValue = parent._context.toTraceStateString()

lib/instrumentation/index.js
@@ -83,7 +83,7 @@ Instrumentation.prototype.currTransaction = function () {
   if (!this._started) {
     return null
   }
-  return this._runCtxMgr.active().currTransaction() || null
+  return this._runCtxMgr.active().currTransaction()
 }
 
 Instrumentation.prototype.currSpan = function () {

commit fd1d154cbc7e6754681769ae74fa27ddd0aa4122

fix: `apm.createSpan()` with no args would create bogus span.name
This fixes a bug recently introduced in commit 520a90113a5 where
`apm.createSpan()` with no args would result in a span with span.name
set to a SpanOptions value (and the options would not be applied).

lib/instrumentation/span.js
@@ -16,10 +16,16 @@ module.exports = Span
 
 util.inherits(Span, GenericSpan)
 
-function Span (transaction, name, ...args) {
+// new Span(transaction)
+// new Span(transaction, name?, opts?)
+// new Span(transaction, name?, type?, opts?)
+// new Span(transaction, name?, type?, subtype?, opts?)
+// new Span(transaction, name?, type?, subtype?, action?, opts?)
+function Span (transaction, ...args) {
   const opts = typeof args[args.length - 1] === 'object'
     ? (args.pop() || {})
     : {}
+  const [name, ...tsaArgs] = args // "tsa" === Type, Subtype, Action
 
   if (!opts.childOf) {
     const defaultChildOf = transaction._agent._instrumentation.currSpan() || transaction
@@ -32,7 +38,7 @@ function Span (transaction, name, ...args) {
   this._exitSpan = !!opts.exitSpan
   delete opts.exitSpan
 
-  GenericSpan.call(this, transaction._agent, ...args, opts)
+  GenericSpan.call(this, transaction._agent, ...tsaArgs, opts)
 
   this._db = null
   this._http = null

test/agent.test.js
@@ -403,6 +403,19 @@ test('#startSpan()', function (t) {
     t.end()
   })
 
+  t.test('startSpan with no name results in span.name="unnamed"', function (t) {
+    const agent = new Agent().start(agentOptsNoopTransport)
+    agent.startTransaction()
+    var span = agent.startSpan()
+    t.ok(span, 'should return a span')
+    t.strictEqual(span.name, 'unnamed')
+    t.strictEqual(span.type, null)
+    t.strictEqual(span.subtype, null)
+    t.strictEqual(span.action, null)
+    agent.destroy()
+    t.end()
+  })
+
   t.test('options.startTime', function (t) {
     const agent = new Agent().start(agentOptsNoopTransport)
     agent.startTransaction()