9c09f42c5fa2e8dc7588d944dc400247a19da1f6
Victor Martinez <VictorMartinezRubio@gmail.com>
2022-03-01 09:38:31 +0000

    publish: use build-aws for all the files

--- .ci/scripts/create-arn-table.sh
+++ .ci/scripts/create-arn-table.sh
@@ -4,8 +4,8 @@ set -o pipefail
 #
 # Create the AWS ARN table given the below environment variables:
 #
-# - AWS_FOLDER      - that's the location of the publish-layer-version output for each region
-#	- SUFFIX_ARN_FILE - that's the output file.
+# - AWS_FOLDER      - that's the location of the publish-layer-version output for each region.
+#	- SUFFIX_ARN_FILE - that's the output file to be stored in the AWS_FOLDER.
 #
 
 ARN_FILE=${SUFFIX_ARN_FILE}
@@ -15,12 +15,12 @@ ARN_FILE=${SUFFIX_ARN_FILE}
 	echo ''
 	echo '|Region|ARN|'
 	echo '|------|---|'
-} > "${ARN_FILE}"
+} > "${AWS_FOLDER}/${ARN_FILE}"
 
 for f in $(ls "${AWS_FOLDER}"); do
 	LAYER_VERSION_ARN=$(grep '"LayerVersionArn"' "$AWS_FOLDER/${f}" | cut -d":" -f2- | sed 's/ //g' | sed 's/"//g' | cut -d"," -f1)
 	echo "INFO: create-arn-table ARN(${LAYER_VERSION_ARN}):region(${f}))"
-	echo "|${f}|${LAYER_VERSION_ARN}|" >> "${ARN_FILE}"
+	echo "|${f}|${LAYER_VERSION_ARN}|" >> "${AWS_FOLDER}/${ARN_FILE}"
 done
 
-echo '' >> "${ARN_FILE}"
+echo '' >> "${AWS_FOLDER}/${ARN_FILE}"
