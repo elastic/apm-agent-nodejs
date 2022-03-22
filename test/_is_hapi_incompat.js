19e3ae30077337a6660328b578360b8ecec0fc79
Trent Mick <trent.mick@elastic.co>
2022-03-22 16:38:20 -0700

    instrumentation of a POST to a Hapi server using hapi@18 (i.e. deprecated legacy hapi) and node >=16 *hangs*. No need to support this.

--- test/_is_hapi_incompat.js
+++ test/_is_hapi_incompat.js
@@ -12,6 +12,12 @@ function isHapiIncompat (moduleName) {
   if (semver.lt(process.version, '8.9.0') && semver.gte(hapiVersion, '17.0.0')) {
     return true
   }
+  // hapi 18.1.0 (the last hapi 18.x) was released before node v12 was released,
+  // and tests with hapi@18 and node >=16 is known to hang.  (Also, hapi <20 is
+  // deprecated, https://github.com/hapijs/hapi/issues/4114.)
+  if (semver.gte(process.version, '16.0.0') && semver.lt(hapiVersion, '19.0.0')) {
+    return true
+  }
   // hapi 19+ requires Node.js 12 or higher
   if (semver.lt(process.version, '12.0.0') && semver.gte(hapiVersion, '19.0.0')) {
     return true
