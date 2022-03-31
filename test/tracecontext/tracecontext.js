430c1cff85214e3676619122e5e0169575435f95
Alan Storm <alan.storm@elastic.co>
2022-03-31 10:49:19 -0700

    feat: inject/propagate headers api for context

---
+++ test/tracecontext/tracecontext.js
@@ -0,0 +1,26 @@
+'use strict'
+const tape = require('tape')
+const { TraceContext } = require('../../lib/tracecontext')
+const TraceState = require('../../lib/tracecontext/tracestate')
+const TraceParent = require('traceparent')
+
+tape.test('trace context tests', function (suite) {
+  suite.test('propagateTraceContextHeaders tests', function (t) {
+    const traceParentString = '00-d3ced7e155ca7d275540a77e6ed5f931-ee2afc1f78c2cfa6-01'
+    const traceStateString = 'foo=34f067aa0ba902b7,bar=0.25,es=a:b;cee:de,34@ree=xxxy'
+
+    const fromContext = new TraceContext(
+      TraceParent.fromString(traceParentString),
+      TraceState.fromStringFormatString(traceStateString)
+    )
+    const context = new TraceContext()
+    const headers = {}
+
+    t.true(!context.hasPropagatedTraceContextHeaders())
+    const newHeaders = context.propagateTraceContextHeaders(headers, fromContext)
+    t.equals(traceParentString, newHeaders.traceparent)
+    t.equals(traceStateString, newHeaders.tracestate)
+    t.true(context.hasPropagatedTraceContextHeaders())
+    t.end()
+  })
+})
