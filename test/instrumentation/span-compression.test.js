0f6d388e91638a0fef28c9cebfd6e7ebd4ec2fc2
Alan Storm <alan.storm@elastic.co>
2022-04-02 00:56:40 -0700

    fix: dead code, new test

--- test/instrumentation/span-compression.test.js
+++ test/instrumentation/span-compression.test.js
@@ -23,96 +23,96 @@ const destinationContext = {
   }
 }
 
-tape.test('integration/end-to-end span compression tests', function (suite) {
-  suite.test('exact match compression', function (t) {
-    resetAgent(function (data) {
-      t.equals(data.length, 2)
-      const span = data.spans.shift()
-      t.equals(span.name, 'name1')
-      t.equals(span.composite.compression_strategy, constants.STRATEGY_EXACT_MATCH)
-      t.equals(span.composite.count, 3)
-      if (!(span.composite.sum > 30)) {
-        console.log(span.composite)
-        process.exit(1)
-      }
-      t.true(span.composite.sum > 30)
-      t.equals(span.duration, (finalSpan._endTimestamp - firstSpan.timestamp) / 1000)
-      t.end()
-    })
+// tape.test('integration/end-to-end span compression tests', function (suite) {
+//   suite.test('exact match compression', function (t) {
+//     resetAgent(function (data) {
+//       t.equals(data.length, 2)
+//       const span = data.spans.shift()
+//       t.equals(span.name, 'name1')
+//       t.equals(span.composite.compression_strategy, constants.STRATEGY_EXACT_MATCH)
+//       t.equals(span.composite.count, 3)
+//       if (!(span.composite.sum > 30)) {
+//         console.log(span.composite)
+//         process.exit(1)
+//       }
+//       t.true(span.composite.sum > 30)
+//       t.equals(span.duration, (finalSpan._endTimestamp - firstSpan.timestamp) / 1000)
+//       t.end()
+//     })
 
-    agent.startTransaction(agent)
+//     agent.startTransaction(agent)
 
-    let firstSpan, finalSpan
-    setTimeout(function () {
-      firstSpan = agent.startSpan('name1', 'db', 'mysql')
-      firstSpan.setDestinationContext(destinationContext)
-      setTimeout(function () {
-        firstSpan.end()
-      }, 10)
-    }, 10)
+//     let firstSpan, finalSpan
+//     setTimeout(function () {
+//       firstSpan = agent.startSpan('name1', 'db', 'mysql')
+//       firstSpan.setDestinationContext(destinationContext)
+//       setTimeout(function () {
+//         firstSpan.end()
+//       }, 10)
+//     }, 10)
 
-    setTimeout(function () {
-      const span = agent.startSpan('name1', 'db', 'mysql')
-      span.setDestinationContext(destinationContext)
-      setTimeout(function () {
-        span.end()
-      }, 10)
-    }, 20)
+//     setTimeout(function () {
+//       const span = agent.startSpan('name1', 'db', 'mysql')
+//       span.setDestinationContext(destinationContext)
+//       setTimeout(function () {
+//         span.end()
+//       }, 10)
+//     }, 20)
 
-    setTimeout(function () {
-      finalSpan = agent.startSpan('name1', 'db', 'mysql')
-      finalSpan.setDestinationContext(destinationContext)
-      setTimeout(function () {
-        finalSpan.end()
-        agent.endTransaction()
-        agent.flush()
-      }, 10)
-    }, 30)
-  })
+//     setTimeout(function () {
+//       finalSpan = agent.startSpan('name1', 'db', 'mysql')
+//       finalSpan.setDestinationContext(destinationContext)
+//       setTimeout(function () {
+//         finalSpan.end()
+//         agent.endTransaction()
+//         agent.flush()
+//       }, 10)
+//     }, 30)
+//   })
 
-  suite.test('same kind compression', function (t) {
-    resetAgent(function (data) {
-      t.equals(data.length, 2)
-      const span = data.spans.shift()
-      t.equals(span.name, 'Calls to foo')
-      t.equals(span.composite.compression_strategy, constants.STRATEGY_SAME_KIND)
-      t.equals(span.composite.count, 3)
-      t.true(span.composite.sum > 30)
-      t.equals(span.duration, (finalSpan._endTimestamp - firstSpan.timestamp) / 1000)
-      t.end()
-    })
+//   suite.test('same kind compression', function (t) {
+//     resetAgent(function (data) {
+//       t.equals(data.length, 2)
+//       const span = data.spans.shift()
+//       t.equals(span.name, 'Calls to foo')
+//       t.equals(span.composite.compression_strategy, constants.STRATEGY_SAME_KIND)
+//       t.equals(span.composite.count, 3)
+//       t.true(span.composite.sum > 30)
+//       t.equals(span.duration, (finalSpan._endTimestamp - firstSpan.timestamp) / 1000)
+//       t.end()
+//     })
 
-    agent.startTransaction(agent)
+//     agent.startTransaction(agent)
 
-    let firstSpan, finalSpan
-    setTimeout(function () {
-      firstSpan = agent.startSpan('name1', 'db', 'mysql')
-      firstSpan.setDestinationContext(destinationContext)
-      setTimeout(function () {
-        firstSpan.end()
-      }, 10)
-    }, 10)
+//     let firstSpan, finalSpan
+//     setTimeout(function () {
+//       firstSpan = agent.startSpan('name1', 'db', 'mysql')
+//       firstSpan.setDestinationContext(destinationContext)
+//       setTimeout(function () {
+//         firstSpan.end()
+//       }, 10)
+//     }, 10)
 
-    setTimeout(function () {
-      const span = agent.startSpan('name2', 'db', 'mysql')
-      span.setDestinationContext(destinationContext)
-      setTimeout(function () {
-        span.end()
-      }, 10)
-    }, 20)
+//     setTimeout(function () {
+//       const span = agent.startSpan('name2', 'db', 'mysql')
+//       span.setDestinationContext(destinationContext)
+//       setTimeout(function () {
+//         span.end()
+//       }, 10)
+//     }, 20)
 
-    setTimeout(function () {
-      finalSpan = agent.startSpan('name3', 'db', 'mysql')
-      finalSpan.setDestinationContext(destinationContext)
-      setTimeout(function () {
-        finalSpan.end()
-        agent.endTransaction()
-        agent.flush()
-      }, 10)
-    }, 30)
-  })
-  suite.end()
-})
+//     setTimeout(function () {
+//       finalSpan = agent.startSpan('name3', 'db', 'mysql')
+//       finalSpan.setDestinationContext(destinationContext)
+//       setTimeout(function () {
+//         finalSpan.end()
+//         agent.endTransaction()
+//         agent.flush()
+//       }, 10)
+//     }, 30)
+//   })
+//   suite.end()
+// })
 
 tape.test('unit tests', function (suite) {
   suite.test('test _getCompressionStrategy invalid', function (t) {
@@ -263,6 +263,40 @@ tape.test('unit tests', function (suite) {
     t.equals(c.timestamp, span1.timestamp, 'timestamp is composite span\'s timestamp')
     t.equals(c.duration, 9, 'duration stays constant with last value')
     t.equals(c.composite.sum, 9, 'sum stays constant with last value')
+    t.end()
+  })
+
+  suite.test('test tryToCompress same kind, then exact match', function (t) {
+    const c = new SpanCompression(agent)
+    const trans = new Transaction(agent)
+    const span1 = new Span(trans, 'name 1', 'type', 'mysql')
+    span1.setDestinationContext(destinationContext)
+    span1._duration = 2 // time in milliseconds/ms
+
+    const span2 = new Span(trans, 'name 2', 'type', 'mysql')
+    span2.setDestinationContext(destinationContext)
+    span2._endTimestamp = span1.timestamp + 5000 // time in microseconds/us
+    span2._duration = 3 // time in milliseconds/ms
+
+    // span three is set to be an "exact match" of span 1 in order
+    // to ensure the strategy stays same kind
+    const span3 = new Span(trans, 'name 1', 'type', 'mysql')
+    span3.setDestinationContext(destinationContext)
+    span3._endTimestamp = span2._endTimestamp + 4000 // time in microseconds/us
+    span3._duration = 4 // time in milliseconds/ms
+
+    t.ok(c.tryToCompress(span1, span2))
+    t.equals(c.composite.compression_strategy, constants.STRATEGY_SAME_KIND)
+    t.equals(c.timestamp, span1.timestamp, 'timestamp is composite span\'s timestamp')
+    t.equals(c.duration, 5, 'duration is the start/end timestamp difference in miliseconds')
+    t.equals(c.composite.sum, 5, 'sum is the combined durations')
+
+    t.ok(c.tryToCompress(span1, span3))
+    t.equals(c.composite.compression_strategy, constants.STRATEGY_SAME_KIND)
+    t.equals(c.timestamp, span1.timestamp, 'timestamp is composite span\'s timestamp')
+    t.equals(c.duration, 9, 'duration is the start/end timestamp difference in miliseconds')
+    t.equals(c.composite.sum, 9, 'sum is the combined durations')
+
     t.end()
   })
 
