577dd5961c27d2e4c75dbcf45ea4562af11589cf
Trent Mick <trent.mick@elastic.co>
2022-04-07 17:00:35 -0700

    Clean up this example a bit.

--- examples/otelbridge1/example-http-request.js
+++ examples/otelbridge1/example-http-request.js
@@ -1,43 +1,27 @@
 // Based on https://github.com/open-telemetry/opentelemetry-js/blob/main/examples/http/client.js
-/* eslint-disable */
+'use strict'
+const otel = require('@opentelemetry/api')
+const tracer = otel.trace.getTracer('example-http-request')
 
-'use strict';
+const http = require('http')
 
-const api = require('@opentelemetry/api');
-// api.diag.setLogger({
-//   verbose() { console.log('diag VERBOSE:', ...arguments) },
-//   debug() { console.log('diag DEBUG:', ...arguments) },
-//   info() { console.log('diag INFO:', ...arguments) },
-//   warn() { console.log('diag WARN:', ...arguments) },
-//   error() { console.log('diag ERROR:', ...arguments) }
-// }, api.DiagLogLevel.ALL)
-
-
-const tracer = api.trace.getTracer('example-http-request')
-
-const http = require('http');
-
-/** A function which makes requests and handles response. */
-function makeRequest() {
-  // span corresponds to outgoing requests. Here, we have manually created
-  // the span, which is created to track work that happens outside of the
-  // request lifecycle entirely.
-  const span = tracer.startSpan('makeRequest');
-  api.context.with(api.trace.setSpan(api.context.active(), span), () => {
-    // XXX TODO sanity check what RunContext and OTelContext are here. Should be the 'makeRequest' transaction.
+function makeRequest () {
+  const span = tracer.startSpan('makeRequest')
+  otel.context.with(otel.trace.setSpan(otel.context.active(), span), () => {
     http.get({
       host: 'httpstat.us',
       path: '/200',
       headers: { accept: '*/*' }
     }, (response) => {
-      const body = [];
-      response.on('data', (chunk) => body.push(chunk));
+      console.log('STATUS:', response.statusCode)
+      const body = []
+      response.on('data', (chunk) => body.push(chunk))
       response.on('end', () => {
-        console.log('BODY:', body.toString());
-        span.end();
-      });
-    });
-  });
+        console.log('BODY:', body.toString())
+        span.end()
+      })
+    })
+  })
 }
 
-makeRequest();
+makeRequest()
