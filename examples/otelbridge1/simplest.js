c0e15f46fa7586ec12df40efbefc43c20ba42a0e
Trent Mick <trent.mick@elastic.co>
2022-04-07 17:36:19 -0700

    fix 'make check'

--- examples/otelbridge1/simplest.js
+++ examples/otelbridge1/simplest.js
@@ -1,7 +1,14 @@
-const api = require('@opentelemetry/api');
+'use strict'
+
+// A simple OTel API-using script.
+// Usage:
+//    node -r ./otel-tracing.js simplest.js                      // using the OTel JS SDK
+//    node -r elastic-apm-node/opentelemetry-sdk.js simplest.js  // using Elastic APM
+
+const api = require('@opentelemetry/api')
 const tracer = api.trace.getTracer('simplest')
-function makeRequest() {
-  const span = tracer.startSpan('makeRequest');
-  span.end();
+function makeRequest () {
+  const span = tracer.startSpan('makeRequest')
+  span.end()
 }
-makeRequest();
+makeRequest()
