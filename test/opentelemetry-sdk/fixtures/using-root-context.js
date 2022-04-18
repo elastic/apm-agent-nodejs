9727baa6d258e7063d2c473047930892e0d18c3a
Trent Mick <trent.mick@elastic.co>
2022-04-18 13:25:15 -0700

    support otel.ROOT_CONTEXT usage to otel.context.with()
    Also implement the {get,set,delete}Value methods on RunContext so that
    OTel Context values are now propagated.

---
+++ test/opentelemetry-sdk/fixtures/using-root-context.js
@@ -0,0 +1,43 @@
+'use strict'
+
+// Expect:
+//   transaction "s2" (trace_id=d4cda95b652f4a1592b449dd92ffda3b, parent_id=6e0c63ffe4e34c42)
+
+// This tests context propagation with direct usage of `otel.ROOT_CONTEXT`.
+//
+// This is an "interesting" case to test because `otel.ROOT_CONTEXT` and
+// `Context` objects derived from it are a class implemented in
+// `@opentelemetry/api` that cannot be overriden. This is as opposed to
+// `Context` objects derived from `api.context.active()` -- which calls into
+// the global ContextManager for creation.
+
+const assert = require('assert')
+const otel = require('@opentelemetry/api')
+const tracer = otel.trace.getTracer('test-using-root-context')
+
+const parentSpanContext = {
+  traceId: 'd4cda95b652f4a1592b449dd92ffda3b',
+  spanId: '6e0c63ffe4e34c42',
+  traceFlags: otel.TraceFlags.SAMPLED
+}
+const s1 = otel.trace.wrapSpanContext(parentSpanContext)
+
+let ctx
+ctx = otel.trace.setSpan(otel.ROOT_CONTEXT, s1) // -> BaseContext
+
+// Also set an arbitrary value on the Context to test that it is properly
+// propagated.
+const fooKey = otel.createContextKey('foo')
+ctx = ctx.setValue(fooKey, 'bar')
+
+otel.context.with(ctx, () => {
+  assert.strictEqual(otel.context.active().getValue(fooKey), 'bar',
+    'the active context has our fooKey value')
+
+  const s2 = tracer.startSpan('s2')
+  assert(s2.isRecording() === true, 's2 is recording')
+  assert.strictEqual(s2.spanContext().traceId, parentSpanContext.traceId, 's2 traceId inherited from s1')
+  assert.strictEqual(s2.parentSpanId /* OTel SDK */ || s2._span.parentId /* Elastic APM */,
+    parentSpanContext.spanId, 's2 parent is s1')
+  s2.end()
+})
--- test/opentelemetry-sdk/opentelemetry-sdk.test.js
+++ test/opentelemetry-sdk/opentelemetry-sdk.test.js
@@ -25,8 +25,8 @@ const cases = [
     //   transaction "mySpan"
     script: 'start-span.js',
     check: (t, events) => {
-      t.ok(events[0].metadata, 'APM server got event metadata object')
       t.equal(events.length, 2, 'exactly 2 events')
+      t.ok(events[0].metadata, 'APM server got event metadata object')
       const mySpan = findObjInArray(events, 'transaction.name', 'mySpan').transaction
       t.ok(mySpan, 'transaction.name')
       // XXX what else to assert here? outcome? type? OTel attributes?
@@ -37,8 +37,21 @@ const cases = [
     //   transaction "s2" (trace_id=d4cda95b652f4a1592b449dd92ffda3b, parent_id=6e0c63ffe4e34c42)
     script: 'nonrecordingspan-parent.js',
     check: (t, events) => {
-      t.ok(events[0].metadata, 'APM server got event metadata object')
       t.equal(events.length, 2, 'exactly 2 events')
+      t.ok(events[0].metadata, 'APM server got event metadata object')
+      const s2 = findObjInArray(events, 'transaction.name', 's2').transaction
+      t.ok(s2, 'transaction.name')
+      t.equal(s2.trace_id, 'd4cda95b652f4a1592b449dd92ffda3b', 'transaction.trace_id')
+      t.ok(s2.parent_id, '6e0c63ffe4e34c42', 'transaction.parent_id')
+    }
+  },
+  {
+    // Expect:
+    //   transaction "s2" (trace_id=d4cda95b652f4a1592b449dd92ffda3b, parent_id=6e0c63ffe4e34c42)
+    script: 'using-root-context.js',
+    check: (t, events) => {
+      t.equal(events.length, 2, 'exactly 2 events')
+      t.ok(events[0].metadata, 'APM server got event metadata object')
       const s2 = findObjInArray(events, 'transaction.name', 's2').transaction
       t.ok(s2, 'transaction.name')
       t.equal(s2.trace_id, 'd4cda95b652f4a1592b449dd92ffda3b', 'transaction.trace_id')
