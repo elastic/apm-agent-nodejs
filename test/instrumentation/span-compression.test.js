0073f1d966d6616a7eed1473c7d3eca935f108f9
Trent Mick <trent.mick@elastic.co>
2022-04-19 13:49:01 -0700

    test: add some context to test assert to help with debugging it (#2647)

--- test/instrumentation/span-compression.test.js
+++ test/instrumentation/span-compression.test.js
@@ -73,7 +73,7 @@ tape.test('integration/end-to-end span compression tests', function (suite) {
       t.equals(span.name, 'Calls to foo')
       t.equals(span.composite.compression_strategy, constants.STRATEGY_SAME_KIND)
       t.equals(span.composite.count, 3)
-      t.true(span.composite.sum > 30)
+      t.true(span.composite.sum > 30, `span.composite.sum > 30: ${span.composite.sum}`)
       t.equals(span.duration, (finalSpan._endTimestamp - firstSpan.timestamp) / 1000)
       t.end()
     })
