87b2157c483bd6a0fcad078678ad4ae04ba7c6f6
Trent Mick <trent.mick@elastic.co>
2022-07-21 16:51:50 -0700

    fix 'make check'

--- examples/esbuild/src/index.js
+++ examples/esbuild/src/index.js
@@ -1,11 +1,17 @@
-const apm = require('elastic-apm-node').start()
-const http = require('http');
+/*
+ * Copyright Elasticsearch B.V. and other contributors where applicable.
+ * Licensed under the BSD 2-Clause License; you may not use this file except in
+ * compliance with the BSD 2-Clause License.
+ */
+
+require('elastic-apm-node').start()
+
+const http = require('http')
 const { handleRequest } = require('./handler')
 
 // Start a simple HTTP server.
-const server = http.createServer(handleRequest);
+const server = http.createServer(handleRequest)
 server.listen(3000, () => {
-
   // Make a single request and then stop.
   http.get('http://localhost:3000', res => {
     console.log('CLIENT: res.headers:', res.headers)
@@ -17,5 +23,4 @@ server.listen(3000, () => {
       server.close()
     })
   })
-
 })
