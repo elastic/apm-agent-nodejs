63651b3671bfcbfe1d822706ee18c6c8f5d48a4c
Trent Mick <trent.mick@elastic.co>
2022-09-07 12:35:06 -0700

    fix this test for http changes for context.destination

--- test/instrumentation/context.test.js
+++ test/instrumentation/context.test.js
@@ -12,12 +12,7 @@ var { getHTTPDestination, getDBDestination } = require('../../lib/instrumentatio
 test('#getHTTPDestination', function (t) {
   t.test('username and pass', (t) => {
     const url = 'http://user:pass@testing.local:1234/path?query'
-    t.deepEqual(getHTTPDestination(url, 'external'), {
-      service: {
-        name: 'http://testing.local:1234',
-        resource: 'testing.local:1234',
-        type: 'external'
-      },
+    t.deepEqual(getHTTPDestination(url), {
       address: 'testing.local',
       port: 1234
     })
@@ -26,13 +21,7 @@ test('#getHTTPDestination', function (t) {
 
   t.test('https with custom port', () => {
     const url = 'https://example.com:2222'
-    t.deepEqual(getHTTPDestination(url, 'external'), {
-      service:
-      {
-        name: 'https://example.com:2222',
-        resource: 'example.com:2222',
-        type: 'external'
-      },
+    t.deepEqual(getHTTPDestination(url), {
       address: 'example.com',
       port: 2222
     })
@@ -41,12 +30,7 @@ test('#getHTTPDestination', function (t) {
 
   t.test('https default port', (t) => {
     const url = 'https://www.elastic.co/products/apm'
-    t.deepEqual(getHTTPDestination(url, 'external'), {
-      service: {
-        name: 'https://www.elastic.co',
-        resource: 'www.elastic.co:443',
-        type: 'external'
-      },
+    t.deepEqual(getHTTPDestination(url), {
       address: 'www.elastic.co',
       port: 443
     })
@@ -55,12 +39,7 @@ test('#getHTTPDestination', function (t) {
 
   t.test('http default port', (t) => {
     const url = 'http://www.elastic.co/products/apm'
-    t.deepEqual(getHTTPDestination(url, 'external'), {
-      service: {
-        name: 'http://www.elastic.co',
-        resource: 'www.elastic.co:80',
-        type: 'external'
-      },
+    t.deepEqual(getHTTPDestination(url), {
       address: 'www.elastic.co',
       port: 80
     })
@@ -69,12 +48,7 @@ test('#getHTTPDestination', function (t) {
 
   t.test('ipv6', (t) => {
     const url = 'http://[::1]'
-    t.deepEqual(getHTTPDestination(url, 'external'), {
-      service: {
-        name: 'http://[::1]',
-        resource: '[::1]:80',
-        type: 'external'
-      },
+    t.deepEqual(getHTTPDestination(url), {
       address: '::1',
       port: 80
     })
@@ -83,12 +57,7 @@ test('#getHTTPDestination', function (t) {
 
   t.test('ipv6 https custom port', (t) => {
     const url = 'https://[::1]:80/'
-    t.deepEqual(getHTTPDestination(url, 'external'), {
-      service: {
-        name: 'https://[::1]:80',
-        resource: '[::1]:80',
-        type: 'external'
-      },
+    t.deepEqual(getHTTPDestination(url), {
       address: '::1',
       port: 80
     })
