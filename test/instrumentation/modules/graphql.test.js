elastic/apm-agent-nodejs

commit b098c9fa6dbba76064e97e408e4ebd5f65140b99

feat: graphql v16 support (#2761)
graphql@16 removed support for positional arguments to

Fixes: #2508
Co-authored-by: Trent Mick <trent.mick@elastic.co>

.tav.yml
@@ -156,10 +156,17 @@ ws-new:
   node: '>=8.6.0'
   versions: '>=7 <8'
   commands: node test/instrumentation/modules/ws.test.js
+
+graphql-v0.7-v16:
+  preinstall: npm uninstall express-graphql
+  versions: '>=0.7.0 <0.11.0 || >=0.11.1 <16'
+  commands: node test/instrumentation/modules/graphql.test.js
 graphql:
   preinstall: npm uninstall express-graphql
-  versions: '>=0.7.0 <0.11.0 || >=0.11.1 <16.0.0'
+  node: '>=12'
+  versions: '>=16.0.0 <17'
   commands: node test/instrumentation/modules/graphql.test.js
+
 express:
   versions: '>=4'
   commands:

lib/instrumentation/modules/graphql.js
@@ -7,7 +7,7 @@ var getPathFromRequest = require('../express-utils').getPathFromRequest
 
 module.exports = function (graphql, agent, { version, enabled }) {
   if (!enabled) return graphql
-  if (!semver.satisfies(version, '>=0.7.0 <16.0.0 || ^14.0.0-rc') ||
+  if (!semver.satisfies(version, '>=0.7.0 <17') ||
       !graphql.Kind ||
       typeof graphql.Source !== 'function' ||
       typeof graphql.parse !== 'function' ||
@@ -41,19 +41,36 @@ module.exports = function (graphql, agent, { version, enabled }) {
   })
 
   function wrapGraphql (orig) {
-    return function wrappedGraphql (schema, requestString, rootValue, contextValue, variableValues, operationName) {
+    return function wrappedGraphql (args) {
       agent.logger.debug('intercepted call to graphql.graphql')
       const span = ins.createSpan('GraphQL: Unknown Query', 'db', 'graphql', 'execute')
       if (!span) {
         return orig.apply(this, arguments)
       }
 
-      var source = new graphql.Source(requestString || '', 'GraphQL request')
-      if (source) {
+      let schema
+      let source
+      let operationName
+      if (arguments.length === 1) {
+        schema = args.schema
+        source = args.source
+        operationName = args.operationName
+      } else {
+        // graphql <=v15 supported positional arguments:
+        //    function (schema, source, rootValue, contextValue, variableValues, operationName)
+        schema = arguments[0]
+        source = arguments[1]
+        operationName = arguments[5]
+      }
+
+      const sourceObj = (typeof source === 'string'
+        ? new graphql.Source(source || '', 'GraphQL request')
+        : source)
+      if (sourceObj) {
         var documentAST
 
         try {
-          documentAST = graphql.parse(source)
+          documentAST = graphql.parse(sourceObj)
         } catch (syntaxError) {
           agent.logger.debug('graphql.parse(source) failed - skipping graphql query extraction')
         }
@@ -79,12 +96,24 @@ module.exports = function (graphql, agent, { version, enabled }) {
   }
 
   function wrapExecute (orig) {
-    function wrappedExecuteImpl (schema, document, rootValue, contextValue, variableValues, operationName) {
+    return function wrappedExecute (args) {
       agent.logger.debug('intercepted call to graphql.execute')
       const span = ins.createSpan('GraphQL: Unknown Query', 'db', 'graphql', 'execute')
       if (!span) {
         agent.logger.debug('no active transaction found - skipping graphql tracing')
         return orig.apply(this, arguments)
+      }
+
+      let document
+      let operationName
+      if (arguments.length === 1) {
+        document = args.document
+        operationName = args.operationName
+      } else {
+        // graphql <=v15 supported positional arguments:
+        //    function (schema, document, rootValue, contextValue, variableValues, operationName)
+        document = arguments[1]
+        operationName = arguments[5]
       }
 
       var details = extractDetails(document, operationName)
@@ -118,26 +147,6 @@ module.exports = function (graphql, agent, { version, enabled }) {
         span.end()
       }
       return p
-    }
-
-    return function wrappedExecute (argsOrSchema, document, rootValue, contextValue, variableValues, operationName) {
-      return arguments.length === 1
-        ? wrappedExecuteImpl(
-          argsOrSchema.schema,
-          argsOrSchema.document,
-          argsOrSchema.rootValue,
-          argsOrSchema.contextValue,
-          argsOrSchema.variableValues,
-          argsOrSchema.operationName
-        )
-        : wrappedExecuteImpl(
-          argsOrSchema,
-          document,
-          rootValue,
-          contextValue,
-          variableValues,
-          operationName
-        )
     }
   }
 

test/instrumentation/modules/graphql.test.js
@@ -8,18 +8,48 @@ var agent = require('../../..').start({
   centralConfig: false
 })
 
+const semver = require('semver')
+const graphqlVer = require('graphql/package.json').version
+if (semver.satisfies(graphqlVer, '>=16') && !semver.satisfies(process.version, '>=12')) {
+  console.log(`# SKIP graphql@${graphqlVer} is incompatible with node ${process.version}`)
+  process.exit()
+}
+
 var graphql = require('graphql')
-var pkg = require('graphql/package.json')
-var semver = require('semver')
 var test = require('tape')
 
 var mockClient = require('../../_mock_http_client')
 
-test('graphql.graphql', function (t) {
+if (semver.lt(graphqlVer, '16.0.0')) {
+  // graphql@16 dropped support for positional arguments.
+  test('graphql.graphql(...) - positional args', function (t) {
+    resetAgent(done(t))
+
+    var schema = graphql.buildSchema('type Query { hello: String }')
+    var rootValue = {
+      hello () {
+        return 'Hello world!'
+      }
+    }
+    var query = '{ hello }'
+
+    agent.startTransaction('foo')
+
+    graphql.graphql(schema, query, rootValue).then(function (response) {
+      t.ok(agent.currentSpan === null, 'no currentSpan .graphql().then(...)')
+      agent.endTransaction()
+      t.deepLooseEqual(response, { data: { hello: 'Hello world!' } })
+      agent.flush()
+    })
+    t.ok(agent.currentSpan === null, 'no currentSpan in sync code after .graphql(...)')
+  })
+}
+
+test('graphql.graphql(...) - single GraphQLArgs arg', function (t) {
   resetAgent(done(t))
 
   var schema = graphql.buildSchema('type Query { hello: String }')
-  var root = {
+  var rootValue = {
     hello () {
       return 'Hello world!'
     }
@@ -28,7 +58,7 @@ test('graphql.graphql', function (t) {
 
   agent.startTransaction('foo')
 
-  graphql.graphql(schema, query, root).then(function (response) {
+  graphql.graphql({ schema, source: query, rootValue }).then(function (response) {
     t.ok(agent.currentSpan === null, 'no currentSpan .graphql().then(...)')
     agent.endTransaction()
     t.deepLooseEqual(response, { data: { hello: 'Hello world!' } })
@@ -41,7 +71,7 @@ test('graphql.graphql - invalid query', function (t) {
   resetAgent(done(t, 'Unknown Query'))
 
   var schema = graphql.buildSchema('type Query { hello: String }')
-  var root = {
+  var rootValue = {
     hello () {
       return 'Hello world!'
     }
@@ -50,7 +80,7 @@ test('graphql.graphql - invalid query', function (t) {
 
   agent.startTransaction('foo')
 
-  graphql.graphql(schema, query, root).then(function (response) {
+  graphql.graphql({ schema, source: query, rootValue }).then(function (response) {
     t.ok(agent.currentSpan === null, 'no currentSpan .graphql().then(...)')
     agent.endTransaction()
     t.deepEqual(Object.keys(response), ['errors'])
@@ -75,7 +105,7 @@ test('graphql.graphql - transaction ended', function (t) {
   })
 
   var schema = graphql.buildSchema('type Query { hello: String }')
-  var root = {
+  var rootValue = {
     hello () {
       return 'Hello world!'
     }
@@ -84,36 +114,39 @@ test('graphql.graphql - transaction ended', function (t) {
 
   agent.startTransaction('foo').end()
 
-  graphql.graphql(schema, query, root).then(function (response) {
+  graphql.graphql({ schema, source: query, rootValue }).then(function (response) {
     t.ok(agent.currentSpan === null, 'no currentSpan .graphql().then(...)')
     t.deepLooseEqual(response, { data: { hello: 'Hello world!' } })
   })
   t.ok(agent.currentSpan === null, 'no currentSpan in sync code after .graphql(...)')
 })
 
-test('graphql.execute', function (t) {
-  resetAgent(done(t))
+if (semver.lt(graphqlVer, '16.0.0')) {
+  // graphql@16 dropped support for positional arguments.
+  test('graphql.execute(...) - positional args', function (t) {
+    resetAgent(done(t))
 
-  var schema = graphql.buildSchema('type Query { hello: String }')
-  var root = {
-    hello () {
-      return Promise.resolve('Hello world!')
+    var schema = graphql.buildSchema('type Query { hello: String }')
+    var rootValue = {
+      hello () {
+        return Promise.resolve('Hello world!')
+      }
     }
-  }
-  var query = '{ hello }'
-  var source = new graphql.Source(query)
-  var documentAST = graphql.parse(source)
+    var query = '{ hello }'
+    var source = new graphql.Source(query)
+    var documentAST = graphql.parse(source)
 
-  agent.startTransaction('foo')
+    agent.startTransaction('foo')
 
-  graphql.execute(schema, documentAST, root).then(function (response) {
-    t.ok(agent.currentSpan === null, 'no currentSpan .execute().then(...)')
-    agent.endTransaction()
-    t.deepLooseEqual(response, { data: { hello: 'Hello world!' } })
-    agent.flush()
+    graphql.execute(schema, documentAST, rootValue).then(function (response) {
+      t.ok(agent.currentSpan === null, 'no currentSpan .execute().then(...)')
+      agent.endTransaction()
+      t.deepLooseEqual(response, { data: { hello: 'Hello world!' } })
+      agent.flush()
+    })
+    t.ok(agent.currentSpan === null, 'no currentSpan in sync code after .execute(...)')
   })
-  t.ok(agent.currentSpan === null, 'no currentSpan in sync code after .execute(...)')
-})
+}
 
 test('graphql.execute - transaction ended', function (t) {
   t.plan(7)
@@ -129,29 +162,29 @@ test('graphql.execute - transaction ended', function (t) {
   })
 
   var schema = graphql.buildSchema('type Query { hello: String }')
-  var root = {
+  var rootValue = {
     hello () {
       return Promise.resolve('Hello world!')
     }
   }
   var query = '{ hello }'
   var source = new graphql.Source(query)
-  var documentAST = graphql.parse(source)
+  var document = graphql.parse(source)
 
   agent.startTransaction('foo').end()
 
-  graphql.execute(schema, documentAST, root).then(function (response) {
+  graphql.execute({ schema, document, rootValue }).then(function (response) {
     t.ok(agent.currentSpan === null, 'no currentSpan .execute().then(...)')
     t.deepLooseEqual(response, { data: { hello: 'Hello world!' } })
   })
   t.ok(agent.currentSpan === null, 'no currentSpan in sync code after .execute(...)')
 })
 
-test('graphql.execute args object', function (t) {
+test('graphql.execute(...) - single ExecutionArgs arg', function (t) {
   resetAgent(done(t))
 
   var schema = graphql.buildSchema('type Query { hello: String }')
-  var root = {
+  var rootValue = {
     hello () {
       return Promise.resolve('Hello world!')
     }
@@ -162,7 +195,7 @@ test('graphql.execute args object', function (t) {
   var args = {
     schema: schema,
     document: documentAST,
-    rootValue: root
+    rootValue: rootValue
   }
 
   agent.startTransaction('foo')
@@ -176,23 +209,23 @@ test('graphql.execute args object', function (t) {
   t.ok(agent.currentSpan === null, 'no currentSpan in sync code after .execute(...)')
 })
 
-if (semver.satisfies(pkg.version, '>=0.12')) {
+if (semver.satisfies(graphqlVer, '>=0.12')) {
   test('graphql.execute sync', function (t) {
     resetAgent(done(t))
 
     var schema = graphql.buildSchema('type Query { hello: String }')
-    var root = {
+    var rootValue = {
       hello () {
         return 'Hello world!'
       }
     }
     var query = '{ hello }'
     var source = new graphql.Source(query)
-    var documentAST = graphql.parse(source)
+    var document = graphql.parse(source)
 
     agent.startTransaction('foo')
 
-    var response = graphql.execute(schema, documentAST, root)
+    var response = graphql.execute({ schema, document, rootValue })
     t.ok(agent.currentSpan === null, 'no currentSpan in sync code after .execute(...)')
 
     agent.endTransaction()

docs/supported-technologies.asciidoc
@@ -77,7 +77,7 @@ The Node.js agent will automatically instrument the following modules to give yo
 |https://www.npmjs.com/package/cassandra-driver[cassandra-driver] |>=3.0.0 <5 |Will instrument all queries
 |https://www.npmjs.com/package/elasticsearch[elasticsearch] |>=8.0.0 |Will instrument all queries
 |https://www.npmjs.com/package/@elastic/elasticsearch[@elastic/elasticsearch] |>=7.0.0 <9.0.0 |Will instrument all queries
-|https://www.npmjs.com/package/graphql[graphql] |>=0.7.0 <16.0.0 |Will instrument all queries
+|https://www.npmjs.com/package/graphql[graphql] |>=0.7.0 <17 |Will instrument all queries
 |https://www.npmjs.com/package/handlebars[handlebars] |* |Will instrument compile and render calls
 |https://www.npmjs.com/package/jade[jade] |>=0.5.6 |Will instrument compile and render calls; Deprecated. No longer tested. Use pug.
 |https://www.npmjs.com/package/pug[pug] |'>=0.1.0' |Will instrument compile and render calls

test/instrumentation/modules/express-graphql.test.js
@@ -19,6 +19,13 @@ if (semver.gte(expressGraphqlVersion, '0.10.0') && semver.lt(process.version, '1
   process.exit()
 }
 
+// graphql@16 crashes with node <12.
+const graphqlVer = require('graphql/package.json').version
+if (semver.satisfies(graphqlVer, '>=16') && !semver.satisfies(process.version, '>=12')) {
+  console.log(`# SKIP graphql@${graphqlVer} is incompatible with node ${process.version}`)
+  process.exit()
+}
+
 var http = require('http')
 
 var buildSchema = require('graphql').buildSchema

commit 39674bfa07f06f9119b68c1b9341c8c69ad53e49

fix: graphql instrumentation for versions <=0.9 were broken
The changes in #2761 to support graphql@16 accidentally broken
instrumentation for really old graphql (versions before 0.10.0).
This wasn't noticed because the .tav.yml block for older graphql
versions didn't have `name: graphql`, so it was never run in CI.

.tav.yml
@@ -159,10 +159,12 @@ ws-new:
   commands: node test/instrumentation/modules/ws.test.js
 
 graphql-v0.7-v16:
+  name: graphql
   preinstall: npm uninstall express-graphql
   versions: '>=0.7.0 <0.11.0 || >=0.11.1 <16'
   commands: node test/instrumentation/modules/graphql.test.js
 graphql:
+  name: graphql
   preinstall: npm uninstall express-graphql
   node: '>=12'
   versions: '>=16.0.0 <17'

lib/instrumentation/modules/graphql.js
@@ -23,6 +23,19 @@ module.exports = function (graphql, agent, { version, enabled }) {
     return graphql
   }
 
+  // Over the many versions the `graphql()` and `execute()` functions have
+  // changed in what arguments they accept:
+  // - Initially they only supported positional arguments:
+  //      function graphql(schema, requestString, rootValue, contextValue, variableValues, operationName)
+  // - Starting in v0.10.0 (in #356), they started accepting a second form where
+  //   all fields were passed in an object as the first argument:
+  //      function graphql(argsOrSchema, source, rootValue, contextValue, variableValues, operationName, fieldResolver)
+  //   and `arguments.length === 1` was used as the test to determine which.
+  // - Starting in v16 (in #2904), they dropped positional arguments:
+  //      function graphql(args)
+  const onlySupportsPositionalArgs = semver.lt(version, '0.10.0')
+  const onlySupportsSingleArg = semver.gte(version, '16.0.0')
+
   const ins = agent._instrumentation
 
   return clone({}, graphql, {
@@ -57,13 +70,12 @@ module.exports = function (graphql, agent, { version, enabled }) {
       let schema
       let source
       let operationName
-      if (arguments.length === 1) {
+      const singleArgForm = onlySupportsSingleArg || (!onlySupportsPositionalArgs && arguments.length === 1)
+      if (singleArgForm) {
         schema = args.schema
         source = args.source
         operationName = args.operationName
       } else {
-        // graphql <=v15 supported positional arguments:
-        //    function (schema, source, rootValue, contextValue, variableValues, operationName)
         schema = arguments[0]
         source = arguments[1]
         operationName = arguments[5]
@@ -112,12 +124,11 @@ module.exports = function (graphql, agent, { version, enabled }) {
 
       let document
       let operationName
-      if (arguments.length === 1) {
+      const singleArgForm = onlySupportsSingleArg || (!onlySupportsPositionalArgs && arguments.length === 1)
+      if (singleArgForm) {
         document = args.document
         operationName = args.operationName
       } else {
-        // graphql <=v15 supported positional arguments:
-        //    function (schema, document, rootValue, contextValue, variableValues, operationName)
         document = arguments[1]
         operationName = arguments[5]
       }

test/instrumentation/modules/graphql.test.js
@@ -26,7 +26,11 @@ var test = require('tape')
 
 var mockClient = require('../../_mock_http_client')
 
-if (semver.lt(graphqlVer, '16.0.0')) {
+// See explanation of these in "lib/instrumentation/modules/graphql.js".
+const onlySupportsPositionalArgs = semver.lt(graphqlVer, '0.10.0')
+const onlySupportsSingleArg = semver.gte(graphqlVer, '16.0.0')
+
+if (!onlySupportsSingleArg) {
   // graphql@16 dropped support for positional arguments.
   test('graphql.graphql(...) - positional args', function (t) {
     resetAgent(done(t))
@@ -51,27 +55,29 @@ if (semver.lt(graphqlVer, '16.0.0')) {
   })
 }
 
-test('graphql.graphql(...) - single GraphQLArgs arg', function (t) {
-  resetAgent(done(t))
+if (!onlySupportsPositionalArgs) {
+  test('graphql.graphql(...) - single GraphQLArgs arg', function (t) {
+    resetAgent(done(t))
 
-  var schema = graphql.buildSchema('type Query { hello: String }')
-  var rootValue = {
-    hello () {
-      return 'Hello world!'
+    var schema = graphql.buildSchema('type Query { hello: String }')
+    var rootValue = {
+      hello () {
+        return 'Hello world!'
+      }
     }
-  }
-  var query = '{ hello }'
+    var query = '{ hello }'
 
-  agent.startTransaction('foo')
+    agent.startTransaction('foo')
 
-  graphql.graphql({ schema, source: query, rootValue }).then(function (response) {
-    t.ok(agent.currentSpan === null, 'no currentSpan .graphql().then(...)')
-    agent.endTransaction()
-    t.deepLooseEqual(response, { data: { hello: 'Hello world!' } })
-    agent.flush()
+    graphql.graphql({ schema, source: query, rootValue }).then(function (response) {
+      t.ok(agent.currentSpan === null, 'no currentSpan .graphql().then(...)')
+      agent.endTransaction()
+      t.deepLooseEqual(response, { data: { hello: 'Hello world!' } })
+      agent.flush()
+    })
+    t.ok(agent.currentSpan === null, 'no currentSpan in sync code after .graphql(...)')
   })
-  t.ok(agent.currentSpan === null, 'no currentSpan in sync code after .graphql(...)')
-})
+}
 
 test('graphql.graphql - invalid query', function (t) {
   resetAgent(done(t, 'Unknown Query'))
@@ -86,7 +92,7 @@ test('graphql.graphql - invalid query', function (t) {
 
   agent.startTransaction('foo')
 
-  graphql.graphql({ schema, source: query, rootValue }).then(function (response) {
+  graphql.graphql(...buildGraphqlArgs({ schema, source: query, rootValue })).then(function (response) {
     t.ok(agent.currentSpan === null, 'no currentSpan .graphql().then(...)')
     agent.endTransaction()
     t.deepEqual(Object.keys(response), ['errors'])
@@ -120,14 +126,14 @@ test('graphql.graphql - transaction ended', function (t) {
 
   agent.startTransaction('foo').end()
 
-  graphql.graphql({ schema, source: query, rootValue }).then(function (response) {
+  graphql.graphql(...buildGraphqlArgs({ schema, source: query, rootValue })).then(function (response) {
     t.ok(agent.currentSpan === null, 'no currentSpan .graphql().then(...)')
     t.deepLooseEqual(response, { data: { hello: 'Hello world!' } })
   })
   t.ok(agent.currentSpan === null, 'no currentSpan in sync code after .graphql(...)')
 })
 
-if (semver.lt(graphqlVer, '16.0.0')) {
+if (!onlySupportsSingleArg) {
   // graphql@16 dropped support for positional arguments.
   test('graphql.execute(...) - positional args', function (t) {
     resetAgent(done(t))
@@ -179,41 +185,43 @@ test('graphql.execute - transaction ended', function (t) {
 
   agent.startTransaction('foo').end()
 
-  graphql.execute({ schema, document, rootValue }).then(function (response) {
+  graphql.execute(...buildExecuteArgs({ schema, document, rootValue })).then(function (response) {
     t.ok(agent.currentSpan === null, 'no currentSpan .execute().then(...)')
     t.deepLooseEqual(response, { data: { hello: 'Hello world!' } })
   })
   t.ok(agent.currentSpan === null, 'no currentSpan in sync code after .execute(...)')
 })
 
-test('graphql.execute(...) - single ExecutionArgs arg', function (t) {
-  resetAgent(done(t))
+if (!onlySupportsPositionalArgs) {
+  test('graphql.execute(...) - single ExecutionArgs arg', function (t) {
+    resetAgent(done(t))
 
-  var schema = graphql.buildSchema('type Query { hello: String }')
-  var rootValue = {
-    hello () {
-      return Promise.resolve('Hello world!')
+    var schema = graphql.buildSchema('type Query { hello: String }')
+    var rootValue = {
+      hello () {
+        return Promise.resolve('Hello world!')
+      }
     }
-  }
-  var query = '{ hello }'
-  var source = new graphql.Source(query)
-  var documentAST = graphql.parse(source)
-  var args = {
-    schema: schema,
-    document: documentAST,
-    rootValue: rootValue
-  }
+    var query = '{ hello }'
+    var source = new graphql.Source(query)
+    var documentAST = graphql.parse(source)
+    var args = {
+      schema: schema,
+      document: documentAST,
+      rootValue: rootValue
+    }
 
-  agent.startTransaction('foo')
+    agent.startTransaction('foo')
 
-  graphql.execute(args).then(function (response) {
-    t.ok(agent.currentSpan === null, 'no currentSpan .execute().then(...)')
-    agent.endTransaction()
-    t.deepLooseEqual(response, { data: { hello: 'Hello world!' } })
-    agent.flush()
+    graphql.execute(args).then(function (response) {
+      t.ok(agent.currentSpan === null, 'no currentSpan .execute().then(...)')
+      agent.endTransaction()
+      t.deepLooseEqual(response, { data: { hello: 'Hello world!' } })
+      agent.flush()
+    })
+    t.ok(agent.currentSpan === null, 'no currentSpan in sync code after .execute(...)')
   })
-  t.ok(agent.currentSpan === null, 'no currentSpan in sync code after .execute(...)')
-})
+}
 
 if (semver.satisfies(graphqlVer, '>=0.12')) {
   test('graphql.execute sync', function (t) {
@@ -231,7 +239,7 @@ if (semver.satisfies(graphqlVer, '>=0.12')) {
 
     agent.startTransaction('foo')
 
-    var response = graphql.execute({ schema, document, rootValue })
+    var response = graphql.execute(...buildExecuteArgs({ schema, document, rootValue }))
     t.ok(agent.currentSpan === null, 'no currentSpan in sync code after .execute(...)')
 
     agent.endTransaction()
@@ -261,6 +269,41 @@ function done (t, spanNameSuffix) {
     t.ok(offset + span.duration * 1000 < trans.duration * 1000)
 
     t.end()
+  }
+}
+
+// Take the modern (single object argument) calling signature for
+// `graphql.graphql()` and return an arguments array that can be used to
+// call it with whatever the appropriate call signature is for the current
+// graphql version.
+function buildGraphqlArgs (args) {
+  if (onlySupportsPositionalArgs) {
+    const {
+      schema,
+      source,
+      rootValue,
+      contextValue,
+      variableValues,
+      operationName,
+      fieldResolver,
+      typeResolver
+    } = args
+    return [schema, source, rootValue, contextValue, variableValues, operationName, fieldResolver, typeResolver].filter(a => a !== undefined)
+  } else {
+    return [args]
+  }
+}
+
+// Take the modern (single object argument) calling signature for
+// `graphql.execute()` and return an arguments array that can be used to
+// call it with whatever the appropriate call signature is for the current
+// graphql version.
+function buildExecuteArgs (args) {
+  if (onlySupportsPositionalArgs) {
+    const { schema, document, variableValues, rootValue } = args
+    return [schema, document, variableValues, rootValue].filter(a => a !== undefined)
+  } else {
+    return [args]
   }
 }
 