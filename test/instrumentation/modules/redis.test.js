3ffa5f221d6b51142528fd10ad2db39d21a276bf
Trent Mick <trent.mick@elastic.co>
2022-10-19 10:25:22 -0700

    test: drop the 'transBeforeClient' testing for redis >=4 tests
    The 'transBeforeClient' part of tests for redis <4 were to ensure the
    instrumentation was not creating *two* spans as a side-effect of
    node-redis's queuing commands before "ready" and calling
    `RedisClient.internal_send_command` twice. The redis@4 instrumentation
    doesn't shim any internal redis commands that are re-called for
    queued commands.
    
    This also drops a test block that was never called in redis.test.js
    (for redis <=2.4.2).

--- test/instrumentation/modules/redis.test.js
+++ test/instrumentation/modules/redis.test.js
@@ -30,31 +30,19 @@ const agent = require('../../..').start({
 const redis = require('redis')
 const test = require('tape')
 
-const findObjInArray = require('../../_utils').findObjInArray
 const mockClient = require('../../_mock_http_client')
 
 test('redis', function (t) {
   resetAgent(function (data) {
-    t.strictEqual(data.transactions.length, 2, 'have 2 transactions')
-
-    // We expect a 'transBeforeClient' transaction, and we want to ensure it
-    // does *not* have spans for each of the client commands. It *possibly*
-    // (with contextManager="patch" it doesn't) has an "INFO" span for the
-    // internal INFO command the RedisClient setup does.
-    let trans = findObjInArray(data.transactions, 'name', 'transBeforeClient')
-    t.ok(trans, 'have "transBeforeClient" transaction')
-    let spans = data.spans.filter(s => s.transaction_id === trans.id)
-      .filter(s => s.name !== 'INFO')
-    t.equal(spans.length, 0, 'there are no non-INFO spans in the "transBeforeClient" transaction')
+    t.strictEqual(data.transactions.length, 1, 'have 1 transaction')
+    const trans = data.transactions[0]
+    t.ok(trans.name, 'aTrans', 'trans.name')
+    t.strictEqual(trans.result, 'success', 'trans.result')
 
     // Sort the remaining spans by timestamp, because asynchronous-span.end()
     // means they can be set to APM server out of order.
-    spans = data.spans
-      .filter(s => s.transaction_id !== trans.id)
+    const spans = data.spans
       .sort((a, b) => { return a.timestamp < b.timestamp ? -1 : 1 })
-    trans = findObjInArray(data.transactions, 'name', 'transAfterClient')
-    t.ok(trans, 'have "transAfterClient" transaction')
-    t.strictEqual(trans.result, 'success', 'trans.result')
 
     const expectedSpanNames = [
       'FLUSHALL',
@@ -89,16 +77,6 @@ test('redis', function (t) {
     t.end()
   })
 
-  // If a redis client is not yet "ready" it will queue client commands, and
-  // then send them after "ready". Internally this results in calling
-  // RedisClient.internal_send_command *twice* for each queued command. If the
-  // instrumentation isn't careful, it is easy to instrument both of those
-  // calls. The latter call will only result in a span if there is a
-  // currentTransaction for the async task in which the redis client is created.
-  // That's what `transBeforeClient` is: to make sure we *don't* get
-  // double-spans.
-  const transBeforeClient = agent.startTransaction('transBeforeClient')
-
   const client = redis.createClient({
     socket: {
       port: '6379',
@@ -107,7 +85,7 @@ test('redis', function (t) {
   })
   client.connect()
 
-  const transAfterClient = agent.startTransaction('transAfterClient')
+  const trans = agent.startTransaction('aTrans')
 
   client.flushAll().then(function (reply) {
     t.strictEqual(reply, 'OK', 'reply is OK')
@@ -145,8 +123,7 @@ test('redis', function (t) {
       done++
       t.strictEqual(done, 4, 'done 4 callbacks')
 
-      transAfterClient.end()
-      transBeforeClient.end()
+      trans.end()
       client.quit()
       agent.flush()
     }).catch(function (err) {
@@ -156,42 +133,6 @@ test('redis', function (t) {
     t.error(err, 'no flushall error')
   })
 })
-
-if (semver.satisfies(redisVersion, '<=2.4.2')) {
-  // Redis <=2.4.2 allowed a callback as the last item in an args array.
-  // Support for this was dropped in commit 60eee34de1.
-  test('client.cmd([args..., myCb]) call signature', function (t) {
-    let nCbCalled = 0
-    function myCb () {
-      nCbCalled++
-    }
-
-    resetAgent(function (data) {
-      t.equal(nCbCalled, 1, 'myCb was called the expected number of times')
-      t.equal(data.transactions.length, 1, 'got 1 transaction')
-      t.equal(data.spans.length, 1, 'got 1 span')
-      t.equal(data.spans[0].name, 'GET', 'span name is GET')
-      t.end()
-    })
-
-    const client = redis.createClient({
-      socket: {
-        port: '6379',
-        host: process.env.REDIS_HOST
-      }
-    })
-
-    client.on('ready', function () {
-      const t0 = agent.startTransaction('t0')
-
-      client.get(['k', myCb])
-
-      t0.end()
-      client.quit()
-      agent.flush()
-    })
-  })
-}
 
 function resetAgent (cb) {
   agent._instrumentation.testReset()
--- test/instrumentation/modules/redis4-legacy.test.js
+++ test/instrumentation/modules/redis4-legacy.test.js
@@ -31,31 +31,19 @@ const agent = require('../../..').start({
 const redis = require('redis')
 const test = require('tape')
 
-const findObjInArray = require('../../_utils').findObjInArray
 const mockClient = require('../../_mock_http_client')
 
 test('redis', function (t) {
   resetAgent(function (data) {
-    t.strictEqual(data.transactions.length, 2, 'have 2 transactions')
-
-    // We expect a 'transBeforeClient' transaction, and we want to ensure it
-    // does *not* have spans for each of the client commands. It *possibly*
-    // (with contextManager="patch" it doesn't) has an "INFO" span for the
-    // internal INFO command the RedisClient setup does.
-    let trans = findObjInArray(data.transactions, 'name', 'transBeforeClient')
-    t.ok(trans, 'have "transBeforeClient" transaction')
-    let spans = data.spans.filter(s => s.transaction_id === trans.id)
-      .filter(s => s.name !== 'INFO')
-    t.equal(spans.length, 0, 'there are no non-INFO spans in the "transBeforeClient" transaction')
+    t.strictEqual(data.transactions.length, 1, 'have 1 transaction')
+    const trans = data.transactions[0]
+    t.ok(trans.name, 'aTrans', 'trans.name')
+    t.strictEqual(trans.result, 'success', 'trans.result')
 
     // Sort the remaining spans by timestamp, because asynchronous-span.end()
     // means they can be set to APM server out of order.
-    spans = data.spans
-      .filter(s => s.transaction_id !== trans.id)
+    const spans = data.spans
       .sort((a, b) => { return a.timestamp < b.timestamp ? -1 : 1 })
-    trans = findObjInArray(data.transactions, 'name', 'transAfterClient')
-    t.ok(trans, 'have "transAfterClient" transaction')
-    t.strictEqual(trans.result, 'success', 'trans.result')
 
     const expectedSpanNames = [
       'FLUSHALL',
@@ -91,16 +79,6 @@ test('redis', function (t) {
     client.disconnect()
   })
 
-  // If a redis client is not yet "ready" it will queue client commands, and
-  // then send them after "ready". Internally this results in calling
-  // RedisClient.internal_send_command *twice* for each queued command. If the
-  // instrumentation isn't careful, it is easy to instrument both of those
-  // calls. The latter call will only result in a span if there is a
-  // currentTransaction for the async task in which the redis client is created.
-  // That's what `transBeforeClient` is: to make sure we *don't* get
-  // double-spans.
-  const transBeforeClient = agent.startTransaction('transBeforeClient')
-
   const client = redis.createClient({
     socket: {
       host: process.env.REDIS_HOST,
@@ -115,7 +93,7 @@ test('redis', function (t) {
   // client.on('ready', () => console.log('client is ready'));
 
   client.connect()
-  const transAfterClient = agent.startTransaction('transAfterClient')
+  const trans = agent.startTransaction('aTrans')
 
   client.flushall(function (err, reply) {
     t.error(err, 'no flushall error')
@@ -151,8 +129,7 @@ test('redis', function (t) {
       done++
       t.strictEqual(done, 4, 'done 4 callbacks')
 
-      transAfterClient.end()
-      transBeforeClient.end()
+      trans.end()
       agent.flush()
     })
   })
