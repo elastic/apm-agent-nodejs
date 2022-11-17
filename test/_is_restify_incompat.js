4d52d720208bcdd53247718b2a104e001c15147a
Trent Mick <trent.mick@elastic.co>
2022-11-17 15:17:16 -0800

    test: fix tests to work with restify@9 (#3031)
    restify@9 was recently released. It only works with node >=v14.18.0
    because it added use of https://nodejs.org/api/modules.html#core-modules
    This updates restify testing to skip out when there is an incompat
    (restify, node) version pair.
    
    Also an unrelated change in "http-run-context.test.js" to get better
    debug output if a spurious error is hit in it again.

---
+++ test/_is_restify_incompat.js
@@ -0,0 +1,42 @@
+/*
+ * Copyright Elasticsearch B.V. and other contributors where applicable.
+ * Licensed under the BSD 2-Clause License; you may not use this file except in
+ * compliance with the BSD 2-Clause License.
+ */
+
+'use strict'
+
+var semver = require('semver')
+
+/**
+ * Return whether the current 'restify' version is incompatible with the
+ * current version of node. If so, this returns a string that can be used
+ * as a reason for a test skip message. Otherwise it returns false.
+ *
+ * Usage:
+ *    const isRestifyIncompat = require('.../_is_restify_incompat')()
+ *    if (isRestifyIncompat) {
+ *      console.log(`# SKIP ${isRestifyIncompat}`)
+ *      process.exit()
+ *    }
+ *
+ * Dev Note: These version ranges should mirror the restify section of ".tav.yml".
+ *
+ * @returns {string | boolean}
+ */
+function isRestifyIncompat () {
+  const nodeVer = process.version
+  const restifyVer = require('restify/package.json').version
+  const msg = `restify@${restifyVer} is incompatible with node@${nodeVer}`
+
+  if (semver.satisfies(restifyVer, '<10.0.0') && semver.satisfies(nodeVer, '>=18.0.0')) {
+    return msg
+  }
+  if (semver.satisfies(restifyVer, '>=9.0.0') && semver.satisfies(nodeVer, '<14.18.0')) {
+    return msg
+  }
+
+  return false
+}
+
+module.exports = isRestifyIncompat
