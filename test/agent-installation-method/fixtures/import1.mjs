48e96d8fb5abc6532b7687257fdaf01f403aa09d
Trent Mick <trent.mick@elastic.co>
2022-11-23 11:17:41 -0800

    fix make check

--- test/agent-installation-method/fixtures/import1.mjs
+++ test/agent-installation-method/fixtures/import1.mjs
@@ -1,2 +1,8 @@
+/*
+ * Copyright Elasticsearch B.V. and other contributors where applicable.
+ * Licensed under the BSD 2-Clause License; you may not use this file except in
+ * compliance with the BSD 2-Clause License.
+ */
+
 import 'elastic-apm-node/start.js'
 console.log("using: import 'elastic-apm-node/start.js'")
