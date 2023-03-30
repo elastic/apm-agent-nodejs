1b54c5fbcfedf2023c3d88aab5877f4532aae272
Victor Martinez <VictorMartinezRubio@gmail.com>
2023-03-30 10:01:15 +0200

    exclude running test on GH windows runners
    those tests are the ones defined in the list of modules in .github/workflows/tav.yml

--- test/instrumentation/modules/undici/undici.test.js
+++ test/instrumentation/modules/undici/undici.test.js
@@ -6,6 +6,11 @@
 
 'use strict'
 
+if (process.env.GITHUB_ACTIONS === 'true' && process.platform === 'win32') {
+  console.log('# SKIP: GH Actions do not support docker services on Windows')
+  process.exit(0)
+}
+
 process.env.ELASTIC_APM_TEST = true
 const { CapturingTransport } = require('../../../_capturing_transport')
 const apm = require('../../../..').start({
