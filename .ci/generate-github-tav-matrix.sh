320909a0f46e402d0afb455bcb2f286ce12fa32a
Victor Martinez <VictorMartinezRubio@gmail.com>
2023-03-31 14:35:06 +0200

    use output with the PR number

--- .ci/generate-github-tav-matrix.sh
+++ .ci/generate-github-tav-matrix.sh
@@ -3,3 +3,4 @@ set -e
 
 echo "versions=$(jq -c .versions .ci/tav.json)" >> $GITHUB_OUTPUT
 echo "modules=$(jq -c .modules .ci/tav.json)" >> $GITHUB_OUTPUT
+echo "pull-request=${PR_NUMBER}" >> $GITHUB_OUTPUT
