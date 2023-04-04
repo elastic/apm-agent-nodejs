989ca2dde89bf9a7886db9ee7f5a05a1eef6b4fe
Victor Martinez <victormartinezrubio@gmail.com>
2023-04-04 09:47:59 +0200

    Update .ci/generate-github-tav-matrix.sh

--- .ci/generate-github-tav-matrix.sh
+++ .ci/generate-github-tav-matrix.sh
@@ -3,4 +3,3 @@ set -e
 
 echo "versions=$(jq -c .versions .ci/tav.json)" >> $GITHUB_OUTPUT
 echo "modules=$(jq -c .modules .ci/tav.json)" >> $GITHUB_OUTPUT
-echo "pull-request=${PR_NUMBER}" >> $GITHUB_OUTPUT
