70ef9678f8c3a902c2f34c9e7cc2c886f4b968ac
Trent Mick <trent.mick@elastic.co>
2023-04-28 16:06:10 -0700

    feat: OTel Metrics support (#3152)
    With this change the APM agent adds support for using OTel Metrics in two ways:
    
    1. The `@opentelemetry/api` package is instrumented so that usage of
       `.metrics.getMeterProvider(...)` will use one provided by the APM agent,
       if the user hasn't explicitly provided one of their own. This implementation
       uses a version of the OTel Metrics SDK included with the APM agent. It
       is configured to sent metrics to APM server by default. This allows a user
       to use the OTel Metrics API and get metrics set to APM server without any
       configuration.
    2. Alternatively, the `@opentelemetry/sdk-metrics` package is instrumented so
       that when a user creates a `MeterProvider`, the APM agent will added a
       `MetricReader` that sends metrics to APM server.  This allows a user to
       configure metrics as they wish using the OTel Metrics SDK and then
       automatically get those metrics sent to APM server.
    
    This also adds some grouping in ".ci/tav.json" for the TAV workflow to avoid the
    256 jobs GH Actions limit. I'm not yet sure if those will work well.
    
    Closes: https://github.com/elastic/apm-agent-nodejs/issues/2954
    Refs: https://github.com/elastic/apm/issues/691
    Refs: https://github.com/elastic/apm/pull/742 (spec PR)

---
+++ examples/opentelemetry-metrics/README.md
@@ -0,0 +1,55 @@
+This directory shows how you can use the OpenTelemetry Metrics API with the Elastic APM Node.js agent.
+
+1. You will need an Elastic deployment to which to send APM data.
+   If you don't have one, start here to use a free hosted trial:
+   https://www.elastic.co/guide/en/apm/guide/current/apm-quick-start.html
+
+2. Install dependencies:
+
+    ```
+    git clone https://github.com/elastic/apm-agent-nodejs.git
+    cd apm-agent-nodejs/examples/opentelemetry-metrics
+    npm install
+    ```
+
+3. Run [the "Hello World" example](./otel-metrics-hello-world.js). This shows
+   a minimal example using the `@opentelemetry/api` to create a custom counter
+   metric.
+
+    ```
+    export ELASTIC_APM_SERVER_URL=https://...  # your Elastic APM Server URL
+    export ELASTIC_APM_SECRET_TOKEN=...
+    node -r elastic-apm-node/start.js otel-metrics-hello-world.js
+    ```
+
+    Then add some load to exercise the custom metrics in that script:
+
+    ```
+    while true; do curl http://127.0.0.1:3000/; sleep 1; done
+    ```
+
+    **The `num_requests` custom metric counter can now be visualized in Kibana.**
+
+    ![A chart of num_requests](./num_requests-chart.png)
+
+5. Stop the hello world example and run [the "Using the OTel Metrics SDK" example](./use-otel-metrics-sdk.js).
+   This example shows how to use the OpenTelemetry Metrics *SDK* to export metrics
+   both via a Prometheus endpoint *and* to Elastic APM:
+
+    ```
+    export ELASTIC_APM_SERVER_URL=https://...  # your Elastic APM Server URL
+    export ELASTIC_APM_SECRET_TOKEN=...
+    node -r elastic-apm-node/start.js use-otel-metrics-sdk.js
+    ```
+
+    Again apply some load to exercise the the script:
+
+    ```
+    while true; do curl http://127.0.0.1:3000/; sleep 1; done
+    ```
+
+    **The a percentile of the `latency` custom histogram metric can now be visualized in Kibana.**
+
+
+See https://www.elastic.co/guide/en/apm/agent/nodejs/current/opentelemetry-bridge.html
+for more details on the OpenTelemetry integration in the Elastic APM Node.js agent.
---
+++ examples/opentelemetry-metrics/use-otel-metrics-sdk.js
@@ -0,0 +1,65 @@
+/*
+ * Copyright Elasticsearch B.V. and other contributors where applicable.
+ * Licensed under the BSD 2-Clause License; you may not use this file except in
+ * compliance with the BSD 2-Clause License.
+ */
+
+// This is a small example showing how to use the OTel Metrics SDK and API
+// together with the Elastic APM agent.
+//
+// This sets up a simple HTTP server with a histogram metric for response
+// latency.
+//    while true; do curl http://127.0.0.1:3000/; sleep 1; done
+//
+// It shows using a `View` to configure the buckets for a histogram metric and
+// sets up a Prometheus endpoint:
+//    curl -i http://127.0.0.1:3001/metrics
+//
+// When the Elastic APM agent is started, the histogram metric will also
+// periodically be exported to the configured Elastic APM server:
+//    export ELASTIC_APM_SERVER_URL='<url of your APM server>'
+//    export ELASTIC_APM_SECRET_TOKEN='<secret token for your APM server>'
+//    node -r elastic-apm-node/start.js use-otel-metrics-sdk.js
+
+'use strict'
+
+const { createServer } = require('http')
+const { performance } = require('perf_hooks')
+
+const otel = require('@opentelemetry/api')
+const { MeterProvider, View, ExplicitBucketHistogramAggregation } = require('@opentelemetry/sdk-metrics')
+const { PrometheusExporter } = require('@opentelemetry/exporter-prometheus')
+
+const exporter = new PrometheusExporter({ host: '127.0.0.1', port: 3001 })
+const meterProvider = new MeterProvider({
+  views: [
+    new View({
+      instrumentName: 'latency',
+      aggregation: new ExplicitBucketHistogramAggregation(
+        // Use the same default buckets as in `prom-client` for comparison.
+        // This is to demonstrate using a View. The default buckets used by the
+        // APM agent would suffice as well.
+        [0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1, 2.5, 5, 10])
+    })
+  ]
+})
+meterProvider.addMetricReader(exporter)
+otel.metrics.setGlobalMeterProvider(meterProvider)
+console.log('Prometheus metrics at http://127.0.0.1:3001/metrics')
+
+const meter = otel.metrics.getMeter('my-meter')
+const latency = meter.createHistogram('latency', { description: 'Response latency (s)' })
+
+const server = createServer((req, res) => {
+  const start = performance.now()
+  req.resume()
+  req.on('end', () => {
+    setTimeout(() => {
+      res.end('pong\n')
+      latency.record((performance.now() - start) / 1000) // latency in seconds
+    }, Math.random() * 80) // random latency up to 80ms
+  })
+})
+server.listen(3000, () => {
+  console.log('Listening at http://127.0.0.1:3000/')
+})
