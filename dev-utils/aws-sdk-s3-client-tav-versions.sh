dfdf4c30d558f66c031af9db14dda52e424022b1
David Luna <david.luna@elastic.co>
2023-05-03 22:11:58 +0200

    test: fix @aws-sdk/client-s3 TAV test failures (#3312)
    * fix resolution of docker compose file for CI tests of client-s3
    * bump min supported @aws-sdk/client-s3 to v3.15.0
    * Fix an issue in the tests sorting APM intake events to be tested.
    
    Fixes: #3311 
    Co-authored-by: Trent Mick <trent.mick@elastic.co>

--- dev-utils/aws-sdk-s3-client-tav-versions.sh
+++ dev-utils/aws-sdk-s3-client-tav-versions.sh
@@ -2,7 +2,7 @@
 #
 # Calculate and emit the "versions:" block of ".tav.yml" for aws-sdk.
 # This will include:
-# - the first supported release (2.858.0)
+# - the first supported release
 # - the latest current release
 # - and ~5 releases in between
 
@@ -14,9 +14,9 @@ npm info -j @aws-sdk/client-s3 | node -e '
         .on("data", (chunk) => { chunks.push(chunk) })
         .on("end", () => {
             var input = JSON.parse(chunks.join(""));
-            var vers = input.versions.filter(v => semver.satisfies(v, ">=3 <4"));
+            var vers = input.versions.filter(v => semver.satisfies(v, ">=3.15.0 <4"));
             var modulus = Math.floor((vers.length - 2) / 5);
-            console.log("  # Test v3.0.0, every N=%d of %d releases, and current latest.", modulus, vers.length);
+            console.log("  # Test v3.15.0, every N=%d of %d releases, and current latest.", modulus, vers.length);
             vers = vers.filter((v, idx, arr) => idx % modulus === 0 || idx === arr.length - 1);
             console.log("  versions: '\''%s || >%s <4'\''", vers.join(" || "), vers[vers.length-1])
         })
