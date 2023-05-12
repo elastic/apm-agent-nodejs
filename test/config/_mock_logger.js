661503d4bad60c20e017812497dd0c86d3a3a72f
David Luna <luna.bistuer@gmail.com>
2023-05-12 16:22:22 +0200

    chore: extract normalizers to its own module

---
+++ test/config/_mock_logger.js
@@ -0,0 +1,54 @@
+/*
+ * Copyright Elasticsearch B.V. and other contributors where applicable.
+ * Licensed under the BSD 2-Clause License; you may not use this file except in
+ * compliance with the BSD 2-Clause License.
+ */
+
+'use strict'
+
+/**
+ * @typedef {object} LogEntry
+ * @property {string} type
+ * @property {string} message
+ * @property {Array<any>} message
+ */
+
+/**
+ * SomeClass is an example class for my question.
+ * @class
+ * @constructor
+ * @public
+ */
+class MockLogger {
+  constructor () {
+    /**
+     * someProperty is an example property that is set to `true`
+     * @type {Array<LogEntry>}
+     * @public
+     */
+    this.calls = []
+  }
+
+  _log (type, message, interpolation) {
+    this.calls.push({
+      type,
+      message,
+      interpolation
+    })
+  }
+
+  // fatal (message) { this._log('fatal', message) }
+  // error (message) { this._log('error', message) }
+  // warn (message) { this._log('warn', message) }
+  // info (message) { this._log('info', message) }
+  // debug (message) { this._log('debug', message) }
+  // trace (message) { this._log('trace', message) }
+  fatal () { this._log('fatal', arguments[0], [].slice.call(arguments, 1)) }
+  error () { this._log('error', arguments[0], [].slice.call(arguments, 1)) }
+  warn () { this._log('warn', arguments[0], [].slice.call(arguments, 1)) }
+  info () { this._log('info', arguments[0], [].slice.call(arguments, 1)) }
+  debug () { this._log('debug', arguments[0], [].slice.call(arguments, 1)) }
+  trace () { this._log('trace', arguments[0], [].slice.call(arguments, 1)) }
+}
+
+module.exports = MockLogger
