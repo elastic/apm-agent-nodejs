6e63b87e1fdb50c404168c75ecfdbd683f24efd6
David Luna <luna.bistuer@gmail.com>
2023-05-17 10:51:30 +0200

    chore: add tedious v16 in tav tests and agent checks

--- test/instrumentation/modules/tedious.test.js
+++ test/instrumentation/modules/tedious.test.js
@@ -22,7 +22,8 @@ const agent = require('../../../').start({
 
 const tediousVer = require('../../../node_modules/tedious/package.json').version
 const semver = require('semver')
-if ((semver.gte(tediousVer, '15.0.0') && semver.lt(process.version, '14.0.0')) ||
+if ((semver.gte(tediousVer, '16.0.0') && semver.lt(process.version, '15.0.0')) ||
+    (semver.gte(tediousVer, '15.0.0') && semver.lt(process.version, '14.0.0')) ||
     (semver.gte(tediousVer, '12.0.0') && semver.lt(process.version, '12.3.0')) ||
     (semver.gte(tediousVer, '11.0.0') && semver.lt(process.version, '10.17.0'))) {
   console.log(`# SKIP tedious@${tediousVer} does not support node ${process.version}`)
