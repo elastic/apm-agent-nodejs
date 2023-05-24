7e6f10cd012c66042ec0c3da95d37834ac9b64f7
Victor Martinez <VictorMartinezRubio@gmail.com>
2023-05-24 10:31:46 +0200

    Revert "action: group logs"
    This reverts commit 92bcd6c3a146f913e5e0d11999e6f44dee0a5291.

--- .ci/scripts/bench.sh
+++ .ci/scripts/bench.sh
@@ -9,7 +9,6 @@ set -eo pipefail
 if [ "$CI" == "true" ] ; then
 
   echo "--- Configure Buildkite worker"
-  echo "::group::GitHub Configure Buildkite worker"
 	# If HOME is not set then use the Buildkite workspace
 	# that's normally happening when running in the CI
 	# owned by Elastic.
@@ -23,12 +22,9 @@ if [ "$CI" == "true" ] ; then
 	export PATH
 
 	echo 'Docker login is done in the Buildkite hooks'
-  echo "::endgroup::"
 fi
 
 .ci/scripts/run-benchmarks.sh "apm-agent-benchmark-results.json" "14"
 
 echo "--- Send benchmarks"
-echo "::group::Send benchmarks"
 echo "TBC: sendBenchmarks(file: \"apm-agent-benchmark-results.json\", index: \"benchmark-nodejs\", archive: true)"
-echo "::endgroup::"
--- .ci/scripts/prepare-benchmarks-env.sh
+++ .ci/scripts/prepare-benchmarks-env.sh
@@ -7,9 +7,7 @@
 #   NODE_VERSION=...
 #   source .../prepare-benchmarks-env.sh
 #
-# Note:
-#   echo "--- ..." helps with presenting the output in Buildkite.
-#   echo "::group..." or echo "::endgroup" help with presenting the output in GitHub actions.
+# Note: echo "--- ..." helps with presenting the output in Buildkite.
 #
 
 set -xeo pipefail
@@ -20,31 +18,23 @@ if [[ -z "$NODE_VERSION" ]]; then
 fi
 
 echo "--- Download nvm"
-echo "::group::Download nvm"
 # This particular configuration is required to be installed in the baremetal
 curl -sS -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
-echo "::endgroup::"
 
 echo "--- Install nvm"
-echo "::group::Download nvm"
 set +x  # Disable xtrace because output using nvm.sh is huge.
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 command -v nvm
 nvm --version
-echo "::endgroup::"
 
 echo "--- Run nvm install ${NODE_VERSION}"
-echo "::group::Run nvm install ${NODE_VERSION}"
 nvm install "${NODE_VERSION}"
-echo "::endgroup::"
 
 echo "--- Run npm"
-echo "::group::Run npm"
 set -x
 npm config list
 npm install
 
 node --version
 npm --version
-echo "::endgroup::"
