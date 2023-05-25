41d50ecdc4fb25b51706e04115cce5768305f6aa
Victor Martinez <VictorMartinezRubio@gmail.com>
2023-05-25 10:27:30 +0200

    ci: test microbenchmark in jenkins

--- .ci/scripts/prepare-benchmarks-env.sh
+++ .ci/scripts/prepare-benchmarks-env.sh
@@ -14,11 +14,18 @@ if [[ -z "$NODE_VERSION" ]]; then
   exit 1
 fi
 
-# This particular configuration is required to be installed in the baremetal
-curl -sS -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
-export NVM_DIR="$HOME/.nvm"
-set +x  # Disable xtrace because output using nvm.sh is huge.
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+# Help to debug whether the shell is login so we know nvm might not be affected by that.
+shopt -q login_shell && echo 'Login shell' || echo 'No login shell'
+
+if ! command -v nvm &> /dev/null ; then
+  # This particular configuration is required to be installed in the baremetal
+  PROFILE=/dev/null bash -c 'curl -sS -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash'
+
+  #set +x  # Disable xtrace because output using nvm.sh is huge.
+  export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+fi
+
 command -v nvm
 nvm --version
 
