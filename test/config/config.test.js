97691af8b6cc2a8acf1211e53f8cf0c7bd30505c
David Luna <luna.bistuer@gmail.com>
2023-05-30 13:39:01 +0200

    chore: use pino to create mock loggers

--- test/config/config.test.js
+++ test/config/config.test.js
@@ -6,50 +6,92 @@
 
 'use strict'
 
-// Test the normalizer functions
-const test = require('tape')
-const { MockLogger } = require('../_mock_logger')
+var fs = require('fs')
+var os = require('os')
+var path = require('path')
 
-const { printLoggingPreamble } = require('../../lib/config/config')
+var mkdirp = require('mkdirp')
+var rimraf = require('rimraf')
+const test = require('tape')
+
+const AGENT_VERSION = require('../../package.json').version
+
+const Agent = require('../../lib/agent')
+const { createMockLogger } = require('../_mock_logger')
+const { NoopApmClient } = require('../../lib/apm-client/noop-apm-client')
 
 test('#printLoggingPreamble()', function (t) {
-  const logger = new MockLogger()
-  const config = { logger }
-  const sources = {
-    environment: {
-      apiRequestSize: '1024kb'
-    },
-    start: {
-      apiRequestSize: '512kb',
-      apiRequestTime: '10s',
-      apiKey: ' a-secret-key',
-      secretToken: 'secret-token',
-      serverUrl: 'https://server-url'
-    },
-    file: {
-      apiRequestSize: '256kb',
-      apiRequestTime: '5s',
-      captureExceptions: false,
-      logger: function () {},
-      transport: function () {}
-    }
+  const loggerCalls = []
+  const logger = createMockLogger(loggerCalls)
+  const agent = new Agent()
+  const origApiReqSize = process.env.ELASTIC_APM_API_REQUEST_SIZE
+
+  // Set options via env
+  process.env.ELASTIC_APM_API_REQUEST_SIZE = '1024kb'
+
+  // Set options via file
+  const tmpDirPath = path.join(os.tmpdir(), 'elastic-apm-node-test', String(Date.now()))
+  const tmpFilePath = path.join(tmpDirPath, 'elastic-apm-node-config.js')
+  const tmpConfigOptions = {
+    apiRequestSize: '256kb',
+    apiRequestTime: '5s',
+    captureExceptions: false
   }
+  mkdirp.sync(tmpDirPath)
+  fs.writeFileSync(tmpFilePath, `module.exports = ${JSON.stringify(tmpConfigOptions, null, 2)}`)
+  t.on('end', function () { rimraf.sync(tmpDirPath) })
 
-  printLoggingPreamble(config, sources, null)
+  // And set start options
+  agent.start({
+    apiRequestSize: '512kb',
+    apiRequestTime: '10s',
+    apiKey: ' a-secret-key',
+    configFile: tmpFilePath,
+    secretToken: 'secret-token',
+    serverUrl: 'https://server-url',
+    transport: () => new NoopApmClient(),
+    logger
+  })
 
-  const { calls } = logger
-  console.log(calls)
-  t.ok(logExists(calls, '- apiRequestSize: 1024kb (environment)'), 'environment options get printed')
-  t.ok(logExists(calls, '- apiRequestTime: 10s (start)'), 'start options get printed')
-  t.ok(logExists(calls, '- captureExceptions: false (file)'), 'file options get printed')
-  t.ok(logExists(calls, '- apiKey: [REDACTED] (start)'), 'sensitive option "apiKey" gets [REDACTED]')
-  t.ok(logExists(calls, '- secretToken: [REDACTED] (start)'), 'sensitive option "secretToken" gets [REDACTED]')
-  t.ok(logExists(calls, '- serverUrl: [REDACTED] (start)'), 'sensitive option "serverUrl" gets [REDACTED]')
-  t.ok(logExists(calls, '- logger:') === false, 'logger should not be printed')
-  t.ok(logExists(calls, '- transport:') === false, 'transport should not be printed')
+  const infoLog = loggerCalls.find(log => log.type === 'info')
+  const preambleHeader = infoLog && infoLog.message
+  const preambleData = infoLog.mergingObject
+
+  t.ok(preambleHeader.indexOf('Elastic APM Node.js Agent, version:') !== -1, 'preamble header is logged')
+  t.ok(preambleData.agentVersion === AGENT_VERSION, 'agent version is present')
+  t.deepEqual(
+    preambleData.config.apiRequestSize,
+    {
+      source: 'environment',
+      sourceValue: '1024kb',
+      normalizedValue: 1024 * 1024
+    },
+    'apiRequestSize is taken from environment'
+  )
+  t.deepEqual(
+    preambleData.config.apiRequestTime,
+    {
+      source: 'start',
+      sourceValue: '10s',
+      normalizedValue: 10
+    },
+    'apiRequestTime is taken from start options'
+  )
+  t.deepEqual(
+    preambleData.config.captureExceptions,
+    {
+      source: 'file',
+      sourceValue: false,
+      normalizedValue: false,
+      file: tmpFilePath
+    },
+    'captureExceptions is taken from file options'
+  )
+  t.ok(!('logger' in preambleData.config), 'logger is not in preamble')
+  t.ok(!('transport' in preambleData.config), 'transport is not in preamble')
+
+  agent.destroy()
+
+  process.env.ELASTIC_APM_API_REQUEST_SIZE = origApiReqSize
   t.end()
 })
-
-function logExists (calls, text) {
-  return calls.some(c => c.message.indexOf(text) !== -1)
-}
