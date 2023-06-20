e005df781ec9fad3a17b4099be2ed69f9e12cc63
Trent Mick <trent.mick@elastic.co>
2023-06-20 14:07:00 -0700

    test: convert sqs.test.js over to use localstack for end-to-end tests (similar to s3.test.js) (#3334)
    - update to the latest localstack
    - restore testing of recent releases of aws-sdk (skipping the "bad" 2.1372.0 release that attempted to switch SQS over to the new "JSON protocol")
    - changes the SQS "integration tests" to run use-sqs.js against localstack, instead of just against a local mock SQS server with canned responses
    
    Fixes: #3324

--- dev-utils/aws-sdk-tav-versions.sh
+++ dev-utils/aws-sdk-tav-versions.sh
@@ -9,12 +9,13 @@
 npm info -j aws-sdk | node -e '
     var semver = require("semver");
     var chunks = [];
+    var wantVerRange = ">=2.858 <2.1372.0 || >2.1372.0 <3"
     process.stdin
         .resume()
         .on("data", (chunk) => { chunks.push(chunk) })
         .on("end", () => {
             var input = JSON.parse(chunks.join(""));
-            var vers = input.versions.filter(v => semver.satisfies(v, ">=2.858.0 <3"));
+            var vers = input.versions.filter(v => semver.satisfies(v, wantVerRange));
             var modulus = Math.floor((vers.length - 2) / 5);
             console.log("  # Test v2.858.0, every N=%d of %d releases, and current latest.", modulus, vers.length);
             vers = vers.filter((v, idx, arr) => idx % modulus === 0 || idx === arr.length - 1);
