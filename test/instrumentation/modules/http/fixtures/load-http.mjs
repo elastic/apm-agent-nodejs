053f70582683d0ad515b3fe46eb8801c482b89aa
Trent Mick <trent.mick@elastic.co>
2023-06-21 15:39:43 -0700

    fix lint

--- test/instrumentation/modules/http/fixtures/load-http.mjs
+++ test/instrumentation/modules/http/fixtures/load-http.mjs
@@ -1,3 +1,9 @@
+/*
+ * Copyright Elasticsearch B.V. and other contributors where applicable.
+ * Licensed under the BSD 2-Clause License; you may not use this file except in
+ * compliance with the BSD 2-Clause License.
+ */
+
 import assert from 'node:assert'
 import http from 'node:http'
 assert(http.get, 'http.get is defined')
