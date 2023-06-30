96e60cfb041d9b522142b94c24ca8af42cf08303
Adrien Mannocci <adrien.mannocci@elastic.co>
2023-06-30 15:55:20 +0200

    ci: let's debug nvm install
    Signed-off-by: Adrien Mannocci <adrien.mannocci@elastic.co>

--- .ci/scripts/bench.sh
+++ .ci/scripts/bench.sh
@@ -14,25 +14,19 @@ BASE_PROJECT="$(dirname "$(dirname "${RELATIVE_DIR}")")"
 # to execute the benchmarks afterwards
 
 ## Buildkite specific configuration
-if [ "$CI" == "true" ] ; then
+if [ "${CI}" == "true" ] ; then
   # If HOME is not set then use the Buildkite workspace
   # that's normally happening when running in the CI
   # owned by Elastic.
-  if [ -z "$HOME" ] ; then
-    HOME=$BUILDKITE_BUILD_CHECKOUT_PATH
+  if [ -z "${HOME}" ] ; then
+    HOME="${BUILDKITE_BUILD_CHECKOUT_PATH}"
     export HOME
   fi
-
-  # required when running the benchmark
-  PATH=$PATH:$HOME/.local/bin
-  export PATH
-
-  echo 'Docker login is done in the Buildkite hooks'
 fi
 
+# Run benchmark
+echo "--- Execute benchmarks"
+"${BASE_PROJECT}/.ci/scripts/run-benchmarks.sh" "apm-agent-benchmark-results.json" "14"
 
-.ci/scripts/run-benchmarks.sh "apm-agent-benchmark-results.json" "14"
-
-echo "--- Send benchmarks"
-set -x
-sendBenchmark "$ES_USER_SECRET" "$ES_PASS_SECRET" "$ES_URL_SECRET" "apm-agent-benchmark-results.json"
+echo "--- Send benchmark results"
+sendBenchmark "${ES_USER_SECRET}" "${ES_PASS_SECRET}" "${ES_URL_SECRET}" "apm-agent-benchmark-results.json"
--- .ci/scripts/prepare-benchmarks-env.sh
+++ .ci/scripts/prepare-benchmarks-env.sh
@@ -12,7 +12,7 @@
 
 set -xeo pipefail
 
-if [[ -z "$NODE_VERSION" ]]; then
+if [[ -z "${NODE_VERSION}" ]]; then
   echo "prepare-benchmarks-env.sh: error: NODE_VERSION envvar is not set" >&2
   exit 1
 fi
@@ -20,33 +20,31 @@ fi
 echo "--- Download nvm"
 # This particular configuration is required to be installed in the baremetal
 curl -sS -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
-export NVM_DIR="$HOME/.nvm"
-
-echo "--- Debug nvm "
-# debug if curl works as explained in https://github.com/nvm-sh/nvm/issues/3117
-curl -I --compressed -v https://nodejs.org/dist/
+export NVM_DIR="${HOME}/.nvm"
 
 echo "--- Install nvm"
-set +x  # Disable xtrace because output using nvm.sh is huge.
-if [ -s "$NVM_DIR/nvm.sh" ] ; then
+# set +x  # Disable xtrace because output using nvm.sh is huge.
+if [ -s "${NVM_DIR}/nvm.sh" ] ; then
   # As long as the Buildkite agent does some weird behaviours compare to the Jenkins agent
   # let's avoid failures when running nvm.sh for the first time. For some reason
   # nvm_resolve_local_alias default returns VERSION= while in Jenkins a similar
   # runner returns VERSION=v14.21.3
   # Already reported to the System owners of the Buildkite agent setup.
-  \. "$NVM_DIR/nvm.sh" || true
+  \. "${NVM_DIR}/nvm.sh"
 fi
 
+# Check nvm command is available
 command -v nvm
 nvm --version
 
 echo "--- Run nvm install ${NODE_VERSION}"
 nvm install "${NODE_VERSION}"
 
-echo "--- Run npm"
+# Check node command is available
+node --version
+npm --version
+
+echo "--- Install dependencies"
 set -x
 npm config list
 npm install
-
-node --version
-npm --version
