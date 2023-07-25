7581d9df7bf29e7de8aacd741c6f8b714a6c517b
David Luna <luna.bistuer@gmail.com>
2023-07-25 23:38:10 +0200

    chore: include http apm client

--- test/apm-client/http-apm-client.test.js
+++ test/apm-client/http-apm-client.test.js
@@ -10,7 +10,7 @@ const test = require('tape')
 
 var apmVersion = require('../../package').version
 
-const { userAgentFromConf } = require('../../lib/apm-client/http-apm-client')
+const { userAgentFromConf } = require('../../lib/apm-client/apm-client')
 
 // Test User-Agent generation. It would be nice to also test against gherkin
 // specs from apm.git.
