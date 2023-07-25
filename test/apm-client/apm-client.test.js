d1174bb85629137e5dca80ca1485ccab8877ca1b
David Luna <luna.bistuer@gmail.com>
2023-07-25 23:48:59 +0200

    chore: fix tests

--- test/apm-client/apm-client.test.js
+++ test/apm-client/apm-client.test.js
@@ -6,13 +6,14 @@
 
 'use strict'
 
-const ElasticAPMHttpClient = require('elastic-apm-http-client')
-
 const test = require('tape')
 
+var apmVersion = require('../../package').version
+
 const Agent = require('../../lib/agent')
+const { HttpApmClient } = require('../../lib/apm-client/http-apm-client')
 const { NoopApmClient } = require('../../lib/apm-client/noop-apm-client')
-const { createApmClient } = require('../../lib/apm-client/apm-client')
+const { createApmClient, userAgentFromConf } = require('../../lib/apm-client/apm-client')
 
 test('#createApmClient - disableSend', (t) => {
   const agent = new Agent()
@@ -50,7 +51,27 @@ test('#createApmClient - elastic APM Transport', (t) => {
     cloudProvider: 'none'
   }, agent)
 
-  t.ok(transport instanceof ElasticAPMHttpClient, 'transport should be an ElasticAPMHttpClient instance')
+  t.ok(transport instanceof HttpApmClient, 'transport should be an ElasticAPMHttpClient instance')
   agent.destroy()
+  t.end()
+})
+
+// Test User-Agent generation. It would be nice to also test against gherkin
+// specs from apm.git.
+// https://github.com/elastic/apm/blob/main/tests/agents/gherkin-specs/user_agent.feature
+test('userAgentFromConf', t => {
+  t.equal(userAgentFromConf({}),
+    `apm-agent-nodejs/${apmVersion}`)
+  t.equal(userAgentFromConf({ serviceName: 'foo' }),
+    `apm-agent-nodejs/${apmVersion} (foo)`)
+  t.equal(userAgentFromConf({ serviceName: 'foo', serviceVersion: '1.0.0' }),
+    `apm-agent-nodejs/${apmVersion} (foo 1.0.0)`)
+  // ISO-8859-1 characters are generally allowed.
+  t.equal(userAgentFromConf({ serviceName: 'party', serviceVersion: '2021-été' }),
+    `apm-agent-nodejs/${apmVersion} (party 2021-été)`)
+  // Higher code points are replaced with `_`.
+  t.equal(userAgentFromConf({ serviceName: 'freeze', serviceVersion: 'do you want to build a ☃ in my 🏰' }),
+    `apm-agent-nodejs/${apmVersion} (freeze do you want to build a _ in my __)`)
+
   t.end()
 })
