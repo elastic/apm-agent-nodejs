b28df51e0d48dc01cc3a5ad35f945559b0dfa37d
Trent Mick <trent.mick@elastic.co>
2023-08-01 16:48:25 -0700

    chore: drop various test script guards for Node.js versions before the new 14.5.0 minimum

--- .ci/scripts/docker-test.sh
+++ .ci/scripts/docker-test.sh
@@ -1,22 +1,10 @@
 #!/usr/bin/env bash
 set -ueo pipefail
 
-major_node_version=`node --version | cut -d . -f1 | cut -d v -f2`
-minor_node_version=`node --version | cut -d . -f2`
-
-if [[ $major_node_version -eq 8 ]] && [[ $minor_node_version -lt 8 ]]; then
-  export NODE_OPTIONS="${NODE_OPTIONS:+${NODE_OPTIONS}} --expose-http2"
-fi
-
 # "test/instrumentation/modules/http2.js" fails if the OpenSSL SECLEVEL=2,
 # which is the case in the node:16 Docker image and could be in other
 # environments. Here we explicitly set it to SECLEVEL=0 for testing.
-#
-# Skip for node v8 because it results in this warning:
-#   openssl config failed: error:25066067:DSO support routines:DLFCN_LOAD:could not load the shared library
-if [[ $major_node_version -gt 8 ]]; then
-  export NODE_OPTIONS="${NODE_OPTIONS:+${NODE_OPTIONS}} --openssl-config=$(pwd)/test/openssl-config-for-testing.cnf"
-fi
+export NODE_OPTIONS="${NODE_OPTIONS:+${NODE_OPTIONS}} --openssl-config=$(pwd)/test/openssl-config-for-testing.cnf"
 
 # Workaround to git <2.7
 # error fatal: unable to look up current user in the passwd file: no such user
