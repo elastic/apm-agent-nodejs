d6db3bcb9c15f119f2b98cb04f2d4a4932118441
apmmachine <58790750+apmmachine@users.noreply.github.com>
2023-08-30 04:26:49 -0400

    [Automation] Update JSON specs (#3589)
    - Updated to content "" in file "test/fixtures/json-specs/service_resource_inference.json"
    - Updated to content "" in file "test/fixtures/json-specs/container_metadata_discovery.json"
    
    Made with ❤️️ by updatecli

--- test/service-resource-inference.test.js
+++ test/service-resource-inference.test.js
@@ -23,7 +23,6 @@ const apm = require('..').start({
 });
 
 const test = require('tape');
-const { URL } = require('url');
 
 test('setup: current trans', (t) => {
   apm.startTransaction('aTrans', 'manual');
@@ -48,24 +47,7 @@ testData.forEach((testDatum) => {
         span.setDbContext(testDatum.span.context.db);
       }
       if (testDatum.span.context.http) {
-        // service_resource_inference.json oddly provides context.http.url as
-        // an object with "host" and "port" fields, rather than as a URL string
-        // as accepted by the intake API. So we need to construct it.
-        // UPDATE: there is a PR fixing it https://github.com/elastic/apm-agent-nodejs/pull/3589
-        // making this test fail so for a short period we need to accept both cases
-        // before removing this tweak.
-        const httpContext = Object.assign({}, testDatum.span.context.http);
-        if (httpContext.url && typeof httpContext.url !== 'string') {
-          const u = new URL('', 'http://example.com');
-          if (httpContext.url.host) {
-            u.hostname = httpContext.url.host;
-          }
-          if (httpContext.url.port) {
-            u.port = httpContext.url.port;
-          }
-          httpContext.url = u.href;
-        }
-        span.setHttpContext(httpContext);
+        span.setHttpContext(testDatum.span.context.http);
       }
       if (testDatum.span.context.message) {
         span.setMessageContext(testDatum.span.context.message);
