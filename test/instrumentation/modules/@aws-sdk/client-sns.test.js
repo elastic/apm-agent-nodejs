2e1eec72d9651e8edc7eb44679cebb004b48aed2
David Luna <david.luna@elastic.co>
2023-09-22 16:36:00 +0200

    feat: add @aws-sdk/client-sns instrumentation (#3532)

---
+++ test/instrumentation/modules/@aws-sdk/client-sns.test.js
@@ -0,0 +1,271 @@
+/*
+ * Copyright Elasticsearch B.V. and other contributors where applicable.
+ * Licensed under the BSD 2-Clause License; you may not use this file except in
+ * compliance with the BSD 2-Clause License.
+ */
+
+'use strict';
+
+// Test SNS instrumentation of the '@aws-sdk/client-sns' module.
+//
+// Note that this uses localstack for testing, which mimicks the SNS API but
+// isn't identical.
+
+if (process.env.GITHUB_ACTIONS === 'true' && process.platform === 'win32') {
+  console.log('# SKIP: GH Actions do not support docker services on Windows');
+  process.exit(0);
+}
+
+const test = require('tape');
+
+const { validateSpan } = require('../../../_validate_schema');
+const { runTestFixtures, sortApmEvents } = require('../../../_utils');
+const { NODE_VER_RANGE_IITM } = require('../../../testconsts');
+
+const LOCALSTACK_HOST = process.env.LOCALSTACK_HOST || 'localhost';
+const endpoint = 'http://' + LOCALSTACK_HOST + ':4566';
+
+const testFixtures = [
+  {
+    name: 'simple SNS V3 usage scenario',
+    script: 'fixtures/use-client-sns.js',
+    cwd: __dirname,
+    timeout: 20000, // sanity guard on the test hanging
+    maxBuffer: 10 * 1024 * 1024, // This is big, but I don't ever want this to be a failure reason.
+    env: {
+      AWS_ACCESS_KEY_ID: 'fake',
+      AWS_SECRET_ACCESS_KEY: 'fake',
+      TEST_TOPIC_NAME: 'elasticapmtest-topic-3',
+      TEST_ENDPOINT: endpoint,
+      TEST_REGION: 'us-east-2',
+    },
+    versionRanges: {
+      node: '>=14',
+    },
+    verbose: true,
+    checkApmServer: (t, apmServer) => {
+      t.ok(apmServer.events[0].metadata, 'metadata');
+      const events = sortApmEvents(apmServer.events);
+
+      // First the transaction.
+      t.ok(events[0].transaction, 'got the transaction');
+      const tx = events.shift().transaction;
+      const errors = events.filter((e) => e.error).map((e) => e.error);
+
+      // Compare some common fields across all spans.
+      // ignore http/external spans
+      const spans = events
+        .filter((e) => e.span && e.span.type !== 'external')
+        .map((e) => e.span);
+      spans.forEach((s) => {
+        const errs = validateSpan(s);
+        t.equal(errs, null, 'span is valid (per apm-server intake schema)');
+      });
+      t.equal(
+        spans.filter((s) => s.trace_id === tx.trace_id).length,
+        spans.length,
+        'all spans have the same trace_id',
+      );
+      t.equal(
+        spans.filter((s) => s.transaction_id === tx.id).length,
+        spans.length,
+        'all spans have the same transaction_id',
+      );
+      t.equal(
+        spans.filter((s) => s.sync === false).length,
+        spans.length,
+        'all spans have sync=false',
+      );
+      t.equal(
+        spans.filter((s) => s.sample_rate === 1).length,
+        spans.length,
+        'all spans have sample_rate=1',
+      );
+      const failingSpanId = spans[2].id; // index of `Publish` to a non exixtent topic
+      spans.forEach((s) => {
+        // Remove variable and common fields to facilitate t.deepEqual below.
+        delete s.id;
+        delete s.transaction_id;
+        delete s.parent_id;
+        delete s.trace_id;
+        delete s.timestamp;
+        delete s.duration;
+        delete s.sync;
+        delete s.sample_rate;
+      });
+
+      t.deepEqual(
+        spans.shift(),
+        {
+          name: 'SNS Publish to <PHONE_NUMBER>',
+          type: 'messaging',
+          subtype: 'sns',
+          action: 'Publish',
+          context: {
+            service: {
+              target: { type: 'sns', name: '<PHONE_NUMBER>' },
+            },
+            destination: {
+              service: { name: '', type: '', resource: 'sns/<PHONE_NUMBER>' },
+              address: LOCALSTACK_HOST,
+              port: 4566,
+              cloud: { region: 'us-east-2' },
+            },
+            message: { queue: { name: '<PHONE_NUMBER>' } },
+          },
+          outcome: 'success',
+        },
+        'publish to PHONE_NUMBER produced expected span',
+      );
+
+      t.deepEqual(
+        spans.shift(),
+        {
+          name: 'SNS Publish to elasticapmtest-topic-3',
+          type: 'messaging',
+          subtype: 'sns',
+          action: 'Publish',
+          context: {
+            service: {
+              target: { type: 'sns', name: 'elasticapmtest-topic-3' },
+            },
+            destination: {
+              service: {
+                name: '',
+                type: '',
+                resource: 'sns/elasticapmtest-topic-3',
+              },
+              address: LOCALSTACK_HOST,
+              port: 4566,
+              cloud: { region: 'us-east-2' },
+            },
+            message: { queue: { name: 'elasticapmtest-topic-3' } },
+          },
+          outcome: 'success',
+        },
+        'publish to topic produced expected span',
+      );
+
+      t.deepEqual(
+        spans.shift(),
+        {
+          name: 'SNS Publish to elasticapmtest-topic-3-unexistent',
+          type: 'messaging',
+          subtype: 'sns',
+          action: 'Publish',
+          context: {
+            service: {
+              target: {
+                type: 'sns',
+                name: 'elasticapmtest-topic-3-unexistent',
+              },
+            },
+            destination: {
+              service: {
+                name: '',
+                type: '',
+                resource: 'sns/elasticapmtest-topic-3-unexistent',
+              },
+              address: LOCALSTACK_HOST,
+              port: 4566,
+              cloud: { region: 'us-east-2' },
+            },
+            message: { queue: { name: 'elasticapmtest-topic-3-unexistent' } },
+          },
+          outcome: 'failure',
+        },
+        'publish to unexistent topic produced expected span',
+      );
+
+      t.deepEqual(
+        spans.shift(),
+        {
+          name: 'SNS Publish to elasticapmtest-topic-3, elasticapmtest-topic-3, <PHONE_NUMBER>',
+          type: 'messaging',
+          subtype: 'sns',
+          action: 'Publish',
+          context: {
+            service: {
+              target: {
+                type: 'sns',
+                name: 'elasticapmtest-topic-3, elasticapmtest-topic-3, <PHONE_NUMBER>',
+              },
+            },
+            destination: {
+              service: {
+                name: '',
+                type: '',
+                resource:
+                  'sns/elasticapmtest-topic-3, elasticapmtest-topic-3, <PHONE_NUMBER>',
+              },
+              address: LOCALSTACK_HOST,
+              port: 4566,
+              cloud: {
+                region: 'us-east-2',
+              },
+            },
+            message: {
+              queue: {
+                name: 'elasticapmtest-topic-3, elasticapmtest-topic-3, <PHONE_NUMBER>',
+              },
+            },
+          },
+          outcome: 'success',
+        },
+        'publish to topic, target & phone in the same command produced expected span',
+      );
+
+      // This is the Publish to a non-existant-topic, so we expect a failure.
+      t.equal(errors.length, 1, 'got 1 error');
+      t.equal(
+        errors[0].parent_id,
+        failingSpanId,
+        'error is a child of the failing span from publish to a non existant topic',
+      );
+      t.equal(errors[0].transaction_id, tx.id, 'error.transaction_id');
+      t.equal(
+        errors[0].exception.type,
+        'NotFoundException',
+        'error.exception.type',
+      );
+
+      t.equal(spans.length, 0, 'all spans accounted for');
+    },
+  },
+  {
+    name: '@aws-sdk/client-sns ESM',
+    script: 'fixtures/use-client-sns.mjs',
+    cwd: __dirname,
+    env: {
+      NODE_OPTIONS:
+        '--experimental-loader=../../../../loader.mjs --require=../../../../start.js',
+      NODE_NO_WARNINGS: '1',
+      AWS_ACCESS_KEY_ID: 'fake',
+      AWS_SECRET_ACCESS_KEY: 'fake',
+      TEST_ENDPOINT: endpoint,
+      TEST_REGION: 'us-east-2',
+    },
+    versionRanges: {
+      node: NODE_VER_RANGE_IITM,
+    },
+    verbose: false,
+    checkApmServer: (t, apmServer) => {
+      t.ok(apmServer.events[0].metadata, 'metadata');
+      const events = sortApmEvents(apmServer.events);
+
+      t.ok(events[0].transaction, 'got the transaction');
+      const tx = events.shift().transaction;
+
+      const span = events.shift().span;
+      t.equal(span.parent_id, tx.id, 'span.parent_id');
+      t.equal(span.name, 'SNS Publish to <PHONE_NUMBER>', 'span.name');
+
+      t.equal(events.length, 0, 'all events accounted for');
+    },
+  },
+];
+
+test('@aws-sdk/client-sns fixtures', (suite) => {
+  runTestFixtures(suite, testFixtures);
+  suite.end();
+});
---
+++ test/instrumentation/modules/@aws-sdk/fixtures/use-client-sns.js
@@ -0,0 +1,261 @@
+/*
+ * Copyright Elasticsearch B.V. and other contributors where applicable.
+ * Licensed under the BSD 2-Clause License; you may not use this file except in
+ * compliance with the BSD 2-Clause License.
+ */
+
+'use strict';
+
+// Run a single scenario of using the SNS client (callback style) with APM
+// enabled. This is used to test that the expected APM events are generated.
+// It writes log.info (in ecs-logging format, see
+// https://github.com/trentm/go-ecslog#install) for each SNS client API call.
+//
+// This script can also be used for manual testing of APM instrumentation of SNS
+// against a real SNS account. This can be useful because tests are done against
+// https://github.com/localstack/localstack that *simulates* SNS with imperfect
+// fidelity.
+//
+// Auth note: By default this uses the AWS profile/configuration from the
+// environment. If you do not have that configured (i.e. do not have
+// "~/.aws/...") files, then you can still use localstack via setting:
+//    unset AWS_PROFILE
+//    export AWS_ACCESS_KEY_ID=fake
+//    export AWS_SECRET_ACCESS_KEY=fake
+// See also: https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-envvars.html
+//
+// Usage:
+//    # Run against the default configured AWS profile, creating a new bucket
+//    # and deleting it afterwards.
+//    node use-client-sns.js | ecslog
+//
+//    # Testing against localstack.
+//    docker run --rm -it -e SERVICES=sns -p 4566:4566 localstack/localstack
+//    TEST_ENDPOINT=http://localhost:4566 node use-client-sns.js | ecslog
+//
+
+const TEST_IGNORE_TOPIC = 'topic-to-ignore';
+const TEST_IGNORE_TARGET = 'target-to-ignore';
+const TEST_IGNORE_PHONE = '6666666';
+
+const apm = require('../../../../..').start({
+  serviceName: 'use-client-sns',
+  captureExceptions: false,
+  centralConfig: false,
+  metricsInterval: 0,
+  cloudProvider: 'none',
+  stackTraceLimit: 4, // get it smaller for reviewing output
+  logLevel: 'info',
+  ignoreMessageQueues: [
+    `*${TEST_IGNORE_TOPIC}`,
+    `*${TEST_IGNORE_TARGET}`,
+    `*${TEST_IGNORE_PHONE}`,
+  ],
+});
+
+const assert = require('assert');
+const {
+  SNSClient,
+  ListTopicsCommand,
+  CreateTopicCommand,
+  DeleteTopicCommand,
+  PublishCommand,
+} = require('@aws-sdk/client-sns');
+
+const TEST_TOPIC_NAME_PREFIX = 'elasticapmtest-topic-';
+
+// https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/client/sns/
+async function useClientSNS(snsClient, topicName) {
+  const region = await snsClient.config.region();
+  const log = apm.logger.child({
+    'event.module': 'app',
+    endpoint: snsClient.config.endpoint,
+    topicName,
+    region,
+  });
+
+  let command;
+  let data;
+
+  // https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/client/sns/command/ListTopicsCommand/
+  // for testing purposes, shouldn't be instrumented
+  command = new ListTopicsCommand({});
+  data = await snsClient.send(command);
+  assert(
+    apm.currentSpan === null,
+    'SNS span (or its HTTP span) should not be currentSpan after awaiting the task',
+  );
+  log.info({ data }, 'listTopics');
+
+  let topicArn;
+  const preexistingTopic = data.Topics.find(
+    (t) => t.TopicArn.split(':').pop() === topicName,
+  );
+
+  if (!preexistingTopic) {
+    // https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/client/sns/command/CreateTopicCommand/
+    // for testing purposes, shouldn't be instrumented
+    command = new CreateTopicCommand({ Name: topicName });
+    data = await snsClient.send(command);
+    assert(
+      apm.currentSpan === null,
+      'SNS span (or its HTTP span) should not be currentSpan after awaiting the task',
+    );
+    log.info({ data }, 'createTopic');
+    topicArn = data.TopicArn;
+  } else {
+    topicArn = preexistingTopic.TopicArn;
+  }
+
+  // https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/client/sns/command/PublishCommand/
+  command = new PublishCommand({
+    Message: 'message to be sent',
+    PhoneNumber: '+34555555555',
+  });
+  data = await snsClient.send(command);
+  assert(
+    apm.currentSpan === null,
+    'SNS span (or its HTTP span) should not be currentSpan after awaiting the task',
+  );
+  log.info({ data }, 'publish with PhoneNumber');
+
+  command = new PublishCommand({
+    Message: 'message to be sent',
+    TopicArn: topicArn,
+  });
+  data = await snsClient.send(command);
+  assert(
+    apm.currentSpan === null,
+    'SNS span (or its HTTP span) should not be currentSpan after awaiting the task',
+  );
+  log.info({ data }, 'publish with TopicArn');
+
+  command = new PublishCommand({
+    Message: 'message to be sent',
+    TopicArn: topicArn + '-unexistent',
+  });
+  try {
+    data = await snsClient.send(command);
+    throw new Error('expected NotFoundException error');
+  } catch (err) {
+    log.info({ data }, 'publish with non existent TopicArn');
+    const statusCode = err && err.$metadata && err.$metadata.httpStatusCode;
+    if (statusCode !== 404) {
+      throw err;
+    }
+  }
+
+  command = new PublishCommand({
+    Message: 'message to be sent',
+    TopicArn: `arn:aws:sns:us-west-2:111111111111:${TEST_IGNORE_TOPIC}`,
+  });
+  try {
+    data = await snsClient.send(command);
+    throw new Error('expected NotFoundException error');
+  } catch (err) {
+    log.info({ data }, 'publish with TopicArn to ignore');
+    const statusCode = err && err.$metadata && err.$metadata.httpStatusCode;
+    if (statusCode !== 404) {
+      throw err;
+    }
+  }
+
+  command = new PublishCommand({
+    Message: 'message to be sent',
+    TargetArn: `arn:aws:sns:us-west-2:111111111111: ${TEST_IGNORE_TARGET}`,
+  });
+  try {
+    data = await snsClient.send(command);
+    throw new Error('expected NotFoundException error');
+  } catch (err) {
+    log.info({ data }, 'publish with TargetArn to ignore');
+    const statusCode = err && err.$metadata && err.$metadata.httpStatusCode;
+    if (statusCode !== 404) {
+      throw err;
+    }
+  }
+
+  command = new PublishCommand({
+    Message: 'message to be sent',
+    PhoneNumber: `+34${TEST_IGNORE_PHONE}`,
+  });
+  data = await snsClient.send(command);
+  assert(
+    apm.currentSpan === null,
+    'SNS span (or its HTTP span) should not be currentSpan after awaiting the task',
+  );
+  log.info({ data }, 'publish with PhoneNumber to ignore');
+
+  command = new PublishCommand({
+    Message: 'message to be sent',
+    TopicArn: topicArn,
+    TargetArn: topicArn,
+    PhoneNumber: `+345555555`,
+  });
+  data = await snsClient.send(command);
+  assert(
+    apm.currentSpan === null,
+    'SNS span (or its HTTP span) should not be currentSpan after awaiting the task',
+  );
+  log.info({ data }, 'publish with TopicArn, TargerArn and PhoneNumber');
+
+  command = new DeleteTopicCommand({ TopicArn: topicArn });
+  data = await snsClient.send(command);
+  assert(
+    apm.currentSpan === null,
+    'SNS span (or its HTTP span) should not be currentSpan after awaiting the task',
+  );
+  log.info({ data }, 'deleteTopic');
+}
+
+// Return a timestamp of the form YYYYMMDDHHMMSS, which can be used in an SNS
+// topic name:
+function getTimestamp() {
+  return new Date()
+    .toISOString()
+    .split('.')[0]
+    .replace(/[^0-9]/g, '');
+}
+
+// ---- mainline
+
+function main() {
+  // Config vars.
+  const region = process.env.TEST_REGION || 'us-east-2';
+  const endpoint = process.env.TEST_ENDPOINT || null;
+  const topicName =
+    process.env.TEST_TOPIC_NAME || TEST_TOPIC_NAME_PREFIX + getTimestamp();
+
+  // Guard against any topic name being used because we will be publishing
+  // messages in it, and potentially *deleting* the topic.
+  if (!topicName.startsWith(TEST_TOPIC_NAME_PREFIX)) {
+    throw new Error(
+      `cannot use topic name "${topicName}", it must start with ${TEST_TOPIC_NAME_PREFIX}`,
+    );
+  }
+
+  const snsClient = new SNSClient({
+    region,
+    endpoint,
+  });
+
+  // Ensure an APM transaction so spans can happen.
+  const tx = apm.startTransaction('manual');
+
+  useClientSNS(snsClient, topicName).then(
+    function () {
+      tx.end();
+      snsClient.destroy();
+      process.exitCode = 0;
+    },
+    function (err) {
+      apm.logger.error(err, 'useClientSNS rejected');
+      tx.setOutcome('failure');
+      tx.end();
+      snsClient.destroy();
+      process.exitCode = 1;
+    },
+  );
+}
+
+main();
