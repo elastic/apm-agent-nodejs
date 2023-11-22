cb27dc32b406ba6b1f18c4c025ac45060bf263ce
David Luna <luna.bistuer@gmail.com>
2023-11-22 10:47:27 +0100

    chore: eenv values set to string

--- test/config/config.test.js
+++ test/config/config.test.js
@@ -21,7 +21,7 @@ let envOverrides;
 /** @type {import('../_utils').TestFixture[]} */
 const testFixtures = [
   {
-    name: 'use agent with defaults',
+    name: 'use agent - shoud have defaults',
     script: 'fixtures/use-agent.js',
     cwd: __dirname,
     noConvenienceConfig: true, // we want full control of the env
@@ -51,11 +51,6 @@ const testFixtures = [
 
       normalize(defaultConfig, console);
 
-      // fields with RegExps are stringified
-      // defaultConfig.sanitizeFieldNamesRegExp.forEach((rexp, index, array) => {
-      //   array[index] = rexp.toString();
-      // });
-
       Object.keys(resolvedConfig)
         .filter((k) => !excludedKeys.has(k))
         .forEach((key) => {
@@ -71,7 +66,7 @@ const testFixtures = [
     },
   },
   {
-    name: 'use agent with env vars',
+    name: 'use agent - shoud be configurable by environment vars',
     script: 'fixtures/use-agent.js',
     cwd: __dirname,
     timeout: 20000, // sanity guard on the test hanging
@@ -79,41 +74,41 @@ const testFixtures = [
     noConvenienceConfig: true,
     env: (function () {
       const envVars = {
-        ELASTIC_APM_ABORTED_ERROR_THRESHOLD: 30,
-        ELASTIC_APM_ACTIVE: false,
-        ELASTIC_APM_API_REQUEST_SIZE: 1024 * 1024,
-        ELASTIC_APM_API_REQUEST_TIME: 20,
+        ELASTIC_APM_ABORTED_ERROR_THRESHOLD: '30',
+        ELASTIC_APM_ACTIVE: 'false',
+        ELASTIC_APM_API_REQUEST_SIZE: '2048',
+        ELASTIC_APM_API_REQUEST_TIME: '20',
         ELASTIC_APM_CAPTURE_BODY: 'on',
         ELASTIC_APM_CAPTURE_ERROR_LOG_STACK_TRACES: 'never',
-        ELASTIC_APM_CAPTURE_EXCEPTIONS: false,
-        ELASTIC_APM_CENTRAL_CONFIG: false,
-        ELASTIC_APM_CONTEXT_PROPAGATION_ONLY: true,
-        ELASTIC_APM_CUSTOM_METRICS_HISTOGRAM_BOUNDARIES: [1, 2, 3, 4],
-        ELASTIC_APM_DISABLE_SEND: true,
-        ELASTIC_APM_DISABLE_INSTRUMENTATIONS: ['my-package'],
+        ELASTIC_APM_CAPTURE_EXCEPTIONS: 'false',
+        ELASTIC_APM_CENTRAL_CONFIG: 'false',
+        ELASTIC_APM_CONTEXT_PROPAGATION_ONLY: 'true',
+        ELASTIC_APM_CUSTOM_METRICS_HISTOGRAM_BOUNDARIES: '1,2,3,4',
+        ELASTIC_APM_DISABLE_SEND: 'true',
+        ELASTIC_APM_DISABLE_INSTRUMENTATIONS: 'package-1,package-2',
         ELASTIC_APM_ENVIRONMENT: 'production',
-        ELASTIC_APM_ERROR_MESSAGE_MAX_LENGTH: 2048,
-        ELASTIC_APM_ERROR_ON_ABORTED_REQUESTS: true,
-        ELASTIC_APM_INSTRUMENT: false,
-        ELASTIC_APM_INSTRUMENT_INCOMING_HTTP_REQUESTS: false,
+        ELASTIC_APM_ERROR_MESSAGE_MAX_LENGTH: '2048',
+        ELASTIC_APM_ERROR_ON_ABORTED_REQUESTS: 'true',
+        ELASTIC_APM_INSTRUMENT: 'false',
+        ELASTIC_APM_INSTRUMENT_INCOMING_HTTP_REQUESTS: 'false',
         ELASTIC_APM_LOG_LEVEL: 'warn',
-        ELASTIC_APM_LONG_FIELD_MAX_LENGTH: 20000,
-        ELASTIC_APM_MAX_QUEUE_SIZE: 2048,
-        ELASTIC_APM_METRICS_INTERVAL: 50,
-        ELASTIC_APM_METRICS_LIMIT: 2000,
-        ELASTIC_APM_SERVER_TIMEOUT: 60,
+        ELASTIC_APM_LONG_FIELD_MAX_LENGTH: '20000',
+        ELASTIC_APM_MAX_QUEUE_SIZE: '2048',
+        ELASTIC_APM_METRICS_INTERVAL: '50',
+        ELASTIC_APM_METRICS_LIMIT: '2000',
+        ELASTIC_APM_SERVER_TIMEOUT: '60',
         ELASTIC_APM_SERVER_URL: 'http://my.server.com:8200',
         ELASTIC_APM_SERVICE_NAME: 'my-service-name',
-        ELASTIC_APM_SOURCE_LINES_ERROR_APP_FRAMES: 10,
-        ELASTIC_APM_SOURCE_LINES_ERROR_LIBRARY_FRAMES: 10,
-        ELASTIC_APM_SOURCE_LINES_SPAN_APP_FRAMES: 5,
-        ELASTIC_APM_SOURCE_LINES_SPAN_LIBRARY_FRAMES: 5,
-        ELASTIC_APM_STACK_TRACE_LIMIT: 100,
+        ELASTIC_APM_SOURCE_LINES_ERROR_APP_FRAMES: '10',
+        ELASTIC_APM_SOURCE_LINES_ERROR_LIBRARY_FRAMES: '10',
+        ELASTIC_APM_SOURCE_LINES_SPAN_APP_FRAMES: '5',
+        ELASTIC_APM_SOURCE_LINES_SPAN_LIBRARY_FRAMES: '5',
+        ELASTIC_APM_STACK_TRACE_LIMIT: '100',
         ELASTIC_APM_TRACE_CONTINUATION_STRATEGY: 'restart',
-        ELASTIC_APM_TRANSACTION_MAX_SPANS: 250,
-        ELASTIC_APM_TRANSACTION_SAMPLE_RATE: 0.5,
-        ELASTIC_APM_USE_PATH_AS_TRANSACTION_NAME: true,
-        ELASTIC_APM_VERIFY_SERVER_CERT: false,
+        ELASTIC_APM_TRANSACTION_MAX_SPANS: '250',
+        ELASTIC_APM_TRANSACTION_SAMPLE_RATE: '0.5',
+        ELASTIC_APM_USE_PATH_AS_TRANSACTION_NAME: 'true',
+        ELASTIC_APM_VERIFY_SERVER_CERT: 'false',
       };
 
       envOverrides = {};
