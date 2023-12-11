ba761ed3a9e3751487e625846cefd1e3a4a8df4e
David Luna <luna.bistuer@gmail.com>
2023-12-11 09:53:49 +0100

    chore: add tests skeleton

--- test/instrumentation/modules/kafkajs/kafkajs.test.js
+++ test/instrumentation/modules/kafkajs/kafkajs.test.js
@@ -17,15 +17,15 @@ const test = require('tape');
 
 const { validateSpan } = require('../../../_validate_schema');
 const { runTestFixtures, sortApmEvents } = require('../../../_utils');
-const { NODE_VER_RANGE_IITM } = require('../../../testconsts');
+// const { NODE_VER_RANGE_IITM } = require('../../../testconsts');
 
 const kafkaUrl = process.env.KAFKA_URL || 'localhost:9093';
 
 /** @type {import('../../../_utils').TestFixture[]} */
 const testFixtures = [
   {
-    name: 'simple Kafkajs usage scenario',
-    script: 'fixtures/use-kafkajs.js',
+    name: 'simple Kafkajs usage scenario for single message processing',
+    script: 'fixtures/use-kafkajs-each-message.js',
     cwd: __dirname,
     env: {
       TEST_CLIENT_ID: 'elastic-kafka-client',
@@ -35,6 +35,42 @@ const testFixtures = [
     checkApmServer(t, apmServer) {
       t.ok(apmServer.events[0].metadata, 'metadata');
       const events = sortApmEvents(apmServer.events);
+
+      console.log(events)
+
+      // Compare some common fields across all spans.
+      // ignore http/external spans
+      const spans = events
+        .filter((e) => e.span && e.span.type !== 'external')
+        .map((e) => e.span);
+      spans.forEach((s) => {
+        const errs = validateSpan(s);
+        t.equal(errs, null, 'span is valid (per apm-server intake schema)');
+      });
+    },
+  },
+  {
+    name: 'simple Kafkajs usage scenario for batch message processing',
+    script: 'fixtures/use-kafkajs-each-batch.js',
+    cwd: __dirname,
+    env: {
+      TEST_CLIENT_ID: 'elastic-kafka-client',
+      TEST_TOPIC: 'elasticapmtest-topic-5678',
+      TEST_KAFKA_URL: kafkaUrl,
+    },
+    checkApmServer(t, apmServer) {
+      t.ok(apmServer.events[0].metadata, 'metadata');
+      const events = sortApmEvents(apmServer.events);
+
+      // Compare some common fields across all spans.
+      // ignore http/external spans
+      const spans = events
+        .filter((e) => e.span && e.span.type !== 'external')
+        .map((e) => e.span);
+      spans.forEach((s) => {
+        const errs = validateSpan(s);
+        t.equal(errs, null, 'span is valid (per apm-server intake schema)');
+      });
     },
   },
 ];
