b04fd1fe2d8a566be7671983b39a566e1dafd8d1
csnate <nate.tovo@gmail.com>
2024-07-02 14:47:17 -0500

    fix: allow file config to override true default values
    This allows a "false" boolean value to override a "true" default value when loading the options via the elastic-apm-node.js config file
    
    Fixes: #4112
    Closes: #4112

--- test/start/file/test.test.js
+++ test/start/file/test.test.js
@@ -19,5 +19,7 @@ const tape = require('tape');
 
 tape('from-file serviceName test', function (t) {
   t.equals(agent._conf.serviceName, 'from-file');
+  t.equals(agent._conf.active, false);
+  t.equals(agent._conf.captureBody, 'off'); // Existing default
   t.end();
 });
