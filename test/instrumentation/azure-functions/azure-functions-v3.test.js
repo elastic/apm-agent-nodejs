68e30c410c24cbcdebd856df811b350ea9abaf76
Trent Mick <trent.mick@elastic.co>
2025-01-15 17:34:40 -0800

    update this test file to use the shared utils moved out to azfunctestutils.js

--- test/instrumentation/azure-functions/azure-functions-v3.test.js
+++ test/instrumentation/azure-functions/azure-functions-v3.test.js
@@ -11,7 +11,6 @@
 const assert = require('assert');
 const { exec, spawn } = require('child_process');
 const fs = require('fs');
-const http = require('http');
 const os = require('os');
 const path = require('path');
 
@@ -21,6 +20,11 @@ const treekill = require('tree-kill');
 
 const { MockAPMServer } = require('../../_mock_apm_server');
 const { formatForTComment } = require('../../_utils');
+const {
+  getEventField,
+  makeTestRequest,
+  waitForServerReady,
+} = require('./azfunctestutils');
 
 // Azure Functions programming model v3 supports node 14.x-20.x:
 // https://learn.microsoft.com/en-ca/azure/azure-functions/functions-reference-node?tabs=javascript%2Cwindows%2Cazure-cli&pivots=nodejs-model-v4#supported-versions
@@ -37,130 +41,6 @@ if (!semver.satisfies(process.version, '^18.0.1')) {
     '# SKIP Azure Functions tests on Windows because of flaky azure-functions-core-tools install (see https://github.com/elastic/apm-agent-nodejs/issues/3107)',
   );
   process.exit();
-}
-
-/**
- * Wait for the test "func start" to be ready.
- *
- * This polls the <http://127.0.0.1:7071/admin/functions> admin endpoint until
- * it gets a 200 response, assuming the server is ready by then.
- * It times out after ~60s -- so long because startup on Windows CI has been
- * found to take a long time (it is downloading 250MB+ in "ExtensionBundle"s).
- *
- * @param {Test} t - This is only used to `t.comment(...)` with progress.
- * @param {Function} cb - Calls `cb(err)` if there was a timeout, `cb()` on
- *    success.
- */
-function waitForServerReady(t, cb) {
-  let sentinel = 30;
-  const INTERVAL_MS = 2000;
-
-  const pollForServerReady = () => {
-    const req = http.get(
-      'http://127.0.0.1:7071/admin/functions',
-      {
-        agent: false,
-        timeout: 500,
-      },
-      (res) => {
-        res.resume();
-        res.on('end', () => {
-          if (res.statusCode !== 200) {
-            scheduleNextPoll(`statusCode=${res.statusCode}`);
-          } else {
-            cb();
-          }
-        });
-      },
-    );
-    req.on('error', (err) => {
-      scheduleNextPoll(err.message);
-    });
-  };
-
-  const scheduleNextPoll = (msg) => {
-    t.comment(
-      `[sentinel=${sentinel} ${new Date().toISOString()}] wait another 2s for server ready: ${msg}`,
-    );
-    sentinel--;
-    if (sentinel <= 0) {
-      cb(new Error('timed out'));
-    } else {
-      setTimeout(pollForServerReady, INTERVAL_MS);
-    }
-  };
-
-  pollForServerReady();
-}
-
-async function makeTestRequest(t, testReq) {
-  return new Promise((resolve, reject) => {
-    const reqOpts = testReq.reqOpts;
-    const url = `http://127.0.0.1:7071${reqOpts.path}`;
-    t.comment(
-      `makeTestRequest: "${testReq.testName}" (${reqOpts.method} ${url})`,
-    );
-    const req = http.request(
-      url,
-      {
-        method: reqOpts.method,
-      },
-      (res) => {
-        const chunks = [];
-        res.on('data', (chunk) => {
-          chunks.push(chunk);
-        });
-        res.on('end', () => {
-          const body = Buffer.concat(chunks);
-          if (testReq.expectedRes.statusCode) {
-            t.equal(
-              res.statusCode,
-              testReq.expectedRes.statusCode,
-              `res.statusCode === ${testReq.expectedRes.statusCode}`,
-            );
-          }
-          if (testReq.expectedRes.headers) {
-            for (const [k, v] of Object.entries(testReq.expectedRes.headers)) {
-              if (v instanceof RegExp) {
-                t.ok(
-                  v.test(res.headers[k]),
-                  `res.headers[${JSON.stringify(k)}] =~ ${v}`,
-                );
-              } else {
-                t.equal(
-                  res.headers[k],
-                  v,
-                  `res.headers[${JSON.stringify(k)}] === ${JSON.stringify(v)}`,
-                );
-              }
-            }
-          }
-          if (testReq.expectedRes.body) {
-            if (testReq.expectedRes.body instanceof RegExp) {
-              t.ok(
-                testReq.expectedRes.body.test(body),
-                `body =~ ${testReq.expectedRes.body}`,
-              );
-            } else if (typeof testReq.expectedRes.body === 'string') {
-              t.equal(body.toString(), testReq.expectedRes.body, 'body');
-            } else {
-              t.fail(
-                `unsupported type for TEST_REQUESTS[].expectedRes.body: ${typeof testReq
-                  .expectedRes.body}`,
-              );
-            }
-          }
-          resolve();
-        });
-      },
-    );
-    req.on('error', reject);
-    req.end();
-  });
-}
-
-function getEventField(e, fieldName) {
-  return (e.transaction || e.error || e.span)[fieldName];
 }
 
 /**
