2b2c7e3bae10cb17bde47d0a5d0a9dd96c222095
Trent Mick <trent.mick@elastic.co>
2025-04-25 13:35:13 -0700

    test: fix testing for @elastic/elasticsearch version 9 (#4583)
    `@elastic/elasticsearch@9.0.0` was released recently. This updates our testing to pass with it. So far no instrumentation changes were necessary. (I'm not sure if testing with v9 of the client would have failed with the older 8.7.1 version of the server that we had been using.)
    
    Also reduce the number of ES client versions tested.
    
    Obsoletes: #4576

--- examples/trace-elasticsearch.js
+++ examples/trace-elasticsearch.js
@@ -6,7 +6,7 @@
  * compliance with the BSD 2-Clause License.
  */
 
-// A small example showing Elastic APM tracing @elastic/elasticsearch version 8.
+// A small example showing Elastic APM tracing @elastic/elasticsearch.
 //
 // This assumes an Elasticsearch running on localhost. You can use:
 //    npm run docker:start elasticsearch
@@ -14,17 +14,12 @@
 //    npm run docker:stop
 
 const apm = require('../').start({
-  serviceName: 'example-trace-elasticsearch8',
+  serviceName: 'example-trace-elasticsearch',
 });
 
-// eslint-disable-next-line no-unused-vars
-const { Client, HttpConnection } = require('@elastic/elasticsearch');
+const { Client } = require('@elastic/elasticsearch');
 
 const client = new Client({
-  // By default version 8 uses the new undici HTTP client lib. You can specify
-  // `HttpConnection` to use the older HTTP client.
-  // Connection: HttpConnection,
-
   node: process.env.ES_URL || 'http://localhost:9200',
   auth: {
     username: process.env.ES_USERNAME || undefined,
@@ -33,7 +28,7 @@ const client = new Client({
   maxRetries: 1,
 });
 
-async function run() {
+async function main() {
   // For tracing spans to be created, there must be an active transaction.
   // Typically, a transaction is automatically started for incoming HTTP
   // requests to a Node.js server. However, because this script is not running
@@ -75,25 +70,23 @@ async function run() {
       t2.end();
     });
 
-  // Example aborting requests using AbortController (node v15 and above).
-  if (global.AbortController) {
-    const t3 = apm.startTransaction('t3');
-    const ac = new AbortController(); // eslint-disable-line no-undef
-    setImmediate(() => {
-      ac.abort();
-    });
-    try {
-      const res = await client.search(
-        { query: { match_all: {} } },
-        { signal: ac.signal },
-      );
-      console.log('[example 3] search response:', res);
-    } catch (err) {
-      console.log('[example 3] search error:', err);
-    } finally {
-      t3.end();
-    }
+  // Example aborting requests using AbortController.
+  const t3 = apm.startTransaction('t3');
+  const ac = new AbortController(); // eslint-disable-line no-undef
+  setImmediate(() => {
+    ac.abort();
+  });
+  try {
+    const res = await client.search(
+      { query: { match_all: {} } },
+      { signal: ac.signal },
+    );
+    console.log('[example 3] search response:', res);
+  } catch (err) {
+    console.log('[example 3] search error:', err);
+  } finally {
+    t3.end();
   }
 }
 
-run();
+main();
