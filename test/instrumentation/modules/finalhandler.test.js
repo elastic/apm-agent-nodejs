e66ff64996f3ddd4ac9e0d2d490a42f2c315232a
Trent Mick <trent.mick@elastic.co>
2026-07-07 12:32:24 -0700

    bump finalhandler min-supported node to 18 (the 2.1.1 release finally formalizes this in engines.node)

--- test/instrumentation/modules/finalhandler.test.js
+++ test/instrumentation/modules/finalhandler.test.js
@@ -14,12 +14,11 @@ if (process.env.GITHUB_ACTIONS === 'true' && process.platform === 'win32') {
   process.exit(0);
 }
 
-// finalhandler@2.1.0 broke <=14 support (https://github.com/pillarjs/finalhandler/issues/87)
 var finalhandlerVer = require('finalhandler/package.json').version;
 var semver = require('semver');
 if (
-  semver.gte(finalhandlerVer, '2.1.0') &&
-  semver.lt(process.version, '16.0.0')
+  semver.gte(finalhandlerVer, '2.0.0') &&
+  semver.lt(process.version, '18.0.0')
 ) {
   console.log(
     `# SKIP finalhandler@${finalhandlerVer} does not support node ${process.version}`,
